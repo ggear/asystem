4a933e8fcdaa72198a33ed300f550a373932fd90
Graham Gear <gearg1@south32.net>
2020-07-08 23:05:21 +0800

    First pass certs implementation

--- macmini-liz/certs/run.sh
+++ macmini-liz/certs/run.sh
@@ -1,1 +1,7 @@
 # TODO: Enable Let's encrypt, auto renew and local DNS as per https://blog.bryanroessler.com/2019-02-09-automatic-certbot-namecheap-acme-dns
+# https://github.com/joohoi/acme-dns#using-docker
+
+# https://github.com/linuxserver/docker-letsencrypt
+# https://github.com/adferrand/dnsrobocert/issues/11
+# Keep authorized keys on UDM, otherwise I cant adopt? sshpass if key not available?
+
