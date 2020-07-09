b2fa53edeed9ac67a0e5a402ec696babda1c63c3
Graham Gear <gearg1@south32.net>
2020-07-09 14:48:37 +0800

    Update todos for deploy, run, init and poll

--- macmini-liz/certs/run.sh
+++ macmini-liz/certs/run.sh
@@ -1,4 +1,7 @@
-# all - generalise deploy artefacts to scripts, docker, certs, 
+# all - generalise deploy artefacts to scripts, docker, certs from target - wrap up as bundle
+#     - add python3 install, include fab, mqtt etc in requirements.txt
+#     - consider an init.sh and a poll.sh script, the former does a cron, the latter does the ticks
+#     - convert all docker-compose to run.sh
 
 # macmini-liz/certs - Add mount points to docker-compose.yml for config, certs, hooks (udm, anode, ha etc)
 
@@ -6,4 +9,6 @@
 
 # macmini-liz/ha - Develop script to copy certs, checkuptime, restart
 
-# macmini-liz/anode - Develop script to copy certs, run speedtest, uptime to down, perhaps restart
+# macmini-liz/anode - Develop script to copy certs, uptime to down, perhaps restart
+
+# macmini-liz/speedtest - run speedtest in docker
