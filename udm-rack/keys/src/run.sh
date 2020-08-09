9f2417f7cb72468e48a03e2e746ff9f0e87204ab
Graham Gear <graham.gear@gmail.com>
2020-08-09 15:21:46 +0800

    Format codebase

--- udm-rack/keys/src/run.sh
+++ udm-rack/keys/src/run.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 
-cp -rvf $(ls -td /var/lib/asystem/install/* | head -1)/udm-rack_*/host/run.sh /mnt/data/on_boot.d/keys.sh
+[ -d "/var/lib/asystem/install" ] &&
+  [ -d "$(ls -td /var/lib/asystem/install/* | head -1)" ] &&
+  [ -f "$(ls -td /var/lib/asystem/install/* | head -1)/udm-rack_*/host/run.sh" ] &&
+  cp -rvf "$(ls -td /var/lib/asystem/install/* | head -1)/udm-rack_*/host/run.sh" /mnt/data/on_boot.d/keys.sh
