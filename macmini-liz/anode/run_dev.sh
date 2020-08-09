b14ae8b1b1371874e074455e16459725ed3d5344
Graham Gear <graham.gear@gmail.com>
2020-08-09 15:21:46 +0800

    Format codebase

--- macmini-liz/anode/run_dev.sh
+++ macmini-liz/anode/run_dev.sh
@@ -1,3 +1,3 @@
 DATA_DIR=./target/runtime-system \
-LOCAL_IP=$(/usr/sbin/ipconfig getifaddr en1) \
-docker-compose --no-ansi run --service-ports anode anode -v
+  LOCAL_IP=$(/usr/sbin/ipconfig getifaddr en1) \
+  docker-compose --no-ansi run --service-ports anode anode -v
--- macmini-liz/vernemq/src/run.sh
+++ macmini-liz/vernemq/src/run.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 DATA_DIR=/home/asystem/vernemq \
-LOCAL_IP=$(/usr/sbin/ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep '192.168.1') \
-docker-compose -f /var/lib/asystem/install/$VERSION_ABSOLUTE/macmini-liz/vernemq/docker-compose.yml --no-ansi up --force-recreate -d
+  LOCAL_IP=$(/usr/sbin/ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep '192.168.1') \
+  docker-compose -f /var/lib/asystem/install/$VERSION_ABSOLUTE/macmini-liz/vernemq/docker-compose.yml --no-ansi up --force-recreate -d
