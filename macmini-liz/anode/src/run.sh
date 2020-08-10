a6404fa56340e867d43fdf1038fd7237901388b0
Graham Gear <graham.gear@gmail.com>
2020-08-10 13:11:41 +0800

    Reduce verbosity on run scripts

--- macmini-liz/anode/src/run.sh
+++ macmini-liz/anode/src/run.sh
@@ -1,16 +1,12 @@
 #!/bin/sh
 
+cd /var/lib/asystem/install/$VERSION_ABSOLUTE/macmini-liz/anode
 mkdir -p /home/asystem/anode
-cp -rf \
-  /var/lib/asystem/install/$VERSION_ABSOLUTE/macmini-liz/anode/.profile \
-  /var/lib/asystem/install/$VERSION_ABSOLUTE/macmini-liz/anode/anode.yaml \
-  /home/asystem/anode
-docker image load -i /var/lib/asystem/install/$VERSION_ABSOLUTE/macmini-liz/anode/anode-$VERSION_ABSOLUTE.tar.gz
+cp -rf .profile anode.yaml /home/asystem/anode
+docker image load -i anode-$VERSION_ABSOLUTE.tar.gz
 docker stop anode 2>&1 >/dev/null && docker wait anode 2>&1 >/dev/null
 VERSION=$VERSION_ABSOLUTE \
   DATA_DIR=/home/asystem/anode \
   LOCAL_IP=$(/usr/sbin/ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep '192.168.1') \
-  docker-compose -f /var/lib/asystem/install/$VERSION_ABSOLUTE/macmini-liz/anode/docker-compose.yml \
-  --env-file /var/lib/asystem/install/$VERSION_ABSOLUTE/macmini-liz/anode/.env \
-  --no-ansi up --force-recreate -d
+  docker-compose --no-ansi up --force-recreate -d
 docker system prune --volumes -f
--- macmini-liz/vernemq/src/run.sh
+++ macmini-liz/vernemq/src/run.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 
+cd /var/lib/asystem/install/$VERSION_ABSOLUTE/macmini-liz/vernemq
 mkdir -p /home/asystem/vernemq
 DATA_DIR=/home/asystem/vernemq \
   LOCAL_IP=$(/usr/sbin/ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep '192.168.1') \
-  docker-compose -f /var/lib/asystem/install/$VERSION_ABSOLUTE/macmini-liz/vernemq/docker-compose.yml \
-  --env-file /var/lib/asystem/install/$VERSION_ABSOLUTE/macmini-liz/vernemq/.env \
-  --no-ansi up --force-recreate -d
+  docker-compose --no-ansi up --force-recreate -d
