49c7f2e9ce0fe779c43f6b406a6c4c1954887ddf
Graham Gear <graham.gear@gmail.com>
2020-08-14 16:47:20 +0800

    Update asystem-10.100.1036

--- macmini-liz/host/src/run.sh
+++ macmini-liz/host/src/run.sh
@@ -15,8 +15,7 @@
 ################################################################################
 # Utilities
 ################################################################################
-apt-get update
-apt-get install -y \
+apt-get update && apt-get install -y \
   vim=2:8.1.0875-5 \
   curl=7.64.0-4+deb10u1 \
   net-tools=1.60+git20180626.aebd88e-1
@@ -24,7 +23,7 @@ apt-get install -y \
 ################################################################################
 # Monitoring
 ################################################################################
-apt-get install -y \
+apt-get update && apt-get install -y \
   htop=2.2.0-1+b1 \
   lm-sensors=1:3.5.0-3
 sensors-detect --auto
@@ -32,7 +31,7 @@ sensors-detect --auto
 ################################################################################
 # Docker
 ################################################################################
-apt-get install -y \
+apt-get update && apt-get install -y \
   apt-transport-https=1.8.2.1 \
   ca-certificates=20200601~deb10u1 \
   gnupg-agent=2.2.12-1+deb10u1 \
@@ -42,7 +41,7 @@ add-apt-repository \
   "deb [arch=amd64] https://download.docker.com/linux/debian \
    $(lsb_release -cs) \
    stable"
-apt-get install -y \
+apt-get update && apt-get install -y \
   docker-ce=5:19.03.12~3-0~debian-buster \
   docker-ce-cli=5:19.03.12~3-0~debian-buster \
   containerd.io=1.2.13-2
