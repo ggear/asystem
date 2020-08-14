a1618f5251c788cc2789db0e762e35956df08c4e
Graham Gear <graham.gear@gmail.com>
2020-08-14 16:38:54 +0800

    Update asystem-10.100.1032

--- fabfile.py
+++ fabfile.py
@@ -235,7 +235,7 @@ def _release(context):
                 if _run_local(context, "ssh -o StrictHostKeyChecking=no -o PasswordAuthentication=no -o BatchMode=yes root@{} exit"
                               .format(host), hide="err", warn=True).exited > 0 else ""
 
-            print(_run_local(context, "{} ssh -q root@{} 'echo Connected to {}'".format(ssh, host, host)))
+            print(_run_local(context, "{} ssh -q root@{} 'echo Connected to {}'".format(ssh, host, host),hide="err", warn=True).exited)
 
             install = "/var/lib/asystem/install/{}/{}".format(_get_versions()[0], module)
             if os.listdir(join(DIR_ROOT, module, "target/release")):
