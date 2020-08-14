3bb6a9237a90ce82e633c3c2293349b67d3ad498
Graham Gear <graham.gear@gmail.com>
2020-08-14 15:32:40 +0800

    Update asystem-10.100.1020

--- macmini-liz/anode/src/run.sh
+++ macmini-liz/anode/src/run.sh
@@ -4,10 +4,10 @@ cd /var/lib/asystem/install/$VERSION_ABSOLUTE/macmini-liz/anode
 mkdir -p /home/asystem/anode
 cp -rf .pem .profile anode.yaml /home/asystem/anode
 docker image load -i anode-$VERSION_ABSOLUTE.tar.gz
-docker stop anode 2>&1 >/dev/null && docker wait anode 2>&1 >/dev/null
+#docker stop anode 2>&1 >/dev/null && docker wait anode 2>&1 >/dev/null
 VERSION=$VERSION_ABSOLUTE \
   DATA_DIR=/home/asystem/anode \
   LOCAL_IP=$(/usr/sbin/ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep '192.168.1') \
   docker-compose --no-ansi up --force-recreate -d
 docker system prune --volumes -f
-cd /root && ls -dt /var/lib/asystem/install/*/ | tail -n -$(($(ls -dt ls -dt /var/lib/asystem/install/*/ | wc -l) - 2)) | xargs rm -rf
+cd /root && ls -dt /var/lib/asystem/install/*/ | tail -n -$(($(ls -dt /var/lib/asystem/install/*/ | wc -l) - 2)) | xargs rm -rf
