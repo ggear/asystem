b45a7d38e0b6a0d25f9d98741abadd8c4c1aaead
Graham Gear <graham.gear@gmail.com>
2020-08-15 10:14:26 +0800

    Update asystem-10.100.1055

--- udm-rack/keys/src/run.sh
+++ udm-rack/keys/src/run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 [ -d "/var/lib/asystem/install" ] &&
-  [ -d "$(ls -td /var/lib/asystem/install/* | head -1)" ] &&
-  [ -f "$(ls -td /var/lib/asystem/install/* | head -1)/udm-rack_*/host/run.sh" ] &&
-  cp -rvf "$(ls -td /var/lib/asystem/install/* | head -1)/udm-rack_*/host/run.sh" /mnt/data/on_boot.d/keys.sh
+  [ -d "$(ls -td /var/lib/asystem/install/udm-rack*/host/* | head -1)" ] &&
+  [ -f "$(ls -td /var/lib/asystem/install/udm-rack*/host/* | head -1)/run.sh" ] &&
+  cp -rvf "$(ls -td /var/lib/asystem/install/udm-rack*/host/* | head -1)/run.sh" /mnt/data/on_boot.d/keys.sh
