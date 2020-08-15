b35a1f5277e4b7342d10326abe27fa7c8d28320d
Graham Gear <graham.gear@gmail.com>
2020-08-15 10:02:18 +0800

    Update asystem-10.100.1052

--- macmini-liz/homeassistant/src/run.sh
+++ macmini-liz/homeassistant/src/run.sh
@@ -2,6 +2,8 @@
 
 cd /var/lib/asystem/install/$(hostname)/homeassistant/$VERSION_ABSOLUTE
 mkdir -p /home/asystem/homeassistant
+docker-compose down -v
+docker system prune --volumes -f
 DATA_DIR=/home/asystem/homeassistant \
   LOCAL_IP=$(/usr/sbin/ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep '192.168.1') \
   docker-compose --no-ansi up --force-recreate -d
