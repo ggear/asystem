f4a78d6cda08af5bb8296560c797ea8e4ba7565a
Graham Gear <graham.gear@gmail.com>
2020-08-15 09:13:17 +0800

    Update asystem-10.100.1042

--- macmini-liz/homeassistant/src/run.sh
+++ macmini-liz/homeassistant/src/run.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 cd /var/lib/asystem/install/$VERSION_ABSOLUTE/${HOSTNAME}/vernemq
-mkdir -p /home/asystem/vernemq
-DATA_DIR=/home/asystem/vernemq \
+mkdir -p /home/asystem/homeassistant
+DATA_DIR=/home/asystem/homeassistant \
   LOCAL_IP=$(/usr/sbin/ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep '192.168.1') \
   docker-compose --no-ansi up --force-recreate -d
