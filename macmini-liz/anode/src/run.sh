e933230c84fee63f2a1e4f2c6e2439694cbc5a59
Graham Gear <graham.gear@gmail.com>
2020-08-15 09:34:45 +0800

    Update asystem-10.100.1047

--- macmini-liz/anode/src/run.sh
+++ macmini-liz/anode/src/run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-cd /var/lib/asystem/install/$VERSION_ABSOLUTE/$(hostname)/anode
+cd /var/lib/asystem/install/$(hostname)/anode/$VERSION_ABSOLUTE
 mkdir -p /home/asystem/anode
 cp -rf .pem .profile anode.yaml /home/asystem/anode
 docker image load -i anode-$VERSION_ABSOLUTE.tar.gz
@@ -9,5 +9,3 @@ VERSION=$VERSION_ABSOLUTE \
   DATA_DIR=/home/asystem/anode \
   LOCAL_IP=$(/usr/sbin/ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep '192.168.1') \
   docker-compose --no-ansi up --force-recreate -d
-docker system prune --volumes -f
-cd /root && ls -dt /var/lib/asystem/install/*/ | tail -n -$(($(ls -dt /var/lib/asystem/install/*/ | wc -l) - 2)) | xargs rm -rf
