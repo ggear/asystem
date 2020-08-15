7f451adffaff3ec2140df899faba673701962a92
Graham Gear <graham.gear@gmail.com>
2020-08-15 09:16:48 +0800

    Update asystem-10.100.1044

--- macmini-liz/homeassistant/src/run.sh
+++ macmini-liz/homeassistant/src/run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-cd /var/lib/asystem/install/$VERSION_ABSOLUTE/$(hostname)/vernemq
+cd /var/lib/asystem/install/$VERSION_ABSOLUTE/$(/usr/bin/hostname)/vernemq
 mkdir -p /home/asystem/homeassistant
 DATA_DIR=/home/asystem/homeassistant \
   LOCAL_IP=$(/usr/sbin/ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep '192.168.1') \
