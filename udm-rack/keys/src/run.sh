f839e5b08311a1a5f3232935ef26e795026df51a
Graham Gear <graham.gear@gmail.com>
2020-08-16 14:24:48 +0800

    Update asystem-10.100.1112

--- udm-rack/keys/src/run.sh
+++ udm-rack/keys/src/run.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
+SERVICE_NAME=homeassistant
+SERVICE_HOME=/home/asystem/${SERVICE_NAME}/${VERSION_ABSOLUTE}
+SERVICE_HOME_OLD=$(ls -dt $(dirname ${SERVICE_HOME})/*/ 2>/dev/null | head -n 1)
+SERVICE_HOME_OLDEST=$(ls -dt $(dirname ${SERVICE_HOME})/*/ 2>/dev/null | tail -n -$(($(ls -dt $(dirname ${SERVICE_HOME})/*/ 2>/dev/null | wc -l) - 1)) 2>/dev/null)
+SERVICE_INSTALL=/var/lib/asystem/install/$(hostname)/${SERVICE_NAME}/${VERSION_ABSOLUTE}
+SERVICE_HOST_IP=$(/usr/sbin/ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep '192.168.1')
+
 [ -d "/var/lib/asystem/install" ] &&
   [ -d "$(ls -td /var/lib/asystem/install/udm-rack*/host/* | head -1)" ] &&
   [ -f "$(ls -td /var/lib/asystem/install/udm-rack*/host/* | head -1)/run.sh" ] &&
