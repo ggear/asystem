74e3f3e7939ed14afe90044a7113b8e815bc1886
Graham Gear <graham.gear@gmail.com>
2020-08-16 13:13:24 +0800

    Update asystem-10.100.1089

--- macmini-liz/homeassistant/src/run.sh
+++ macmini-liz/homeassistant/src/run.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-NAME=homeassistant
-HOME=/home/asystem/${NAME}
-INSTALL=/var/lib/asystem/install/$(hostname)/${NAME}/$VERSION_ABSOLUTE
-HOST=$(/usr/sbin/ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep '192.168.1')
+SERVICE_NAME=homeassistant
+SERVICE_HOME=/home/asystem/${SERVICE_NAME}
+SERVICE_INSTALL=/var/lib/asystem/install/$(hostname)/${SERVICE_NAME}/$VERSION_ABSOLUTE
+SERVICE_HOST_IP=$(/usr/sbin/ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep '192.168.1')
 
-cd ${INSTALL}
-mkdir -p ${HOME}
-cp -rvf $(find config -mindepth 1) ${HOME}
-docker stop ${NAME} 2>&1 >/dev/null && docker wait ${NAME} 2>&1 >/dev/null && docker system prune --volumes -f
-DATA_DIR=${HOME} LOCAL_IP=${HOST} docker-compose --no-ansi up --force-recreate -d
+cd ${SERVICE_INSTALL}
+mkdir -p ${SERVICE_HOME}
+cp -rvf $(find config -mindepth 1) ${SERVICE_HOME}
+docker stop ${SERVICE_NAME} 2>&1 >/dev/null && docker wait ${SERVICE_NAME} 2>&1 >/dev/null && docker system prune --volumes -f
+DATA_DIR=${SERVICE_HOME} LOCAL_IP=${SERVICE_HOST_IP} docker-compose --no-ansi up --force-recreate -d
