f49c042175257c2e9371d47df9ffef4e77adb68a
Graham Gear <graham.gear@gmail.com>
2020-08-16 14:14:04 +0800

    Update asystem-10.100.1111

--- macmini-liz/vernemq/src/run.sh
+++ macmini-liz/vernemq/src/run.sh
@@ -1,9 +1,19 @@
 #!/bin/sh
 
-cd /var/lib/asystem/install/$(hostname)/vernemq/$VERSION_ABSOLUTE
-mkdir -p /home/asystem/vernemq
-cp -rvf $(find config -mindepth 1) /home/asystem/vernemq
-docker stop vernemq 2>&1 >/dev/null && docker wait vernemq 2>&1 >/dev/null && docker system prune --volumes -f
-DATA_DIR=/home/asystem/vernemq \
-  LOCAL_IP=$(/usr/sbin/ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep '192.168.1') \
-  docker-compose --no-ansi up --force-recreate -d
+SERVICE_NAME=vernemq
+SERVICE_HOME=/home/asystem/${SERVICE_NAME}/${VERSION_ABSOLUTE}
+SERVICE_HOME_OLD=$(ls -dt $(dirname ${SERVICE_HOME})/*/ 2>/dev/null | head -n 1)
+SERVICE_HOME_OLDEST=$(ls -dt $(dirname ${SERVICE_HOME})/*/ 2>/dev/null | tail -n -$(($(ls -dt $(dirname ${SERVICE_HOME})/*/ 2>/dev/null | wc -l) - 1)) 2>/dev/null)
+SERVICE_INSTALL=/var/lib/asystem/install/$(hostname)/${SERVICE_NAME}/${VERSION_ABSOLUTE}
+SERVICE_HOST_IP=$(/usr/sbin/ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep '192.168.1')
+
+cd "${SERVICE_INSTALL}" || exit
+docker stop "${SERVICE_NAME}" 2>&1 >/dev/null && docker wait "${SERVICE_NAME}" 2>&1 >/dev/null && docker system prune --volumes -f
+if [ -d "$SERVICE_HOME_OLD" ]; then
+  cp -rvf "$SERVICE_HOME_OLD" "$SERVICE_HOME"
+else
+  mkdir -p "${SERVICE_HOME}"
+fi
+cp -rvf $(find config -mindepth 1) "${SERVICE_HOME}"
+rm -rvf "$SERVICE_HOME_OLDEST"
+DATA_DIR="${SERVICE_HOME}" LOCAL_IP="${SERVICE_HOST_IP}" docker-compose --no-ansi up --force-recreate -d
