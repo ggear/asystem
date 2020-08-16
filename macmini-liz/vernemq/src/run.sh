3ad1b99dd029f9afe0f61b26115e083e7e765908
Graham Gear <graham.gear@gmail.com>
2020-08-16 11:43:04 +0800

    Update asystem-10.100.1075

--- macmini-liz/vernemq/src/run.sh
+++ macmini-liz/vernemq/src/run.sh
@@ -2,6 +2,7 @@
 
 cd /var/lib/asystem/install/$(hostname)/vernemq/$VERSION_ABSOLUTE
 mkdir -p /home/asystem/vernemq
+docker stop vernemq 2>&1 >/dev/null && docker wait vernemq 2>&1 >/dev/null && docker system prune --volumes -f
 DATA_DIR=/home/asystem/vernemq \
   LOCAL_IP=$(/usr/sbin/ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep '192.168.1') \
   docker-compose --no-ansi up --force-recreate -d
