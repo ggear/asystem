7b239503a4240aa2c4ea4d279848f82f1d7fffa1
Graham Gear <graham.gear@gmail.com>
2020-08-16 13:12:11 +0800

    Update asystem-10.100.1088

--- macmini-liz/homeassistant/src/run.sh
+++ macmini-liz/homeassistant/src/run.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 
-cd /var/lib/asystem/install/$(hostname)/homeassistant/$VERSION_ABSOLUTE
-mkdir -p /home/asystem/homeassistant
-cp -rvf $(find config -mindepth 1) /home/asystem/homeassistant
-docker stop homeassistant 2>&1 >/dev/null && docker wait homeassistant 2>&1 >/dev/null && docker system prune --volumes -f
-DATA_DIR=/home/asystem/homeassistant \
-  LOCAL_IP=$(/usr/sbin/ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep '192.168.1') \
-  docker-compose --no-ansi up --force-recreate -d
+NAME=homeassistant
+HOME=/home/asystem/${NAME}
+INSTALL=/var/lib/asystem/install/$(hostname)/${NAME}/$VERSION_ABSOLUTE
+HOST=$(/usr/sbin/ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep '192.168.1')
+
+cd ${INSTALL}
+mkdir -p ${HOME}
+cp -rvf $(find config -mindepth 1) ${HOME}
+docker stop ${NAME} 2>&1 >/dev/null && docker wait ${NAME} 2>&1 >/dev/null && docker system prune --volumes -f
+DATA_DIR=${HOME} LOCAL_IP=${HOST} docker-compose --no-ansi up --force-recreate -d
