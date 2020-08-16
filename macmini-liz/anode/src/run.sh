ca6acdd8e9c63b01bd654b6757c5e48c2ff9433e
Graham Gear <graham.gear@gmail.com>
2020-08-16 15:03:06 +0800

    Update asystem-10.100.1115

--- macmini-liz/anode/src/run.sh
+++ macmini-liz/anode/src/run.sh
@@ -9,12 +9,14 @@ SERVICE_HOST_IP=$(/usr/sbin/ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]
 
 cd "${SERVICE_INSTALL}" || exit
 docker image load -i anode-${VERSION_ABSOLUTE}.tar.gz
-docker stop "${SERVICE_NAME}" 2>&1 >/dev/null && docker wait "${SERVICE_NAME}" 2>&1 >/dev/null && docker system prune --volumes -f
-if [ -d "$SERVICE_HOME_OLD" ]; then
-  cp -rvf "$SERVICE_HOME_OLD" "$SERVICE_HOME"
-else
-  mkdir -p "${SERVICE_HOME}"
+docker stop "${SERVICE_NAME}" 2>/dev/null && docker wait "${SERVICE_NAME}" 2>/dev/null && docker system prune --volumes -f
+if [ ! -d "$SERVICE_HOME" ]; then
+  if [ -d "$SERVICE_HOME_OLD" ]; then
+    cp -rvf "$SERVICE_HOME_OLD" "$SERVICE_HOME"
+  else
+    mkdir -p "${SERVICE_HOME}"
+  fi
+  rm -rvf "$SERVICE_HOME_OLDEST"
 fi
 cp -rvf $(find config -mindepth 1) "${SERVICE_HOME}"
-rm -rvf "$SERVICE_HOME_OLDEST"
-DATA_DIR="${SERVICE_HOME}" LOCAL_IP="${SERVICE_HOST_IP}" docker-compose --no-ansi up --force-recreate -d
+VERSION=${VERSION_ABSOLUTE} DATA_DIR="${SERVICE_HOME}" LOCAL_IP="${SERVICE_HOST_IP}" docker-compose --no-ansi up --force-recreate -d
