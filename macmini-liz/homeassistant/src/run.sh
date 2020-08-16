ec8b2b42dc150a4101c4e9ff62c4cf4a1d5a36f1
Graham Gear <graham.gear@gmail.com>
2020-08-16 13:38:53 +0800

    Update asystem-10.100.1092

--- macmini-liz/homeassistant/src/run.sh
+++ macmini-liz/homeassistant/src/run.sh
@@ -1,12 +1,15 @@
 #!/bin/sh
 
 SERVICE_NAME=homeassistant
-SERVICE_HOME=/home/asystem/$SERVICE_NAME
-SERVICE_INSTALL=/var/lib/asystem/install/$(hostname)/$SERVICE_NAME/${VERSION_ABSOLUTE}
+SERVICE_HOME=/home/asystem/${SERVICE_NAME}/${VERSION_ABSOLUTE}
+SERVICE_INSTALL=/var/lib/asystem/install/$(hostname)/${SERVICE_NAME}/${VERSION_ABSOLUTE}
 SERVICE_HOST_IP=$(/usr/sbin/ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep '192.168.1')
 
-cd "$SERVICE_INSTALL" || exit
-mkdir -p "$SERVICE_HOME"
-cp -rvf $(find config -mindepth 1) "$SERVICE_HOME"
-docker stop "$SERVICE_NAME" 2>&1 >/dev/null && docker wait "$SERVICE_NAME" 2>&1 >/dev/null && docker system prune --volumes -f
-DATA_DIR="$SERVICE_HOME" LOCAL_IP="$SERVICE_HOST_IP" docker-compose --no-ansi up --force-recreate -d
+cd "${SERVICE_INSTALL}" || exit
+
+#ls -dt ${SERVICE_HOME}/../*/ | tail -n -$(($(ls -dt ${SERVICE_HOME}/../*/ | wc -l) - 2))
+
+mkdir -p "${SERVICE_HOME}"
+cp -rvf $(find config -mindepth 1) "${SERVICE_HOME}"
+docker stop "${SERVICE_NAME}" 2>&1 >/dev/null && docker wait "${SERVICE_NAME}" 2>&1 >/dev/null && docker system prune --volumes -f
+DATA_DIR="${SERVICE_HOME}" LOCAL_IP="${SERVICE_HOST_IP}" docker-compose --no-ansi up --force-recreate -d
