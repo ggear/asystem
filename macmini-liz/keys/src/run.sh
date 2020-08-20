de0c239969b254fbfdb84c59471f66c7e6e371d4
Graham Gear <graham.gear@gmail.com>
2020-08-20 20:38:43 +0800

    Update asystem-10.100.1195

--- macmini-liz/keys/src/run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-SERVICE_INSTALL=/var/lib/asystem/install/$(hostname)/${SERVICE_NAME}/${VERSION_ABSOLUTE}
-
-cd "${SERVICE_INSTALL}" || exit
-cp -rvf ./config/id_rsa.pub /root/.ssh
-cp -rvf ./config/.id_rsa /root/.ssh/id_rsa
-rm -rfv ./config/.id_rsa
