9ed1f904db142ae24f80033acbd4f3b758f1886a
Graham Gear <graham.gear@gmail.com>
2020-08-20 20:31:40 +0800

    Update asystem-10.100.1191

--- macmini-liz/keys/src/run.sh
+++ macmini-liz/keys/src/run.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
 #TODO: Copy keys to /home/root/.ssh && /home/graham, chmod/chown them
+
+cp -rvf ./config/id_rsa.pub /root/.ssh
+cp -rvf ./config/.id_rsa /root/.ssh/id_rsa
+rm -rfv ./config/.id_rsa
