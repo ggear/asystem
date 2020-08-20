c273310bbf3b3945f2679afd6ccec7f6e88c2859
Graham Gear <graham.gear@gmail.com>
2020-08-20 20:32:51 +0800

    Update asystem-10.100.1192

--- macmini-liz/keys/src/run.sh
+++ macmini-liz/keys/src/run.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
-#TODO: Copy keys to /home/root/.ssh && /home/graham, chmod/chown them
+SERVICE_INSTALL=/var/lib/asystem/install/$(hostname)/${SERVICE_NAME}/${VERSION_ABSOLUTE}
 
+cd "${SERVICE_INSTALL}" || exit
 cp -rvf ./config/id_rsa.pub /root/.ssh
 cp -rvf ./config/.id_rsa /root/.ssh/id_rsa
 rm -rfv ./config/.id_rsa
