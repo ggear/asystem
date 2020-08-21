2d4945a51d965aa48e5b2b8b4e125d881ef8c52a
Graham Gear <gearg1@south32.net>
2020-08-21 15:15:02 +0800

    Add place certficate distribution place holders

---
+++ macmini-liz/letsencrypt/run_pre.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+SERVICE_HOME=/home/asystem/${SERVICE_NAME}/${VERSION_ABSOLUTE}
+SERVICE_INSTALL=/var/lib/asystem/install/$(hostname)/${SERVICE_NAME}/${VERSION_ABSOLUTE}
+
+cd "${SERVICE_INSTALL}" || exit
+
+#TODO: Install service
