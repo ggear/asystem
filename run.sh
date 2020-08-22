cb83642ddf407677834072ea57ce3fb33281ca15
Graham Gear <graham.gear@gmail.com>
2020-08-22 17:09:49 +0800

    Update asystem-10.100.1228

--- run.sh
+++ run.sh
@@ -14,8 +14,8 @@ SERVICE_HOST_IP=$(/usr/sbin/ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]
 cd "${SERVICE_INSTALL}" || exit
 [ -f "./run_pre.sh" ] && chmod +x ./run_pre.sh && ./run_pre.sh
 [ -f "${SERVICE_NAME}-${VERSION_ABSOLUTE}.tar.gz" ] && docker image load -i ${SERVICE_NAME}-${VERSION_ABSOLUTE}.tar.gz
-docker stop "${SERVICE_NAME}" 2>&1 >/dev/null
-docker wait "${SERVICE_NAME}" 2>&1 >/dev/null
+docker stop "${SERVICE_NAME}" >/dev/null 2>&1
+docker wait "${SERVICE_NAME}" >/dev/null 2>&1
 docker system prune --volumes -f 2>&1 >/dev/null
 if [ ! -d "$SERVICE_HOME" ]; then
   if [ -d "$SERVICE_HOME_OLD" ]; then
@@ -26,16 +26,14 @@ if [ ! -d "$SERVICE_HOME" ]; then
   rm -rvf "$SERVICE_HOME_OLDEST"
 fi
 [ "$(ls -A config | wc -l)" -gt 0 ] && cp -rvf $(find config -mindepth 1) "${SERVICE_HOME}"
-if [ ! -f ".env" ]; then
-  cat <<EOF >>.env
+cat <<EOF >>.env
 
 RESTART=always
 VERSION=${VERSION_ABSOLUTE}
 DATA_DIR=${SERVICE_HOME}
 LOCAL_IP=${SERVICE_HOST_IP}
 EOF
-  [ -f ".config/.profile" ] && sed 's/export //g' config/.profile >>.env
-fi
+[ -f ".config/.profile" ] && sed 's/export //g' config/.profile >>.env
 docker-compose --no-ansi up --force-recreate -d
 [ -f "./run_post.sh" ] && chmod +x ./run_post.sh && ./run_post.sh
 docker ps -f name="${SERVICE_NAME}"
