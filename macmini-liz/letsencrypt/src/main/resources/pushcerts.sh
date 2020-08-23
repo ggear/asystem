f8fd882b3696c85d1bac507c55982ebf8c2e0a1e
Graham Gear <graham.gear@gmail.com>
2020-08-23 15:51:36 +0800

    Update asystem-10.100.1272

--- macmini-liz/letsencrypt/src/main/resources/pushcerts.sh
+++ macmini-liz/letsencrypt/src/main/resources/pushcerts.sh
@@ -4,6 +4,8 @@ SERVICE_HOME=/home/asystem/${SERVICE_NAME}/${VERSION_ABSOLUTE}
 
 #TODO: while true, fswatch on .last_updated, then copy into git/anode, local/anode, udm-rack/unifi
 
+fswatch ${SERVICE_HOME}/letsencrypt/live/janeandgraham.com/privkey.pem
+
 # git/anode
 # cat /Users/graham/_/dev/asystem/macmini-liz/letsencrypt/target/letsencrypt/live/janeandgraham.com/fullchain.pem /Users/graham/_/dev/asystem/macmini-liz/letsencrypt/target/letsencrypt/live/janeandgraham.com/privkey.pem > /Users/graham/_/dev/asystem/macmini-liz/anode/src/main/resources/config/.pem
 
