9d300070bfa1cc96833179d3f1c1a3a70b1923f4
Graham Gear <graham.gear@gmail.com>
2020-08-24 09:47:44 +0800

    Update asystem-10.100.1288

--- macmini-liz/letsencrypt/run_pre.sh
+++ macmini-liz/letsencrypt/run_pre.sh
@@ -6,7 +6,7 @@ SERVICE_INSTALL=/var/lib/asystem/install/$(hostname)/${SERVICE_NAME}/${VERSION_A
 
 cd "${SERVICE_INSTALL}" || exit
 chmod +x "./pushcerts.sh"
-cp -rvfp "./pushcerts.service" /etc/systemd/system
-systemctl daemon-reload
-systemctl enable pushcerts.service
-systemctl restart pushcerts.service
+#cp -rvfp "./pushcerts.service" /etc/systemd/system
+#systemctl daemon-reload
+#systemctl enable pushcerts.service
+#systemctl restart pushcerts.service
