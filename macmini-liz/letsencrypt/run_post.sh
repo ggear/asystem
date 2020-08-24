d2db2ec2b600160937b00ef4f2ed28d4afb0f412
Graham Gear <graham.gear@gmail.com>
2020-08-24 17:10:43 +0800

    Update asystem-10.100.1303

--- macmini-liz/letsencrypt/run_post.sh
+++ macmini-liz/letsencrypt/run_post.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
+SERVICE_HOME=/home/asystem/${SERVICE_NAME}/${VERSION_ABSOLUTE}
 SERVICE_INSTALL=/var/lib/asystem/install/$(hostname)/${SERVICE_NAME}/${VERSION_ABSOLUTE}
 
 cd "${SERVICE_INSTALL}" || exit
 chmod +x "./pushcerts.sh"
+cp -rvfp "./pushcerts.sh" "${SERVICE_HOME}"
 cp -rvfp "./pushcerts.service" /etc/systemd/system
 systemctl daemon-reload
 systemctl enable pushcerts.service
