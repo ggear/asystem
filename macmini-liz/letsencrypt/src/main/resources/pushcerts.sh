38feb2cb822b89248b2cdd54658a3a288f136bf5
Graham Gear <graham.gear@gmail.com>
2020-08-25 15:44:51 +0800

    Update asystem-10.100.1317

--- macmini-liz/letsencrypt/src/main/resources/pushcerts.sh
+++ macmini-liz/letsencrypt/src/main/resources/pushcerts.sh
@@ -5,11 +5,11 @@ SERVICE_INSTALL=/var/lib/asystem/install/$(hostname)/${SERVICE_NAME}/${VERSION_A
 
 cd "${SERVICE_HOME}" || exit
 [ ! -d "./certificates" ] && [ -d "./letsencrypt/archive/janeandgraham.com" ] && cp -rvfpL letsencrypt/live/janeandgraham.com certificates && logger -t pushcerts "Cached existing certificates"
-[ ! -d "./certificates" ] && mkdir "./certificates" && touch "./certificates/privkey.pem" && logger -t pushcerts "Cached null certificates"
 while :; do
   if [ $(fswatch -1 "${SERVICE_HOME}/letsencrypt/live/janeandgraham.com" | wc -l) -gt 0 ]; then
     sleep 5
-    if [ ! $(cmp ./certificates/privkey.pem ./letsencrypt/live/janeandgraham.com/privkey.pem >/dev/null) ]; then
+    [ ! -d "./certificates" ] && mkdir "./certificates" && touch "./certificates/privkey.pem"
+    if [ -f ./letsencrypt/live/janeandgraham.com/privkey.pem ] && [ ! $(cmp ./certificates/privkey.pem ./letsencrypt/live/janeandgraham.com/privkey.pem >/dev/null) ]; then
       cp -rvfpL letsencrypt/live/janeandgraham.com/* certificates &&
         cat ./certificates/fullchain.pem ./certificates/privkey.pem >./certificates/fullchain_privkey.pem &&
         logger -t pushcerts "Cached new certificates"
