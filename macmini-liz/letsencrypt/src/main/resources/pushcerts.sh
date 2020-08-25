d3998de999a608dfe06c04de7042fb1a51012d90
Graham Gear <graham.gear@gmail.com>
2020-08-25 15:29:59 +0800

    Update asystem-10.100.1315

--- macmini-liz/letsencrypt/src/main/resources/pushcerts.sh
+++ macmini-liz/letsencrypt/src/main/resources/pushcerts.sh
@@ -7,7 +7,7 @@ cd "${SERVICE_HOME}" || exit
 [ ! -d "./certificates" ] && [ -d "./letsencrypt/archive/janeandgraham.com" ] && cp -rvfpL letsencrypt/live/janeandgraham.com certificates && logger -t pushcerts "Cached existing certificates"
 [ ! -d "./certificates" ] && mkdir "./certificates" && touch "./certificates/privkey.pem" && logger -t pushcerts "Cached null certificates"
 while :; do
-  if [ $(fswatch -1 "${SERVICE_HOME}/letsencrypt/live/janeandgraham.com") -gt 0 ]; then
+  if [ $(fswatch -1 "${SERVICE_HOME}/letsencrypt/live/janeandgraham.com" | wc -l) -gt 0 ]; then
     sleep 5
     if [ ! $(cmp ./certificates/privkey.pem ./letsencrypt/live/janeandgraham.com/privkey.pem >/dev/null) ]; then
       cp -rvfpL letsencrypt/live/janeandgraham.com/* certificates &&
