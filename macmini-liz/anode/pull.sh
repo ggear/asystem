afccf5a858ff208484fdceb40c0092537cd9e963
Graham Gear <graham.gear@gmail.com>
2020-09-01 14:09:48 +0800

    Update asystem-10.100.1336

--- macmini-liz/anode/pull.sh
+++ macmini-liz/anode/pull.sh
@@ -2,4 +2,4 @@ HOST_ANODE=$(basename $(dirname $(find . -type d -mindepth 1 -maxdepth 2 ! -path
 HOST_LETSENCRYPT=$(basename $(dirname $(find . -type d -mindepth 1 -maxdepth 2 ! -path '/*/.*' | grep letsencrypt)))
 DIR_LETSENCRYPT=$(sshpass -f /Users/graham/.ssh/.password ssh -q "root@${HOST_LETSENCRYPT}" 'find /home/asystem/letsencrypt -maxdepth 1 -mindepth 1 2>/dev/null | sort | tail -n 1')
 sshpass -f /Users/graham/.ssh/.password scp -qpr "root@${HOST_LETSENCRYPT}:${DIR_LETSENCRYPT}/certificates/fullchain_privkey.pem" "${HOST_ANODE}/anode/src/main/resources/config/.pem" 2>/dev/null
-echo "Pulled latest anode certificates"
+echo "Pulled latest anode certificate"
