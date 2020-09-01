d4e66884c44dd106632ad9e9fdd66f05a7f6a182
Graham Gear <graham.gear@gmail.com>
2020-09-01 14:45:00 +0800

    Update asystem-10.100.1339

--- macmini-liz/homeassistant/pull.sh
+++ macmini-liz/homeassistant/pull.sh
@@ -3,4 +3,4 @@ HOST_LETSENCRYPT=$(basename $(dirname $(find . -type d -mindepth 1 -maxdepth 2 !
 DIR_LETSENCRYPT=$(sshpass -f /Users/graham/.ssh/.password ssh -q "root@${HOST_LETSENCRYPT}" 'find /home/asystem/letsencrypt -maxdepth 1 -mindepth 1 2>/dev/null | sort | tail -n 1')
 sshpass -f /Users/graham/.ssh/.password scp -qpr "root@${HOST_LETSENCRYPT}:${DIR_LETSENCRYPT}/certificates/privkey.pem" "${HOST_HOMEASSISTANT}/homeassistant/src/main/resources/config/.pem" 2>/dev/null
 sshpass -f /Users/graham/.ssh/.password scp -qpr "root@${HOST_LETSENCRYPT}:${DIR_LETSENCRYPT}/certificates/fullchain.pem" "${HOST_HOMEASSISTANT}/homeassistant/src/main/resources/config/certificate.pem" 2>/dev/null
-echo "Pulled latest homeassistant certificate"
+echo "Pulled latest home assistant certificate"
