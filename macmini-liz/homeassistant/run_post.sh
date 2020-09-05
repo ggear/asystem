4edb7d9b9c2c5559dfc9e69d939a6fcdd3d72cec
Graham Gear <graham.gear@gmail.com>
2020-09-05 14:52:02 +0800

    Update asystem-10.100.1393

--- macmini-liz/homeassistant/run_post.sh
+++ macmini-liz/homeassistant/run_post.sh
@@ -13,11 +13,11 @@ SERVICE_INSTALL=/var/lib/asystem/install/$(hostname)/${SERVICE_NAME}/${VERSION_A
 #
 
 cd "${SERVICE_INSTALL}" || exit
-if [ $(docker exec -it homeassistant /bin/bash -c "[ ! -d /config/custom_components/hacs ]") ]; then
+if [ $(docker exec -it homeassistant /bin/bash -c "ls /config/custom_components/hacs 2>/dev/null" | wc -l) -eq 0 ]; then
   docker exec -it homeassistant /bin/bash -c "curl -sfSL https://hacs.xyz/install | bash -"
   docker-compose --no-ansi up --force-recreate -d
 fi
-if [ $(docker exec -it homeassistant /bin/bash -c "[ ! -d /config/www/icons/weather_icons ]") ]; then
+if [ $(docker exec -it homeassistant /bin/bash -c "ls /config/www/icons/weather_icons 2>/dev/null" | wc -l) -eq 0 ]; then
   docker exec -it homeassistant /bin/bash -c "mkdir -p /config/www/icons/weather_icons && curl -o /tmp/icons.zip https://raw.githubusercontent.com/DavidFW1960/bom-weather-card/master/weather_icons.zip && unzip /tmp/icons.zip -d /config/www/icons/weather_icons && rm -rf /tmp/icons.zip"
   docker-compose --no-ansi up --force-recreate -d
 fi
