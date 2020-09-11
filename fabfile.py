932a44acae8a5c5165f70ae911b296bddefcf58a
Graham Gear <graham.gear@gmail.com>
2020-09-11 12:35:54 +0800

    Update asystem-10.100.1479

--- fabfile.py
+++ fabfile.py
@@ -382,7 +382,7 @@ def _ssh_pass(context, host):
 
 def _get_hosts(context, module):
 
-    print(module.split("/")[0])
+    print(module.split("/")[0].split("_"))
 
     return module.split("/")[0].split("_")
 
