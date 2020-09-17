8a86de110945d7e8c766cd6355286e7ddd284d05
Graham Gear <graham.gear@gmail.com>
2020-09-17 14:49:30 +0800

    Update asystem-10.100.1519

--- macmini-liz/weewx/src/main/resources/entrypoint.sh
+++ macmini-liz/weewx/src/main/resources/entrypoint.sh
@@ -4,6 +4,9 @@ set -o nounset
 set -o errexit
 
 service rsyslog start
+
+[ ! -f /data/html/index.html ] && mkdir -p /data/html && touch /data/html/index.html
 service apache2 start
 
+[ ! -f /data/weewx.conf ] && cp -f /home/weewx/weewx.conf /data
 /home/weewx/bin/weewxd "$@"
