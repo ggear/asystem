fdde8fd013ba80caca51a62da7683e5cc3e6aacb
Graham Gear <graham.gear@gmail.com>
2020-09-27 17:01:46 +0800

    Update asystem-10.100.1538

--- macmini-liz/grafana/run_post.sh
+++ macmini-liz/grafana/run_post.sh
@@ -4,15 +4,8 @@ SERVICE_HOME=/home/asystem/${SERVICE_NAME}/${VERSION_ABSOLUTE}
 SERVICE_INSTALL=/var/lib/asystem/install/$(hostname)/${SERVICE_NAME}/${VERSION_ABSOLUTE}
 
 cd "${SERVICE_INSTALL}" || exit
-
-
 . config/.profile
-
-export GRAFANA_URL=http://${GRAFANA_USER}:${GRAFANA_KEY}@macmini-liz:3000
-export DASHBOARD_ASYSTEM_UID=0fcqnVOGz
-
 cd config/grizzly
 make dev
-
 cd ../grafonnet-lib
-./../grizzly/grr apply ./../dashboards_all.jsonnet
+GRAFANA_URL=http://${GRAFANA_USER}:${GRAFANA_KEY}@macmini-liz:3000 ./../grizzly/grr apply ./../dashboards_all.jsonnet
