b9bddbf68d98a334a9f86751aff0171ae3d0a234
Graham Gear <graham.gear@gmail.com>
2020-09-27 19:05:33 +0800

    Update asystem-10.100.1540

--- macmini-liz/grafana/run_post.sh
+++ macmini-liz/grafana/run_post.sh
@@ -7,13 +7,15 @@ cd "${SERVICE_INSTALL}" || exit
 . config/.profile
 cd config/grizzly
 make dev
-curl -i -XPOST --silent -H "Accept: application/json" -H "Content-Type: application/json" -H "Authorization: ${GRAFANA_KEY}" "http://localhost:3000/api/datasources" -d '
-{
-  "name": "influxdb",
-  "type": "influxdb",
-  "url":"http://192.168.1.10:9999",
-  "organisation":"home",
-  "token":"${INFLUXDB_TOKEN}",
+curl -i -XPOST --silent -H "Accept: application/json" -H "Content-Type: application/json" "http://${GRAFANA_USER}:${GRAFANA_KEY}@localhost:3000/api/datasources" -d '{
+  "name": "InfluxDB",
+  "type": "grafana-influxdb-flux-datasource",
+  "url": "http://macmini-liz:9999",
+  "access": "proxy",
+  "queryLanguage": "Flux",
+  "organization": "home",
+  "defaultBucket": "asystem",
+  "token": "${INFLUXDB_TOKEN}"
 }'
 cd ../grafonnet-lib
 GRAFANA_URL=http://${GRAFANA_USER}:${GRAFANA_KEY}@macmini-liz:3000 ./../grizzly/grr apply ./../dashboards_all.jsonnet
