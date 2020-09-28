dadc3bf7fc4b37e378b8bd72537a60612ee5a1c8
Graham Gear <graham.gear@gmail.com>
2020-09-28 08:32:56 +0800

    Update asystem-10.100.1544

--- macmini-liz/grafana/run_post.sh
+++ macmini-liz/grafana/run_post.sh
@@ -7,15 +7,13 @@ cd "${SERVICE_INSTALL}" || exit
 . config/.profile
 cd config/grizzly
 make dev
-
-#TODO: Does not currently configure for queryLanguage, have to flip to it in the UI
 curl -i -XPOST --silent -H "Accept: application/json" -H "Content-Type: application/json" "http://${GRAFANA_USER}:${GRAFANA_KEY}@localhost:3000/api/datasources" -d '{
   "name": "InfluxDB",
   "type": "influxdb",
   "url": "http://macmini-liz:9999",
   "access": "proxy",
-  "queryLanguage": "Flux",
   "jsonData": {
+    "version": "Flux",
     "organization": "home",
     "defaultBucket": "asystem"
   },
@@ -26,6 +24,5 @@ curl -i -XPOST --silent -H "Accept: application/json" -H "Content-Type: applicat
     "token": true
   }
 }'
-
 cd ../grafonnet-lib
 GRAFANA_URL=http://${GRAFANA_USER}:${GRAFANA_KEY}@macmini-liz:3000 ./../grizzly/grr apply ./../dashboards_all.jsonnet
