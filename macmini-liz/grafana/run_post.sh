23d09c3432ffac7a22c17e4ccaa1ef968c147971
Graham Gear <graham.gear@gmail.com>
2020-10-04 19:41:11 +0800

    Update asystem-10.100.1569

--- macmini-liz/grafana/run_post.sh
+++ macmini-liz/grafana/run_post.sh
@@ -7,22 +7,25 @@ cd "${SERVICE_INSTALL}" || exit
 . config/.profile
 cd config/grizzly
 make dev
-curl -i -XPOST --silent -H "Accept: application/json" -H "Content-Type: application/json" "http://${GRAFANA_USER}:${GRAFANA_KEY}@localhost:3000/api/datasources" -d '{
-  "name": "InfluxDB",
-  "type": "influxdb",
-  "url": "http://macmini-liz:8086",
-  "access": "proxy",
-  "jsonData": {
-    "version": "Flux",
-    "organization": "home",
-    "defaultBucket": "asystem"
-  },
-  "secureJsonData": {
-    "token": "${INFLUXDB_TOKEN}"
-  },
-  "secureJsonFields": {
-    "token": true
-  }
-}'
+curl -i -XPOST --silent "http://${GRAFANA_USER}:${GRAFANA_KEY}@localhost:3000/api/datasources" \
+  -H "Accept: application/json" \
+  -H "Content-Type: application/json"
+  -d '{
+        "name": "InfluxDB",
+        "type": "influxdb",
+        "url": "http://macmini-liz:8086",
+        "access": "proxy",
+        "jsonData": {
+          "version": "Flux",
+          "organization": "home",
+          "defaultBucket": "asystem"
+        },
+        "secureJsonData": {
+          "token": "${INFLUXDB_TOKEN}"
+        },
+        "secureJsonFields": {
+          "token": true
+        }
+      }'
 cd ../grafonnet-lib
 GRAFANA_URL=http://${GRAFANA_USER}:${GRAFANA_KEY}@macmini-liz:3000 ./../grizzly/grr apply ./../dashboards_all.jsonnet
