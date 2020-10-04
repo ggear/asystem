899f73463d6a25dc2afb5423edbe75c37e131869
Graham Gear <graham.gear@gmail.com>
2020-10-04 19:41:11 +0800

    Update asystem-10.100.1569

---
+++ macmini-liz/influxdb/run_post.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+SERVICE_HOME=/home/asystem/${SERVICE_NAME}/${VERSION_ABSOLUTE}
+SERVICE_INSTALL=/var/lib/asystem/install/$(hostname)/${SERVICE_NAME}/${VERSION_ABSOLUTE}
+
+#cd "${SERVICE_INSTALL}" || exit
+
+#. config/.profile
+. src/main/resources/config/.profile
+
+# TODO: Create buckets (host/asystem) if not exist, get IDs, create DBRPS in this script
+# TODO: Create both INfluxDB1 and 2 Grafana datasources in the grafan post script
+
+curl -XPOST http://localhost:8086/api/v2/dbrps \
+  -H "Authorization: Token ${INFLUXDB_TOKEN}" \
+  -H 'Content-type: application/json' \
+  -d '{
+        "organization": "home",
+        "bucket_id": "ace73b34f9f08175",
+        "database": "asystem",
+        "retention_policy": "autogen",
+        "default": true
+      }'
