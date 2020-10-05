e8e8e478d950e4717353ac2d66685253732543e3
Graham Gear <graham.gear@gmail.com>
2020-10-05 20:21:37 +0800

    Update asystem-10.100.1608

--- macmini-liz/homeassistant/deploy.sh
+++ macmini-liz/homeassistant/deploy.sh
@@ -5,4 +5,4 @@ HOME=$(ssh root@${HOST} "find /home/asystem/homeassistant -maxdepth 1 -mindepth 
 INSTALL=$(ssh root@${HOST} "find /var/lib/asystem/install/${HOST}/homeassistant -maxdepth 1 -mindepth 1 2>/dev/null | sort | tail -n 1")
 
 scp -r $PWD/src/main/resources/config/* root@${HOST}:${HOME}
-ssh root@${HOST} "cd ${INSTALL} && docker-compose restart" 2>/dev/null
+ssh root@${HOST} "cd ${INSTALL} && docker-compose --compatibility restart" 2>/dev/null
