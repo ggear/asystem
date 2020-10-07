7307569f52563375a63687f8c0f1e541135cd325
Graham Gear <graham.gear@gmail.com>
2020-10-07 09:24:50 +0800

    Update asystem-10.100.1609

--- macmini-liz/influxdb/run_post.sh
+++ macmini-liz/influxdb/run_post.sh
@@ -3,20 +3,17 @@
 SERVICE_HOME=/home/asystem/${SERVICE_NAME}/${VERSION_ABSOLUTE}
 SERVICE_INSTALL=/var/lib/asystem/install/$(hostname)/${SERVICE_NAME}/${VERSION_ABSOLUTE}
 
-#cd "${SERVICE_INSTALL}" || exit
-
-#. config/.profile
-. src/main/resources/config/.profile
+cd "${SERVICE_INSTALL}" || exit
+. config/.profile
 
 # TODO: Create buckets (host/asystem) if not exist, get IDs, create DBRPS in this script
-
-curl -XPOST http://localhost:8086/api/v2/dbrps \
-  -H "Authorization: Token ${INFLUXDB_TOKEN}" \
-  -H 'Content-type: application/json' \
-  -d '{
-        "organization": "home",
-        "bucket_id": "ace73b34f9f08175",
-        "database": "asystem",
-        "retention_policy": "autogen",
-        "default": true
-      }'
+#curl -XPOST http://localhost:8086/api/v2/dbrps \
+#  -H "Authorization: Token ${INFLUXDB_TOKEN}" \
+#  -H 'Content-type: application/json' \
+#  -d '{
+#        "organization": "home",
+#        "bucket_id": "ace73b34f9f08175",
+#        "database": "asystem",
+#        "retention_policy": "autogen",
+#        "default": true
+#      }'
