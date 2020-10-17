5fb1ba55fc5c3f4f37884f87740b855e66db764e
Graham Gear <graham.gear@gmail.com>
2020-10-17 09:45:04 +0800

    Update asystem-10.100.1633

---
+++ macmini-liz/letsencrypt/src/main/resources/entrypoint.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+set -e
+
+if [ -n "$TIMEZONE" ]
+then
+  echo "Waiting for DNS"
+  ping -c1 -W60 google.com || ping -c1 -W60 www.google.com
+  apk add --no-cache tzdata
+  if [ -f /usr/share/zoneinfo/"$TIMEZONE" ]
+  then
+    echo "Setting timezone to $TIMEZONE"
+    cp /usr/share/zoneinfo/"$TIMEZONE" /etc/localtime
+    echo "$TIMEZONE" > /etc/timezone
+  else
+    echo "$TIMEZONE does not exist"
+  fi
+  apk del tzdata
+fi
+
+# Inhibit config.yml creation if legacy config is detected
+# to avoid skipping this legacy config processing.
+if [ ! -f "/etc/letsencrypt/domains.conf" ]; then
+  if [ ! -f "${CONFIG_PATH}" ]; then
+      echo "draft: true" >> "${CONFIG_PATH}"
+  fi
+fi
+
+export PATH="${HOME}/.local/bin:${PATH}"
+
+nohup telegraf &
+
+exec dnsrobocert -c "${CONFIG_PATH}" -d "${CERTS_PATH}"
