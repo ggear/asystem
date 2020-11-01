fde9d24e3d0b13592da1c8f0e0fcbcdd929f4b1d
Graham Gear <graham.gear@gmail.com>
2020-11-01 13:59:57 +0800

    Update asystem-10.100.1669

--- macmini-nel/internet/src/main/python/internet.py
+++ macmini-nel/internet/src/main/python/internet.py
@@ -27,32 +27,52 @@
 # - Add configurable .profile location and pass into docker container
 # - Add unit tests against run codes, happy paths
 # - Develop grafana graphs
-# - Add a delay and repeat if we get a fail
-# - Add a DNS flush (or connect direct to root DNS server) for lookup
+# - Add a query to get public eth8 address
+# - Add certifcate tests
 
 from __future__ import print_function
 
+import sys
 import time
 from socket import gethostbyname
 
+from dns.resolver import Resolver
 from requests import post
 from speedtest import Speedtest
 
 HOST_HOME_NAME = "home.janeandgraham.com"
+
 HOST_SPEEDTEST_PING_IDS = ["6359", "4153"]
 HOST_SPEEDTEST_THROUGHPUT_IDS = ["2627", "30932", "7581"]
 
-PING_COUNT = 1
+
+RESOLVER_IPS = ["192.168.1.1", "162.159.44.190", "1.1.1.1", "8.8.8.8"]
+
+HOST_INTERNET_LOCATION = "darlington"
+HOST_INTERNET_INTERFACE_ID = "udm-rack-eth8"
+
+PING_COUNT = 3
+PING_SLEEP_SECONDS = 1
 
 RUN_CODE_SUCCESS = 0
 RUN_CODE_FAIL_CONFIG = 1
 RUN_CODE_FAIL_NETWORK = 2
 
-FORMAT_PING = "internet,metric=ping,location={},host={} ping_min_ms={},ping_max_ms={},ping_med_ms={},pings_lost={},run_code={},run_ms={} {}"
-FORMAT_UPLOAD = "internet,metric=upload,location={},host={} upload_mbps={},upload_bytes={},run_code={},run_ms={} {}"
-FORMAT_DOWNLOAD = "internet,metric=download,location={},host={} download_mbps={},download_bytes={},run_code={},run_ms={} {}"
-FORMAT_LOOKUP = "internet,metric=lookup,location={},host={} ip={},run_code={},run_ms={} {}"
-FORMAT_UPTIME = "internet,metric=uptime,location={},host={} uptime_s={},run_code={},run_ms={} {}"
+FORMAT_PING = \
+    "internet,metric=ping,host_id={},host_location={},host_name={} " \
+    "ping_min_ms={},ping_max_ms={},ping_med_ms={},pings_lost={},run_code={},run_ms={} {}"
+FORMAT_UPLOAD = \
+    "internet,metric=upload,host_id={},host_location={},host_name={} " \
+    "upload_mbps={},upload_bytes={},run_code={},run_ms={} {}"
+FORMAT_DOWNLOAD = \
+    "internet,metric=download,host_id={},host_location={},host_name={} " \
+    "download_mbps={},download_bytes={},run_code={},run_ms={} {}"
+FORMAT_LOOKUP = \
+    "internet,metric=lookup,host_id={},host_location={},host_name={},resolver_ip={} " \
+    "ip={},run_code={},run_ms={} {}"
+FORMAT_UPTIME = \
+    "internet,metric=uptime,host_id={},host_location={},host_name={} " \
+    "uptime_s={},metrics_failed={},run_code={},run_ms={} {}"
 
 
 def load_profile(profile_file):
@@ -108,28 +128,39 @@ from(bucket: "hosts")
 def ping():
     run_code = RUN_CODE_FAIL_CONFIG
     for host_speedtest_id in HOST_SPEEDTEST_THROUGHPUT_IDS + HOST_SPEEDTEST_PING_IDS:
-        time_start = time_ms()
         pings = []
+        host_speedtest = None
+        time_start = time_ms()
         for i in xrange(PING_COUNT):
+            run_code_iteration = RUN_CODE_FAIL_NETWORK
             try:
                 speedtest = Speedtest()
                 speedtest.get_servers([host_speedtest_id])
-                speedtest.get_best_server()
+                host_speedtest = speedtest.best
                 speedtest_results = speedtest.results.dict()
                 pings.append(speedtest_results["ping"])
-            except:
-                None
-        run_code_host = RUN_CODE_SUCCESS if len(pings) > 0 else RUN_CODE_FAIL_NETWORK
-        if run_code > 0 and run_code_host == RUN_CODE_SUCCESS:
+                if i < PING_COUNT - 1:
+                    time.sleep(PING_SLEEP_SECONDS)
+                    time_start += PING_SLEEP_SECONDS * 1000
+            except Exception as exception:
+                print("Error processing speedtest ping [{}{}]"
+                      .format(type(exception).__name__, "" if str(exception) == "" else ":{}".format(exception), file=sys.stderr))
+                run_code_iteration = RUN_CODE_FAIL_CONFIG
+        run_code_iteration = RUN_CODE_SUCCESS \
+            if (len(pings) > 0 and max(pings) > 0) else run_code_iteration
+        if run_code > RUN_CODE_SUCCESS and run_code_iteration == RUN_CODE_SUCCESS:
             run_code = RUN_CODE_SUCCESS
+        else:
+            run_code = run_code_iteration
         print(FORMAT_PING.format(
-            speedtest.best["name"].lower(),
-            speedtest.best["host"].split(":")[0],
+            "speedtest-" + host_speedtest_id,
+            host_speedtest["name"].lower() if host_speedtest is not None else "",
+            host_speedtest["host"].split(":")[0] if host_speedtest is not None else "",
             min(pings) if len(pings) > 0 else "",
             max(pings) if len(pings) > 0 else "",
             med(pings) if len(pings) > 0 else "",
             PING_COUNT - len(pings),
-            run_code_host,
+            run_code_iteration,
             time_ms() - time_start,
             time_ns()))
     return run_code
@@ -138,20 +169,34 @@ def ping():
 def upload():
     run_code = RUN_CODE_FAIL_CONFIG
     for host_speedtest_id in HOST_SPEEDTEST_THROUGHPUT_IDS:
+        host_speedtest = None
+        results_speedtest = None
+        run_code_iteration = RUN_CODE_FAIL_NETWORK
         time_start = time_ms()
-        speedtest = Speedtest()
-        speedtest.get_servers([host_speedtest_id])
-        speedtest.upload()
-        speedtest_results = speedtest.results.dict()
-        run_code_host = RUN_CODE_SUCCESS if speedtest_results["upload"] > 0 else RUN_CODE_FAIL_NETWORK
-        if run_code > 0 and run_code_host == RUN_CODE_SUCCESS:
+        try:
+            speedtest = Speedtest()
+            speedtest.get_servers([host_speedtest_id])
+            host_speedtest = speedtest.best
+            speedtest.upload()
+            results_speedtest = speedtest.results.dict()
+        except Exception as exception:
+            print("Error processing speedtest upload [{}{}]"
+                  .format(type(exception).__name__, "" if str(exception) == "" else ":{}".format(exception), file=sys.stderr))
+            run_code_iteration = RUN_CODE_FAIL_CONFIG
+        run_code_iteration = RUN_CODE_SUCCESS \
+            if (results_speedtest is not None and "upload" in results_speedtest and results_speedtest["upload"] > 0) \
+            else run_code_iteration
+        if run_code > RUN_CODE_SUCCESS and run_code_iteration == RUN_CODE_SUCCESS:
             run_code = RUN_CODE_SUCCESS
+        else:
+            run_code = run_code_iteration
         print(FORMAT_UPLOAD.format(
-            speedtest.best["name"].lower(),
-            speedtest.best["host"].split(":")[0],
-            speedtest_results["upload"] / 8000000,
-            speedtest_results["bytes_sent"],
-            run_code_host,
+            "speedtest-" + host_speedtest_id,
+            host_speedtest["name"].lower() if host_speedtest is not None else "",
+            host_speedtest["host"].split(":")[0] if host_speedtest is not None else "",
+            results_speedtest["upload"] / 8000000 if results_speedtest is not None else "",
+            results_speedtest["bytes_sent"] if results_speedtest is not None else "",
+            run_code_iteration,
             time_ms() - time_start,
             time_ns()))
     return run_code
@@ -160,52 +205,108 @@ def upload():
 def download():
     run_code = RUN_CODE_FAIL_CONFIG
     for host_speedtest_id in HOST_SPEEDTEST_THROUGHPUT_IDS:
+        host_speedtest = None
+        results_speedtest = None
+        run_code_iteration = RUN_CODE_FAIL_NETWORK
         time_start = time_ms()
-        speedtest = Speedtest()
-        speedtest.get_servers([host_speedtest_id])
-        speedtest.download()
-        speedtest_results = speedtest.results.dict()
-        run_code_host = RUN_CODE_SUCCESS if speedtest_results["download"] > 0 else RUN_CODE_FAIL_NETWORK
-        if run_code > 0 and run_code_host == RUN_CODE_SUCCESS:
+        try:
+            speedtest = Speedtest()
+            speedtest.get_servers([host_speedtest_id])
+            host_speedtest = speedtest.best
+            speedtest.download()
+            results_speedtest = speedtest.results.dict()
+        except Exception as exception:
+            print("Error processing speedtest download [{}{}]"
+                  .format(type(exception).__name__, "" if str(exception) == "" else ":{}".format(exception), file=sys.stderr))
+            run_code_iteration = RUN_CODE_FAIL_CONFIG
+        run_code_iteration = RUN_CODE_SUCCESS \
+            if (results_speedtest is not None and "download" in results_speedtest and results_speedtest["download"] > 0) \
+            else run_code_iteration
+        if run_code > RUN_CODE_SUCCESS and run_code_iteration == RUN_CODE_SUCCESS:
             run_code = RUN_CODE_SUCCESS
-        print(FORMAT_DOWNLOAD.format(
-            speedtest.best["name"].lower(),
-            speedtest.best["host"].split(":")[0],
-            speedtest_results["download"] / 8000000,
-            speedtest_results["bytes_received"],
-            run_code_host,
+        else:
+            run_code = run_code_iteration
+        print(FORMAT_UPLOAD.format(
+            "speedtest-" + host_speedtest_id,
+            host_speedtest["name"].lower() if host_speedtest is not None else "",
+            host_speedtest["host"].split(":")[0] if host_speedtest is not None else "",
+            results_speedtest["download"] / 8000000 if results_speedtest is not None else "",
+            results_speedtest["bytes_received"] if results_speedtest is not None else "",
+            run_code_iteration,
             time_ms() - time_start,
             time_ns()))
     return run_code
 
 
 def lookup():
-    run_code = RUN_CODE_FAIL_CONFIG
+    run_responses = set()
+    run_response_count = 0
+    home_host_ip = None
+    run_code_iteration = RUN_CODE_FAIL_NETWORK
     time_start = time_ms()
-    home_host_ip = gethostbyname(HOST_HOME_NAME)
+    try:
+        home_host_ip = gethostbyname(HOST_HOME_NAME)
+    except Exception as exception:
+        print("Error processing DNS lookup [{}{}]"
+              .format(type(exception).__name__, "" if str(exception) == "" else ":{}".format(exception), file=sys.stderr))
     if home_host_ip is not None and home_host_ip != "":
-        run_code = RUN_CODE_SUCCESS
+        run_code_iteration = RUN_CODE_SUCCESS
+        run_response_count += 1
+        run_responses.add(home_host_ip)
     print(FORMAT_LOOKUP.format(
-        "darlington",
+        HOST_INTERNET_INTERFACE_ID,
+        HOST_INTERNET_LOCATION,
         HOST_HOME_NAME,
-        home_host_ip,
-        run_code,
+        "127.0.0.1",
+        home_host_ip if home_host_ip is not None else "",
+        run_code_iteration,
         time_ms() - time_start,
         time_ns()))
-    return run_code
+    for home_host_resolver_ip in RESOLVER_IPS:
+        home_host_response = None
+        run_code_iteration = RUN_CODE_FAIL_NETWORK
+        time_start = time_ms()
+        try:
+            home_host_resolver = Resolver()
+            home_host_resolver.nameservers = [home_host_resolver_ip]
+            home_host_responses = home_host_resolver.query(HOST_HOME_NAME, lifetime=2)
+            if home_host_responses is not None and len(home_host_responses) == 1:
+                home_host_response = home_host_responses[0]
+        except Exception as exception:
+            print("Error processing DNS lookup [{}{}]"
+                  .format(type(exception).__name__, "" if str(exception) == "" else ":{}".format(exception), file=sys.stderr))
+        if home_host_response is not None and home_host_response.address is not None and home_host_response.address != "":
+            run_code_iteration = RUN_CODE_SUCCESS
+            run_response_count += 1
+            run_responses.add(home_host_response.address)
+        print(FORMAT_LOOKUP.format(
+            HOST_INTERNET_INTERFACE_ID,
+            HOST_INTERNET_LOCATION,
+            HOST_HOME_NAME,
+            home_host_resolver_ip,
+            home_host_response.address \
+                if (home_host_response is not None and home_host_response.address is not None and home_host_response.address != "") else "",
+            run_code_iteration,
+            time_ms() - time_start,
+            time_ns()))
+    return RUN_CODE_SUCCESS if (run_response_count == len(RESOLVER_IPS) + 1 and len(run_responses) == 1) else RUN_CODE_FAIL_NETWORK
 
 
 if __name__ == "__main__":
-    time_start = time_ms()
-    run_code = RUN_CODE_SUCCESS
-    run_code += ping()
-    run_code += upload()
-    run_code += download()
-    run_code += lookup()
+    time_start_all = time_ms()
+    run_code_all = [
+        ping(),
+        upload(),
+        download(),
+        lookup(),
+    ]
+
     print(FORMAT_UPTIME.format(
-        "darlington",
+        HOST_INTERNET_INTERFACE_ID,
+        HOST_INTERNET_LOCATION,
         HOST_HOME_NAME,
         0,
-        run_code,
-        time_ms() - time_start,
+        len(run_code_all) - run_code_all.count(0),
+        0,
+        time_ms() - time_start_all,
         time_ns()))
