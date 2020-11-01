8e2df9f43ba56f8cb178e7e945be6f3f58605209
Graham Gear <graham.gear@gmail.com>
2020-11-01 19:18:49 +0800

    Update asystem-10.100.1673

--- macmini-nel/internet/src/main/python/internet.py
+++ macmini-nel/internet/src/main/python/internet.py
@@ -1,30 +1,6 @@
-# Ping: https://github.com/certator/pyping
-# Throughput: https://github.com/sivel/speedtest-cli/wiki
-# DNS: https://docs.python.org/2/library/socket.html#socket.gethostbyname
-# InfluxDB: https://docs.influxdata.com/influxdb/v2.0/tools/client-libraries/python/
-# Telegraf: https://github.com/influxdata/telegraf/tree/master/plugins/serializers/influx
-
-# internet,operation=ping,host=janeandgraham.com average_response_msec=23.066,ttl=63,maximum_response_msec=24.64,
-# minimum_response_msec=22.451,packets_received=5i,packets_transmitted=5i,percent_packet_loss=0,result_code=0i 1535747258000000000
-# internet,operation=ping,host=per1.speedtest.telstra.net average_response_msec=23.066,ttl=63,maximum_response_msec=24.64,
-# minimum_response_msec=22.451,packets_received=5i,packets_transmitted=5i,percent_packet_loss=0,result_code=0i 1535747258000000000
-
-# Introduce random element to drop frequency, repeat InfluxDB last() if not executed
-# internet,operation=upload,host=per1.speedtest.telstra.net ping_msec=23,transfer_bytes=100,rate_bytes_sec=100,result_code=0i
-# 1535747258000000000
-# internet,operation=download,host=per1.speedtest.telstra.net ping_msec=23,transfer_bytes=100,rate_bytes_sec=100,result_code=0i
-# 1535747258000000000
-
-# internet,operation=lookup,host=google.com response_msec=23,ip=10.0.0.1,result_code=0i 1535747258000000000
-# internet,operation=lookup,host=janeandgraham.com response_msec=23,ip=10.0.0.1,result_code=0i 1535747258000000000
-
-# Determined by if ping/download/upload are all successful, use InfluxDB last() to incrament uptime by timestamp diff, otherwise down and 0
-# internet,operation=uptime uptime_sec=90000,status=up 1535747258000000000
-
 # TODO:
 # - Test for all exceptions, wrong codes, pull ethernet, pull modem etc
-# - Add InfluxDB logic (throughput run, throughput echo/new, uptime calcualtion)
-# - Add configurable .profile location and pass into docker container
+# - Add InfluxDB logic (throughput echo or new)
 # - Add unit tests against run codes, happy paths
 # - Develop grafana graphs
 # - Add a query to get public eth8 address
@@ -32,19 +8,26 @@
 
 from __future__ import print_function
 
+import os
 import sys
 import time
+from datetime import datetime
 from socket import gethostbyname
 
+import pytz
+from dateutil.parser import parse
 from dns.resolver import Resolver
 from requests import post
 from speedtest import Speedtest
+
+DEFAULT_PROFILE_PATH = "../resources/config/.profile"
+DEFAULT_INFLUXDB_HOST = "192.168.1.10"
+DEFAULT_INFLUXDB_PORT = "8086"
 
 HOST_HOME_NAME = "home.janeandgraham.com"
 
 HOST_SPEEDTEST_PING_IDS = ["6359", "4153"]
 HOST_SPEEDTEST_THROUGHPUT_IDS = ["2627", "30932", "7581"]
-
 
 RESOLVER_IPS = ["192.168.1.1", "162.159.44.190", "1.1.1.1", "8.8.8.8"]
 
@@ -72,7 +55,17 @@ FORMAT_LOOKUP = \
     "ip=\"{}\",run_code={},run_ms={} {}"
 FORMAT_UPTIME = \
     "internet,metric=uptime,host_id={},host_location={},host_name={} " \
-    "uptime_s={},metrics_failed={},run_code={},run_ms={} {}"
+    "uptime_s={},metrics_suceeded={},metrics_failed={},run_code={},run_ms={} {}"
+
+QUERY_UPTIME_LAST = """
+from(bucket: "hosts")
+  |> range(start: -1h, stop: now())
+  |> filter(fn: (r) => r["_measurement"] == "internet")
+  |> filter(fn: (r) => r["_field"] == "uptime_s")
+  |> filter(fn: (r) => r["metric"] == "uptime")
+  |> keep(columns: ["_value", "_time"])
+  |> last()
+"""
 
 
 def load_profile(profile_file):
@@ -85,6 +78,8 @@ def load_profile(profile_file):
             continue
         profile_key, profile_value = profile_line.split("=", 1)
         profile[profile_key] = profile_value
+    profile["INFLUXDB_HOST"] = os.environ['INFLUXDB_HOST'] if "INFLUXDB_HOST" in os.environ else DEFAULT_INFLUXDB_HOST
+    profile["INFLUXDB_PORT"] = os.environ['INFLUXDB_PORT'] if "INFLUXDB_PORT" in os.environ else DEFAULT_INFLUXDB_PORT
     return profile
 
 
@@ -102,27 +97,19 @@ def med(data):
     return (data[mid] + data[~mid]) / 2
 
 
-def state():
-    with open("../resources/config/.profile", 'r') as profile_file:
-        p = load_profile(profile_file)
-
-        r = post(
-            url="http://192.168.1.10:8086/api/v2/query?org=home",
-            headers={
-                'Authorization': 'Token {}'.format(p["INFLUXDB_TOKEN"]),
-                'Accept': 'application/csv',
-                'Content-type': 'application/vnd.flux'
-            },
-            data="""
-from(bucket: "hosts")
-    |> range(start: -1m, stop: now())
-    |> filter(fn: (r) => r["_measurement"] == "cpu")
-    |> keep(columns: ["_value", "_time"])
-    |> aggregateWindow(every: 10s, fn: mean, createEmpty: false)
-    |> group()
-    |> last()
-            """)
-        print("cpu={}".format(r.content.split("\n")[1].split(",")[5]))
+def query(env, flux):
+    response = post(
+        url="http://{}:{}/api/v2/query?org=home".format(env["INFLUXDB_HOST"], env["INFLUXDB_PORT"]),
+        headers={
+            'Accept': 'application/csv',
+            'Content-type': 'application/vnd.flux',
+            'Authorization': 'Token {}'.format(env["INFLUXDB_TOKEN"])
+        }, data=flux)
+    rows = []
+    for row in response.content.strip().split("\n")[1:]:
+        cols = row.split(",")
+        rows.append([parse(cols[3])] + cols[4:])
+    return rows
 
 
 def ping():
@@ -294,19 +281,49 @@ def lookup():
 
 if __name__ == "__main__":
     time_start_all = time_ms()
-    run_code_all = [
-        ping(),
-        upload(),
-        download(),
-        lookup(),
-    ]
+    profile_path = DEFAULT_PROFILE_PATH if len(sys.argv) == 1 else sys.argv[1]
+    profile = None
+    try:
+        with open(profile_path, 'r') as profile_file:
+            profile = load_profile(profile_file)
+    except Exception as exception:
+        print("Error processing profile [{}] [{}{}]"
+              .format(profile_path, type(exception).__name__, "" if str(exception) == "" else ":{}".format(exception), file=sys.stderr))
+    if profile is not None:
+        run_code_all = []
+        up_code = 0
 
-    print(FORMAT_UPTIME.format(
-        HOST_INTERNET_INTERFACE_ID,
-        HOST_INTERNET_LOCATION,
-        HOST_HOME_NAME,
-        0,
-        len(run_code_all) - run_code_all.count(0),
-        0,
-        time_ms() - time_start_all,
-        time_ns()))
+        run_code_all.append(ping())
+        up_code += run_code_all[-1]
+        run_code_all.append(upload())
+        up_code += run_code_all[-1]
+        run_code_all.append(download())
+        up_code += run_code_all[-1]
+
+        run_code_all.append(lookup())
+        run_code_uptime = RUN_CODE_FAIL_CONFIG
+        uptime_new = None
+        uptime_epoch = None
+        try:
+            uptime_now = datetime.now(pytz.utc)
+            uptime_epoch = int((uptime_now - datetime(1970, 1, 1, tzinfo=pytz.utc)).total_seconds() * 1000000000)
+            uptime_rows = query(profile, QUERY_UPTIME_LAST)
+            if len(uptime_rows) == 0 or up_code > 0:
+                uptime_new = 0
+            else:
+                uptime_new = int((uptime_now - uptime_rows[0][0]).total_seconds()) + int(uptime_rows[0][1])
+        except Exception as exception:
+            print("Error processing uptime [{}{}]"
+                  .format(type(exception).__name__, "" if str(exception) == "" else ":{}".format(exception), file=sys.stderr))
+        if uptime_new is not None and uptime_epoch is not None:
+            run_code_uptime = RUN_CODE_SUCCESS
+        print(FORMAT_UPTIME.format(
+            HOST_INTERNET_INTERFACE_ID,
+            HOST_INTERNET_LOCATION,
+            HOST_HOME_NAME,
+            uptime_new if uptime_new is not None else "",
+            run_code_all.count(0) + (1 if run_code_uptime == RUN_CODE_SUCCESS else 0),
+            len(run_code_all) - run_code_all.count(0) + (1 if run_code_uptime != RUN_CODE_SUCCESS else 0),
+            run_code_uptime,
+            time_ms() - time_start_all,
+            uptime_epoch if uptime_epoch is not None else ""))
