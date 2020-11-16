d72056ffe5d81198edcc0cf3301dfe5b81ce9534
Graham Gear <graham.gear@gmail.com>
2020-11-16 13:03:29 +0800

    Update asystem-10.100.1744

--- macmini-liz/influxdb/src/main/resources/admin.sh
+++ macmini-liz/influxdb/src/main/resources/admin.sh
@@ -2,54 +2,69 @@
 
 . config/.profile
 
-influx config create \
-    --config-name influx_old \
-    --host-url http://macmini-liz:9999 \
-    --org home \
-    --token ${INFLUXDB_TOKEN}
+#influx config create \
+#    --config-name influx_old \
+#    --host-url http://macmini-liz:9999 \
+#    --org home \
+#    --token ${INFLUXDB_TOKEN}
+#
+#influx config create \
+#    --config-name influx_new \
+#    --host-url http://macmini-liz:8086 \
+#    --org home \
+#    --token ${INFLUXDB_TOKEN}
+#
+#influx export all -c influx_old | influx apply -c influx_new
+#
+#influx query -c influx_old 'from(bucket: "asystem") |> range(start: -3y)' --raw > /tmp/data.csv
+#influx query -c influx_new 'from(bucket: "asystem") |> range(start: -1m)' --raw > /tmp/asystem_$(date +%s).csv
+#
+#influx write -c influx_new --format csv -b asystem -f /tmp/data.csv --skipRowOnError
 
-influx config create \
-    --config-name influx_new \
-    --host-url http://macmini-liz:8086 \
-    --org home \
-    --token ${INFLUXDB_TOKEN}
-
-influx export all -c influx_old | influx apply -c influx_new
-
-influx query -c influx_old 'from(bucket: "asystem") |> range(start: -3y)' --raw > /tmp/data.csv
-influx query -c influx_new 'from(bucket: "asystem") |> range(start: -1m)' --raw > /tmp/asystem_$(date +%s).csv
-
-influx write -c influx_new --format csv -b asystem -f /tmp/data.csv --skipRowOnError
-
-curl -G --request GET http://macmini-liz:8086/api/v2/dbrps \
-  --header "Authorization: Token ${INFLUXDB_TOKEN}" \
-  --header "Content-type: application/json" \
-  --data-urlencode "org=home"
-
-curl -G --request GET http://macmini-liz:8086/api/v2/dbrps \
+echo "Get [all] Buckets:" && curl -G --request GET http://macmini-liz:8086/api/v2/buckets \
   --header "Authorization: Token ${INFLUXDB_TOKEN}" \
   --header "Content-type: application/json" \
   --data-urlencode "orgID=3ac4477553d89245"
+echo "" && echo "" && echo "" && echo ""
 
-curl --request POST http://macmini-liz:8086/api/v2/dbrps \
+echo "Create [asystem] DB RP:" && curl --request POST http://macmini-liz:8086/api/v2/dbrps \
   --header "Authorization: Token ${INFLUXDB_TOKEN}" \
   --header 'Content-type: application/json' \
   --data '{
-     "bucketID": "fa47990c0ce24cd1",
-     "database": "hosts",
-     "default": true,
-     "organization": "home",
-     "organizationID": "3ac4477553d89245",
-     "retention_policy": "home-retention-policy"
+    "organization_id": "3ac4477553d89245",
+    "bucket_id": "54b31e32e392a1cb",
+    "database": "asystem",
+    "retention_policy": "autogen",
+    "default": true
   }'
+echo "" && echo "" && echo "" && echo ""
 
-curl --request POST http://macmini-liz:8086/api/v2/dbrps \
+echo "Create [hosts] DB RP:" && curl --request POST http://macmini-liz:8086/api/v2/dbrps \
   --header "Authorization: Token ${INFLUXDB_TOKEN}" \
   --header 'Content-type: application/json' \
   --data '{
-     "bucket": "hosts",
-     "organization": "home"
+    "organization_id": "3ac4477553d89245",
+    "bucket_id": "fa47990c0ce24cd1",
+    "database": "hosts",
+    "retention_policy": "autogen",
+    "default": true
   }'
+echo "" && echo "" && echo "" && echo ""
 
+echo "Get [all] DB RP:" && echo "" && curl -G --request GET http://macmini-liz:8086/api/v2/dbrps \
+  --header "Authorization: Token ${INFLUXDB_TOKEN}" \
+  --header "Content-type: application/json" \
+  --data-urlencode "orgID=3ac4477553d89245"
+echo "" && echo "" && echo "" && echo ""
 
-curl -G --request GET http://macmini-liz:8086/query --header "Authorization: Token ${INFLUXDB_TOKEN}" --data-urlencode "db=hosts" --data-urlencode "q=SELECT "uptime_s" FROM "internet" WHERE time >= now() - 15m"
+echo "Query [asystem] example:" && echo "" && curl -G --request GET http://macmini-liz:8086/query \
+  --header "Authorization: Token ${INFLUXDB_TOKEN}" \
+  --data-urlencode "db=asystem" \
+  --data-urlencode "q=SELECT * FROM W WHERE time >= now() - 15m"
+echo "" && echo "" && echo "" && echo ""
+
+echo "Query [hosts] example:" && echo "" && curl -G --request GET http://macmini-liz:8086/query \
+  --header "Authorization: Token ${INFLUXDB_TOKEN}" \
+  --data-urlencode "db=hosts" \
+  --data-urlencode "q=SELECT uptime_s FROM internet WHERE time >= now() - 15m"
+echo "" && echo "" && echo "" && echo ""
