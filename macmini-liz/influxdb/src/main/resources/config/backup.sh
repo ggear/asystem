01bc015701527102328d7c546459751b6862a6f6
Graham Gear <graham.gear@gmail.com>
2020-11-20 15:27:47 +0800

    Update asystem-10.100.1811

---
+++ macmini-liz/influxdb/src/main/resources/config/backup.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+. /root/.influxdbv2/.profile
+
+TIMESTAMP=$(date +"%Y-%m-%d_%H-%M-%S")
+
+influx backup -b asystem /root/.influxdbv2/backup/${TIMESTAMP} -t $INFLUXDB_TOKEN
+influx backup -b hosts /root/.influxdbv2/backup/${TIMESTAMP} -t $INFLUXDB_TOKEN
