f164d1ccab4f87baba9659ed468b71149def71c9
Graham Gear <graham.gear@gmail.com>
2020-11-20 15:27:47 +0800

    Update asystem-10.100.1811

--- macmini-liz/influxdb/src/main/resources/config/setup.sh
+++ macmini-liz/influxdb/src/main/resources/config/setup.sh
@@ -12,6 +12,25 @@ if [ ! -f "/root/.influxdbv2/configs" ]; then
     export BUCKET=${BUCKET}
     export BUCKET_ID=$(influx bucket list -o home -n ${BUCKET} -t ${INFLUXDB_TOKEN} --json | jq -r '.[0].id')
     influx v1 dbrp create -o home --db ${BUCKET} --rp default --default --bucket-id ${BUCKET_ID} -t ${INFLUXDB_TOKEN}
+    cat <<EOF >>setup_create_auth.exp
+#!/usr/bin/expect -f
+set force_conservative 0
+if {$force_conservative} {
+	set send_slow {1 .1}
+	proc send {ignore arg} {
+		sleep .1
+		exp_send -s -- $arg
+	}
+}
+set timeout -1
+spawn influx v1 auth create -o home --username "influxdb_$env(BUCKET)" --read-bucket "$env(BUCKET_ID)" --write-bucket "$env(BUCKET_ID)" -t "$env(INFLUXDB_TOKEN)"
+match_max 100000
+expect -re {.*password.*}
+send -- "$env(INFLUXDB_KEY)\r"
+expect -re {.*password.*}
+send -- "$env(INFLUXDB_KEY)\r"
+expect eof
+EOF
     expect /root/.influxdbv2/setup_create_auth.exp
     curl -G --silent --request GET "http://influxdb_${BUCKET}:${INFLUXDB_KEY}@localhost:8086/query?db=${BUCKET}" \
       --data-urlencode "q=SELECT count(*) FROM test_metric WHERE time >= now() - 15m"
