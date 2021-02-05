f5e783489f1c9f4a25426ff820a8172b1980ae3d
Graham Gear <graham.gear@gmail.com>
2021-02-05 14:51:04 +0800

    Update asystem-10.100.1876-SNAPSHOT

--- macmini-liz/influxdb/src/main/resources/config/backup.sh
+++ macmini-liz/influxdb/src/main/resources/config/backup.sh
@@ -6,3 +6,6 @@ TIMESTAMP=$(date +"%Y-%m-%d_%H-%M-%S")
 
 influx backup -b asystem /root/.influxdbv2/backup/${TIMESTAMP} -t $INFLUXDB_TOKEN
 influx backup -b hosts /root/.influxdbv2/backup/${TIMESTAMP} -t $INFLUXDB_TOKEN
+
+
+influx delete -org home --bucket asystem --predicate '_measurement="fx"' --start '1970-01-01T00:00:00Z'  --stop $(date +"%Y-%m-%dT%H:%M:%SZ")
