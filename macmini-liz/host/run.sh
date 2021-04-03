1297dbf5a6c137bc75fc13669258fd95c07861b3
Graham Gear <graham.gear@gmail.com>
2021-04-03 12:19:41 +0800

    Update asystem-10.100.1932

---
+++ macmini-liz/host/run.sh
@@ -0,0 +1,48 @@
+#!/bin/sh
+
+################################################################################
+# Samba
+################################################################################
+mkdir -p /data/media /data/backup/timemachine
+cat <<EOF >/etc/samba/smb.conf
+[global]
+  workgroup = WORKGROUP
+  log file = /var/log/samba/log.%m
+  max log size = 1000
+  logging = file
+  panic action = /usr/share/samba/panic-action %d
+  server role = standalone server
+  obey pam restrictions = yes
+  unix password sync = yes
+  passwd program = /usr/bin/passwd %u
+  passwd chat = *Enter\snew\s*\spassword:* %n\n *Retype\snew\s*\spassword:* %n\n *password\supdated\ssuccessfully* .
+  pam password change = yes
+  map to guest = bad user
+  usershare allow guests = yes
+  mdns name = mdns
+  min protocol = SMB2
+  vfs objects = fruit streams_xattr
+  fruit:metadata = stream
+  fruit:model = MacSamba
+  fruit:posix_rename = yes
+  fruit:veto_appledouble = no
+  fruit:wipe_intentionally_left_blank_rfork = yes
+  fruit:delete_empty_adfiles = yes
+
+[Media]
+  comment = Media Files
+  path = /data/media
+  browseable = yes
+  read only = no
+  guest ok = yes
+
+[Time Machine]
+  comment = Backup Files
+  path = /data/backup/timemachine
+  browseable = yes
+  read only = no
+  guest ok = yes
+  vfs objects = fruit streams_xattr
+  fruit:time machine = yes
+EOF
+systemctl restart smbd
--- macmini-liz_macmini-nel/host/run.sh
+++ macmini-liz_macmini-nel/host/run.sh
@@ -134,49 +134,6 @@ DEBIAN_FRONTEND=noninteractive apt-get install -y \
   cifs-utils=2:6.8-2 \
   samba=2:4.9.5+dfsg-5+deb10u1 \
   smbclient=2:4.9.5+dfsg-5+deb10u1
-mkdir -p /data/backup /data/media
-cat <<EOF >/etc/samba/smb.conf
-[global]
-  workgroup = WORKGROUP
-  log file = /var/log/samba/log.%m
-  max log size = 1000
-  logging = file
-  panic action = /usr/share/samba/panic-action %d
-  server role = standalone server
-  obey pam restrictions = yes
-  unix password sync = yes
-  passwd program = /usr/bin/passwd %u
-  passwd chat = *Enter\snew\s*\spassword:* %n\n *Retype\snew\s*\spassword:* %n\n *password\supdated\ssuccessfully* .
-  pam password change = yes
-  map to guest = bad user
-  usershare allow guests = yes
-  mdns name = mdns
-  min protocol = SMB2
-  vfs objects = fruit streams_xattr
-  fruit:metadata = stream
-  fruit:model = MacSamba
-  fruit:posix_rename = yes
-  fruit:veto_appledouble = no
-  fruit:wipe_intentionally_left_blank_rfork = yes
-  fruit:delete_empty_adfiles = yes
-
-[backup]
-  comment = Backup Files
-  path = /data/backup
-  browseable = yes
-  read only = no
-  guest ok = yes
-  vfs objects = fruit streams_xattr
-  fruit:time machine = yes
-
-[media]
-  comment = Media Files
-  path = /data/media
-  browseable = yes
-  read only = no
-  guest ok = yes
-EOF
-systemctl restart smbd
 
 ################################################################################
 # Upgrade
