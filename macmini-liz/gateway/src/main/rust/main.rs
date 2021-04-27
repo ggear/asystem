1b977738ac23e3a916c12f246982d315609d8853
Graham Gear <graham.gear@gmail.com>
2021-04-27 15:43:20 +0800

    Update rust package layout to confirm to project

--- macmini-liz/gateway/src/main/rust/main.rs
+++ macmini-liz/gateway/src/main/rust/main.rs
@@ -1,12 +1,9 @@
-// printing current date from: https://rust-lang-nursery.github.io/rust-cookbook/datetime/parse.html#display-formatted-date-and-time
-
 extern crate chrono;
-use chrono::{DateTime, Utc};
 
+use chrono::{DateTime, Utc};
 
 fn main() {
     let now: DateTime<Utc> = Utc::now();
-
     println!("UTC now is: {}", now);
     println!("UTC now in RFC 2822 is: {}", now.to_rfc2822());
     println!("UTC now in RFC 3339 is: {}", now.to_rfc3339());
