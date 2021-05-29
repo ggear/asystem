2734668d186a39db599aaee356ec4ff955dc29cf
Graham Gear <graham.gear@gmail.com>
2021-05-29 20:12:30 +0800

    Update asystem-10.100.2025

--- macmini-liz/grafana/deploy.sh
+++ macmini-liz/grafana/deploy.sh
@@ -9,5 +9,14 @@ cd ../grafonnet-lib
 #curl -XPOST --silent ${GRAFANA_URL}/api/user/using/1 | jq
 #./../grizzly/grr apply ./../dashboards/template/generated/dashboards_all.jsonnet
 
+if [ $(curl --silent ${GRAFANA_URL}/api/orgs/2 | jq -r '.id' | grep 2 | wc -l) -eq 0 ]; then
+  curl -XPOST --silent ${GRAFANA_URL}/api/orgs \
+    -H "Accept: application/json" \
+    -H "Content-Type: application/json" \
+    -d '{
+          "name": "Private Portal"
+        }' | jq
+fi
+
 curl -XPOST --silent ${GRAFANA_URL}/api/user/using/2 | jq
 ./../grizzly/grr apply ./../dashboards/template/private/generated/dashboards_all.jsonnet
