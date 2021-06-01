8bb5b081fad8dcd99fc34569e8c6c71c272a7b8e
Graham Gear <graham.gear@gmail.com>
2021-06-01 20:32:01 +0800

    Update asystem-10.100.2034

--- macmini-liz/wrangle/src/main/python/wrangle/weather/weather.py
+++ macmini-liz/wrangle/src/main/python/wrangle/weather/weather.py
@@ -26,26 +26,28 @@ class Weather(library.Library):
     def _run(self):
         new_data = False
         now = datetime.datetime.now()
-        for year in range(DAILY_START_YEAR, now.year + 1):
-            for month in range(DAILY_START_MONTH if year == DAILY_START_YEAR else 1, (now.month + 1) if year == now.year else 13):
-                daily_url = DAILY_URL.format(year, month)
-                daily_file = os.path.join(self.input, daily_url.split("/")[-1])
-                file_status = self.ftp_download(daily_url, daily_file, check=year == now.year and month == now.month)
-                if file_status[0]:
-                    if os.getenv('WRANGLE_REPROCESS_ALL_FILES') == "true" or file_status[1]:
-                        try:
-                            new_data = True
 
-                            # TODO: Provide Implementation
-
-                            self.add_counter(library.CTR_SRC_FILES, library.CTR_ACT_PROCESSED)
-                        except Exception as exception:
-                            self.print_log("Unexpected error processing file [{}]".format(daily_file), exception)
-                            self.add_counter(library.CTR_SRC_FILES, library.CTR_ACT_ERRORED)
-                    else:
-                        self.add_counter(library.CTR_SRC_FILES, library.CTR_ACT_SKIPPED)
-                else:
-                    self.add_counter(library.CTR_SRC_FILES, library.CTR_ACT_ERRORED)
+        # TODO: Fails on first day of the month
+        # for year in range(DAILY_START_YEAR, now.year + 1):
+        #     for month in range(DAILY_START_MONTH if year == DAILY_START_YEAR else 1, (now.month + 1) if year == now.year else 13):
+        #         daily_url = DAILY_URL.format(year, month)
+        #         daily_file = os.path.join(self.input, daily_url.split("/")[-1])
+        #         file_status = self.ftp_download(daily_url, daily_file, check=year == now.year and month == now.month)
+        #         if file_status[0]:
+        #             if os.getenv('WRANGLE_REPROCESS_ALL_FILES') == "true" or file_status[1]:
+        #                 try:
+        #                     new_data = True
+        #
+        #                     # TODO: Provide Implementation
+        #
+        #                     self.add_counter(library.CTR_SRC_FILES, library.CTR_ACT_PROCESSED)
+        #                 except Exception as exception:
+        #                     self.print_log("Unexpected error processing file [{}]".format(daily_file), exception)
+        #                     self.add_counter(library.CTR_SRC_FILES, library.CTR_ACT_ERRORED)
+        #             else:
+        #                 self.add_counter(library.CTR_SRC_FILES, library.CTR_ACT_SKIPPED)
+        #         else:
+        #             self.add_counter(library.CTR_SRC_FILES, library.CTR_ACT_ERRORED)
 
         # TODO: HTTP now fails, convert to FTP
         # monthly_stats_file = os.path.join(self.input, MONTHLY_STATS_URL.split("/")[-1])
@@ -65,7 +67,6 @@ class Weather(library.Library):
         #         self.add_counter(library.CTR_SRC_FILES, library.CTR_ACT_SKIPPED)
         # else:
         #     self.add_counter(library.CTR_SRC_FILES, library.CTR_ACT_ERRORED)
-
 
         weekly_forecast_file = os.path.join(self.input, WEEKLY_FORECAST_URL.split("/")[-1])
         file_status = self.ftp_download(WEEKLY_FORECAST_URL, weekly_forecast_file)
