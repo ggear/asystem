6f218a56051329205f96db5bee341550b0ff822b
Graham Gear <graham.gear@gmail.com>
2021-07-12 12:53:50 +0800

    Update asystem-10.100.2092

--- macmini-zoe_macbookpro-flo_macmini-nel_macmini-liz/keys/run.sh
+++ macmini-zoe_macbookpro-flo_macmini-nel_macmini-liz/keys/run.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-SERVICE_HOME=/home/asystem/${SERVICE_NAME}/${SERVICE_VERSION_ABSOLUTE}
 SERVICE_INSTALL=/var/lib/asystem/install/*$(hostname)*/${SERVICE_NAME}/${SERVICE_VERSION_ABSOLUTE}
 
 cd ${SERVICE_INSTALL} || exit
