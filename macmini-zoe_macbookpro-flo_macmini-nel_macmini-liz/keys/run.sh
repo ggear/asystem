34c72a2bff67d18bbc38c45abd61d2f5a6019225
Graham Gear <graham.gear@gmail.com>
2021-07-12 13:32:32 +0800

    Update asystem-10.100.2094

--- macmini-zoe_macbookpro-flo_macmini-nel_macmini-liz/keys/run.sh
+++ macmini-zoe_macbookpro-flo_macmini-nel_macmini-liz/keys/run.sh
@@ -3,6 +3,16 @@
 SERVICE_INSTALL=/var/lib/asystem/install/*$(hostname)*/${SERVICE_NAME}/${SERVICE_VERSION_ABSOLUTE}
 
 cd ${SERVICE_INSTALL} || exit
-cp -rvf ./config/id_rsa.pub /root/.ssh
-cp -rvf ./config/.id_rsa /root/.ssh/id_rsa
+
+USER=root
+GROUP=root
+HOME=/root
+if [ -d /Users/graham ]; then
+  USER=graham
+  GROUP=staff
+  HOME=/Users/graham
+fi
+
+cp -rvf ./config/id_rsa.pub ${HOME}/.ssh
+cp -rvf ./config/.id_rsa ${HOME}/.ssh/id_rsa
 rm -rfv ./config/.id_rsa
