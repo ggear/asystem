c78d2d5cb75553e70caa9a2d74ae6a9b3a5ac2cd
Graham Gear <graham.gear@gmail.com>
2021-07-28 13:26:33 +0800

    Update asystem-10.100.2121-SNAPSHOT

--- macmini-liz/wrangle/src/test/python/system/system_tests.py
+++ macmini-liz/wrangle/src/test/python/system/system_tests.py
@@ -67,7 +67,7 @@ def test_reload():
     data_public_len = bucket_length("data_public")
     data_private_len = bucket_length("data_private")
     process = subprocess.Popen("docker exec -e WRANGLE_REPROCESS_ALL_FILES=true wrangle telegraf --debug --once",
-        shell=True, stdout=subprocess.PIPE)
+                               shell=True, stdout=subprocess.PIPE)
     process.wait()
     assert process.returncode == 0
     assert bucket_length("data_public") > data_public_len
