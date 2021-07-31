14cbfcd6e8b621b70c28a5eb93d7a3a434039d89
Graham Gear <graham.gear@gmail.com>
2021-07-31 09:36:54 +0800

    Update asystem-10.100.2125

--- macmini-zoe/host/run.sh
+++ macmini-zoe/host/run.sh
@@ -1,3 +1,51 @@
 #!/bin/sh
 
-echo "Install!"
+# Command Line
+# - sudo passwd
+# - vi /etc/ssh/sshd_config -> PermitRootLogin yes
+
+# Settings
+# - Sharing -> Computer Name
+# - Sharing -> Remote Login
+# - Security & Privacy -> Firewall
+# - Dock & Menu Bar -> Automatically hide and show the Dock
+# - Displays -> Nightshift -> Sunset to Sunrise
+# - Keyboard -> Key repeat
+# - Keyboard -> Delay until repeat
+# - Keyboard -> Shortcuts -> App Shortcuts -> Show Previous Tab
+# - Keyboard -> Shortcuts -> App Shortcuts -> Show Next Tab
+# - Users & Groups -> Icon edit
+
+# Terminal Settings
+# - Profiles -> When the shell exits -> Close the window
+
+# App Store
+# - Calca
+# - Infuse
+# - Adblock Plus
+
+# Home
+#mkdir -p ~/_/dev
+#cd ~/_/dev
+#git clone https://github.com/ggear/asystem.git
+
+# Brew
+#/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+#brew update
+#brew upgrade
+#brew install htop wget watch
+
+# CleanMyMac X
+#wget https://dl.devmate.com/com.macpaw.CleanMyMac4/CleanMyMacX.dmg
+
+# iStat Pro
+#wget https://cdn.bjango.com/files/istatmenus6/istatmenus6.60.zip
+
+# Sublime Text
+#wget https://download.sublimetext.com/sublime_text_build_4107_mac.zip
+
+# Kiwi for Gmail
+#wget https://downloads.kiwiforgmail.com/kiwi/release/consumer/Kiwi+for+Gmail+Setup.dmg
+
+# VLC Download
+#wget https://mirror.aarnet.edu.au/pub/videolan/vlc/3.0.16/macosx/vlc-3.0.16-intel64.dmg
