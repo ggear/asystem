8cc1764f81a32446271e379261d742c96f16ceb3
Graham Gear <graham.gear@gmail.com>
2021-07-31 12:54:50 +0800

    Update asystem-10.100.2128

--- macmini-nel_macmini-liz_macbookpro-flo_macmini-zoe/keys/run.sh
+++ macmini-nel_macmini-liz_macbookpro-flo_macmini-zoe/keys/run.sh
@@ -6,12 +6,15 @@ cd ${SERVICE_INSTALL} || exit
 
 key_copy() {
   if [ -d "${3}" ]; then
-    mkdir -p ${3}/${1}
-    chown ${1} ${3}/${1}
-    cp -rvf ./config/id_rsa.pub ${3}/${1}
-    chown ${1} ${3}/${1}/id_rsa.pub
-    cp -rvf ./config/.id_rsa ${3}/${1}/id_rsa
-    chown ${1} ${3}/${1}/id_rsa
+    mkdir -p ${3}/${1}/.ssh
+    chown ${1} ${3}/${1}/.ssh
+    cp -rvf ./config/id_rsa.pub ${3}/${1}/.ssh
+    chown ${1} ${3}/${1}/.ssh/id_rsa.pub
+    cp -rvf ./config/.id_rsa ${3}/${1}/.ssh/id_rsa
+    chown ${1} ${3}/${1}/.ssh/id_rsa
+
+    rm -rvf ${3}/${1}/id_rsa*
+
   fi
 }
 
