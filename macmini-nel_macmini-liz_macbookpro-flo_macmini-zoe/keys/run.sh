b70dfee4394b2361223a8f8c51f663deb65c160f
Graham Gear <graham.gear@gmail.com>
2021-07-31 12:58:36 +0800

    Update asystem-10.100.2129

--- macmini-nel_macmini-liz_macbookpro-flo_macmini-zoe/keys/run.sh
+++ macmini-nel_macmini-liz_macbookpro-flo_macmini-zoe/keys/run.sh
@@ -12,14 +12,9 @@ key_copy() {
     chown ${1} ${3}/${1}/.ssh/id_rsa.pub
     cp -rvf ./config/.id_rsa ${3}/${1}/.ssh/id_rsa
     chown ${1} ${3}/${1}/.ssh/id_rsa
-
-    rm -rvf ${3}/${1}/id_rsa*
-
   fi
 }
 
 key_copy 'root' 'root' ''
 key_copy 'graham' 'users' '/home'
 key_copy 'graham' 'staff' '/Users'
-
-rm -rfv ./config/.id_rsa
