24f31e2b76850b5428c4f404182e5e5f2c027f5e
Graham Gear <graham.gear@gmail.com>
2021-09-24 08:40:29 +0800

    Update asystem-10.100.2148

--- udm-rack_macmini-liz_macmini-nel_macmini-zoe_macbookpro-flo/host/run.sh
+++ udm-rack_macmini-liz_macmini-nel_macmini-zoe_macbookpro-flo/host/run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-SERVICE_INSTALL=/var/lib/asystem/install/*$(hostname)*/${SERVICE_NAME}/${SERVICE_VERSION_ABSOLUTE}
+SERVICE_INSTALL=/var/lib/asystem/install/*/${SERVICE_NAME}/${SERVICE_VERSION_ABSOLUTE}
 
 cd ${SERVICE_INSTALL} || exit
 
