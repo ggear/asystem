9d9ea29594a9bf0a8254d758e9846a5716f1c543
Graham Gear <graham.gear@gmail.com>
2021-11-08 21:04:14 +0800

    Update asystem-10.100.2196

--- macmini-liz/wrangle/run_post.sh
+++ macmini-liz/wrangle/run_post.sh
@@ -4,4 +4,4 @@ SERVICE_HOME=/home/asystem/${SERVICE_NAME}/${SERVICE_VERSION_ABSOLUTE}
 SERVICE_INSTALL=/var/lib/asystem/install/*$(hostname)*/${SERVICE_NAME}/${SERVICE_VERSION_ABSOLUTE}
 
 cd ${SERVICE_INSTALL} || exit
-docker exec -e WRANGLE_REPROCESS_ALL_FILES=true wrangle telegraf --debug --once
+#docker exec -e WRANGLE_REPROCESS_ALL_FILES=true wrangle telegraf --debug --once
