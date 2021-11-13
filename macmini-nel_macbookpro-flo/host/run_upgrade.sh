6fae9bf228ea523e56060467bf820a8f3d4f7b30
Graham Gear <graham.gear@gmail.com>
2021-11-13 12:06:46 +0800

    Update asystem-10.100.2210

--- macmini-nel_macbookpro-flo/host/run_upgrade.sh
+++ macmini-nel_macbookpro-flo/host/run_upgrade.sh
@@ -5,16 +5,19 @@
 ################################################################################
 cat <<EOF >/etc/apt/sources.list
 deb http://deb.debian.org/debian bullseye main contrib non-free
-deb-src http://deb.debian.org/debian bullseye main contrib non-free
+#deb-src http://deb.debian.org/debian bullseye main contrib non-free
 
 deb http://deb.debian.org/debian-security/ bullseye-security main contrib non-free
-deb-src http://deb.debian.org/debian-security/ bullseye-security main contrib non-free
+#deb-src http://deb.debian.org/debian-security/ bullseye-security main contrib non-free
 
 deb http://deb.debian.org/debian bullseye-updates main contrib non-free
-deb-src http://deb.debian.org/debian bullseye-updates main contrib non-free
+#deb-src http://deb.debian.org/debian bullseye-updates main contrib non-free
 
 deb http://deb.debian.org/debian bullseye-backports main contrib non-free
-deb-src http://deb.debian.org/debian bullseye-backports main contrib non-free
+#deb-src http://deb.debian.org/debian bullseye-backports main contrib non-free
+
+deb [arch=amd64] http://download.docker.com/linux/debian bullseye stable
+#deb-src [arch=amd64] http://download.docker.com/linux/debian bullseye stable
 
 EOF
 
