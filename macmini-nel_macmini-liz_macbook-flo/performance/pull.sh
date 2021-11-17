610c0fed87058386ea41e78b21d2117f01752442
Graham Gear <graham.gear@gmail.com>
2021-11-17 21:29:11 +0800

    Update asystem-10.100.2289

--- macmini-nel_macmini-liz_macbook-flo/performance/pull.sh
+++ macmini-nel_macmini-liz_macbook-flo/performance/pull.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-rm -rf src/main/resources/scripts
-mkdir -p src/main/resources/scripts &&
-  cp -rvf ../../../asystem-external/performance/BSFD/benchmarks/sysbench/parse* src/main/resources/scripts
+rm -rf src/test/resources/scripts
+mkdir -p src/test/resources/scripts &&
+  cp -rvf ../../../asystem-external/performance/BSFD/benchmarks/sysbench/parse* src/test/resources/scripts
 
 DIR_ROOT=src/test/resources/results/$(date +%F)
-mkdir -p ${DIR_ROOT}
+rm -rf ${DIR_ROOT} && mkdir -p ${DIR_ROOT}
 HOSTS=$(echo $(basename $(dirname $(pwd))) | tr "_" "\n")
 for HOST in ${HOSTS}; do
     scp -r root@${HOST}:/home/asystem/performance ${DIR_ROOT}/${HOST}
