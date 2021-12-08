ea8fdffb09b59873825d21d5bb471c5f6dc6d373
Graham Gear <graham.gear@gmail.com>
2021-12-08 14:23:46 +0800

    Update asystem-10.100.2396

--- macmini-nel_macmini-liz_macbook-flo/_samba/src/main/resources/config/import.sh
+++ macmini-nel_macmini-liz_macbook-flo/_samba/src/main/resources/config/import.sh
@@ -1,19 +1,25 @@
 #!/bin/bash
 
-chown -R nobody:nogroup media
-setfacl -bR media
-chmod -R 644 media
-chmod -R a+rwX media
-chmod +x ./repair.sh
-find . -type f -name .DS_Store -exec rm -f {} \;
+setfacl -bR /data
+chmod -R 644 /data
+chmod -R a+rwX /data
+chown -R nobody:nogroup /data
+find /data -type f -name .DS_Store -exec rm -f {} \;
 
+import_files() {
+  if [ -b /dev/sdc1 ] && [ -d /data/media/${1} ]; then
+    mkdir -p /media/usbdrive
+    umount -fq /media/usbdrive
+    mount /dev/sdc1 /media/usbdrive
+    if [ -d /media/usbdrive/${1} ]; then
+      rsync -avP /media/usbdrive/${1} /data/tmp
+    fi
+    umount -fq /media/usbdrive
+  fi
+}
 
+import_files Audio
+import_files Movies
+import_files Series
 
-# mount /dev/sdc1 /media/cdrom
-# rsync -avP /media/cdrom/TV/XX /data/media/Series/XX
-# rename -v s/\ .*mkv/\.mkv/ Battlestar-Galactica_s04e*
-# rename -v 's/(Veep_s07e[0-9][0-9])\..*\.mkv/$1.mkv/' *.mkv
-# rename -v 's/(.*)s02e([0-9][0-9])\..*\.mkv/$1s02e$2.mkv/' *.mkv
-# for f in *.mkv; do mkvmerge -o _$f $f; mv -vf _$f $f; done
-# nohup rsync -ahv --no-owner --no-group --no-perms --ignore-existing --progress /data/media/* /media/cdrom &
-# nohup rsync -avP /media/cdrom /data/tmp &
+echo "Example rename command:" && echo "rename -v 's/(.*)S([0-9][0-9])E([0-9][0-9])\..*\.mkv/\$1s\$2e\$3.mkv/' *.mkv"
