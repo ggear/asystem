102300b0d63a4b87e104096757095cf24b8f4ae5
Graham Gear <graham.gear@gmail.com>
2022-01-01 18:52:20 +0800

    Update asystem-10.100.2445

--- macbook-rae/_users/run.sh
+++ macbook-rae/_users/run.sh
@@ -6,8 +6,10 @@ cd ${SERVICE_INSTALL} || exit
 
 user_add() {
   if [ -d "${3}" ]; then
-    [ $(grep "${1}" "/etc/passwd" | wc -l) -eq 0 ] && adduser -D "${1}"
-    [ -d "${3}${1}" ] && mkdir -p ${3}${1} && chmod 711 ${3}
+    if [ ${4} ]; then
+      [ $(grep "${1}" "/etc/passwd" | wc -l) -eq 0 ] && adduser -D "${1}"
+      [ -d "${3}${1}" ] && mkdir -p ${3}${1} && chmod 711 ${3}
+    fi
     if [ ! -e "${3}${1}/.ssh/authorized_keys" ] || [ $(grep graham "${3}${1}/.ssh/authorized_keys" | wc -l) -eq 0 ]; then
       mkdir -p ${3}${1}/.ssh
       chmod 700 ${3}${1}/.ssh
@@ -16,11 +18,14 @@ user_add() {
 ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC17bbhX9GtT/YyDrYO98Q9xzfyhn3WtBbftpFJ1yTm0wkssNMrW6YNjW2dVDm2qPgJUg9pKqw+XdyDUcxKWal1mLecPDYNAJgU0mJkFehDAxW91YNzjCH+kY70mVgZCzhi6XAx8pX5TDDHRMNnp76OyblMlge8g21tf3AwrzvJIQuC7UTrJYRWsxAIxTQBKqPW96JfvPLXk9l+vs31xC1y+wbWlKRey8LpYi4v/dePkkpQaac4R2DR4AlJNPRsoSn+W1zYYMi34bw4smpglrH83fA42rWClPkth/X2RzXPrQMyBNPFLalMbDe+xXMq6ExdfTlU6gE4s8dW4Gi3b1J1 graham.gear@gmail.com
 EOF
     fi
-    chown -R ${1} ${3}${1} || true
-    chgrp -R ${2} ${3}${1} || true
+    if [ ${4} ]; then
+      chown -R ${1} ${3}${1} || true
+      chgrp -R ${2} ${3}${1} || true
+    fi
   fi
 }
 
-user_add 'root' 'root' '/'
-user_add 'graham' 'users' '/home/'
-user_add 'graham' 'staff' '/Users/'
+user_add 'root' 'root' '/' false
+user_add 'root' 'root' '/var' false
+user_add 'graham' 'users' '/home/' true
+user_add 'graham' 'staff' '/Users/' true
