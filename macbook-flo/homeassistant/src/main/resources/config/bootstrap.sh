2e127de229df1aae8c95ca3abfd93ee58fac5edf
Graham Gear <graham.gear@gmail.com>
2022-02-08 11:12:35 +0800

    Update asystem-10.100.2489

--- macbook-flo/homeassistant/src/main/resources/config/bootstrap.sh
+++ macbook-flo/homeassistant/src/main/resources/config/bootstrap.sh
@@ -18,10 +18,11 @@ echo "--------------------------------------------------------------------------
 # Install integrations manually (no option to do so programmatically currently):
 #   - Sonos
 #   - Apple TV
+#   - Withings
 #   - Google Cast
+#   - Unifi Protect
 #   - Philips hue (allow groups)
 #   - SenseME (have to add for each device)
-#   - Unifi Protect
 #   - TP-Link Smart Home (configure each device integration after adding primary integration)
 #   - Bureau of Meteorology (gooseberry_hill for observations, darlington_forecast for weather forecast, 7 day forecast)
 
