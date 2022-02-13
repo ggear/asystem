c610b856a354b34a9550692e027b1ddc96864b0f
Graham Gear <graham.gear@gmail.com>
2022-02-13 15:51:45 +0800

    Update asystem-10.100.2550

--- macmini-liz/weewx/pull.sh
+++ macmini-liz/weewx/pull.sh
@@ -4,7 +4,7 @@ rm -rf src/main/resources/config/skins
 mkdir -p src/main/resources/config/skins &&
   cp -rvf ../../../asystem-external/weewx/skins/Seasons src/main/resources/config/skins
 sed -i '' 's/anti_alias = 1/anti_alias = 5/g' src/main/resources/config/skins/Seasons/skin.conf
-sed -i '' 's/image_width = 500/image_width = 1000/g' src/main/resources/config/skins/Seasons/skin.conf
-sed -i '' 's/image_height = 180/image_height = 360/g' src/main/resources/config/skins/Seasons/skin.conf
-sed -i '' 's/width: 500px; \/\* should match the image width in skin.conf \*\//width: 1000px;/g' src/main/resources/config/skins/Seasons/seasons.css
+sed -i '' 's/image_width = 500/image_width = 750/g' src/main/resources/config/skins/Seasons/skin.conf
+sed -i '' 's/image_height = 180/image_height = 270/g' src/main/resources/config/skins/Seasons/skin.conf
+sed -i '' 's/width: 500px; \/\* should match the image width in skin.conf \*\//width: 750px;/g' src/main/resources/config/skins/Seasons/seasons.css
 sed -i '' 's/bottom_label_format = %x %X/bottom_label_format = %d\/%m\/%Y %H:%M/g' src/main/resources/config/skins/Seasons/skin.conf
