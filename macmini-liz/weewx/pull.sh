94e6af30f9e619ce6a087a9b2527d9c401f20627
Graham Gear <graham.gear@gmail.com>
2022-02-13 19:08:43 +0800

    Update asystem-10.100.2557

--- macmini-liz/weewx/pull.sh
+++ macmini-liz/weewx/pull.sh
@@ -8,4 +8,4 @@ sed -i '' 's/image_width = 500/image_width = 750/g' src/main/resources/config/sk
 sed -i '' 's/image_height = 180/image_height = 270/g' src/main/resources/config/skins/Seasons/skin.conf
 sed -i '' 's/width: 500px; \/\* should match the image width in skin.conf \*\//width: 750px;/g' src/main/resources/config/skins/Seasons/seasons.css
 sed -i '' 's/bottom_label_format = %x %X/bottom_label_format = %d\/%m\/%Y %H:%M/g' src/main/resources/config/skins/Seasons/skin.conf
-sed -i '' 's/plot_groups = barometer, tempdew, tempfeel, hum, wind, winddir, windvec, rain, ET, UV, radiation, lightning, tempin, humin, tempext, humext, tempext2, humext2, templeaf, wetleaf, tempsoil, moistsoil, pm/plot_groups = tempdew, tempfeel, hum, wind, windvec, rain, tempin, humin/g' src/main/resources/config/skins/Seasons/skin.conf
+sed -i '' 's/plot_groups = barometer, tempdew, tempfeel, hum, wind, winddir, windvec, rain, ET, UV, radiation, lightning, tempin, humin, tempext, humext, tempext2, humext2, templeaf, wetleaf, tempsoil, moistsoil, pm/plot_groups = tempdew, tempfeel, hum, rain, wind, windvec, tempin, humin/g' src/main/resources/config/skins/Seasons/skin.conf
