3ea03710b0d32445a7959cd31df1d722b0bf073f
Graham Gear <graham.gear@gmail.com>
2022-02-13 19:47:17 +0800

    Update asystem-10.100.2558

--- macmini-liz/weewx/pull.sh
+++ macmini-liz/weewx/pull.sh
@@ -2,7 +2,8 @@
 
 rm -rf src/main/resources/config/skins
 mkdir -p src/main/resources/config/skins &&
-  cp -rvf ../../../asystem-external/weewx/skins/Seasons src/main/resources/config/skins
+  cp -rvf ../../../asystem-external/weewx/weewx-core/skins/Seasons src/main/resources/config/skins &&
+  cp -rvf ../../../asystem-external/weewx/weewx-responsive-skin/Responsive src/main/resources/config/skins
 sed -i '' 's/anti_alias = 1/anti_alias = 5/g' src/main/resources/config/skins/Seasons/skin.conf
 sed -i '' 's/image_width = 500/image_width = 750/g' src/main/resources/config/skins/Seasons/skin.conf
 sed -i '' 's/image_height = 180/image_height = 270/g' src/main/resources/config/skins/Seasons/skin.conf
