9908d0759facfd0306b1736e193e89f7c2194f15
Graham Gear <graham.gear@gmail.com>
2022-03-01 12:59:39 +0800

    Update asystem-10.100.2658

--- udm-rack/udmutilities/run_post.sh
+++ udm-rack/udmutilities/run_post.sh
@@ -3,6 +3,13 @@
 SERVICE_HOME=/home/asystem/${SERVICE_NAME}/${SERVICE_VERSION_ABSOLUTE}
 SERVICE_INSTALL=/var/lib/asystem/install/*$(hostname)*/${SERVICE_NAME}/${SERVICE_VERSION_ABSOLUTE}
 
+add_on_boot_script() {
+  [ ! -f "/mnt/data/on_boot.d/${1}.sh" ] &&
+    [ -d "/var/lib/asystem/install" ] &&
+    [ -f "$(ls -td /var/lib/asystem/install/*udm-rack*/${2}/* | head -1)/run.sh" ] &&
+    cp -rvf "$(ls -td /var/lib/asystem/install/*udm-rack*/${2}/* | head -1)/run.sh" "/mnt/data/on_boot.d/${1}.sh"
+}
+
 cd ${SERVICE_INSTALL} || exit
 
 chmod a+x ./config/udm-utilities/on-boot-script/remote_install.sh
@@ -15,6 +22,10 @@ if [ ! -f /mnt/data/on_boot.d/05-container-common.sh ]; then
   cp -rvf ./config/udm-utilities/container-common/on_boot.d/05-container-common.sh /mnt/data/on_boot.d
   /mnt/data/on_boot.d/05-container-common.sh
 fi
+
+add_on_boot_script "06-unifios" "_unifios"
+add_on_boot_script "07-users" "_users"
+add_on_boot_script "08-home" "_home"
 
 if [ ! -f /mnt/data/on_boot.d/06-unifios.sh ]; then
   cp -rvf /var/lib/asystem/install/*udm-rack*/_unifios/*/run.sh /mnt/data/on_boot.d/06-unifios.sh
