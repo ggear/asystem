fe370c2bff6a3654d82d89bf333b5569455fff49
Graham Gear <graham.gear@gmail.com>
2022-03-01 09:31:31 +0800

    Update asystem-10.100.2639

--- udm-rack_macmini-liz_macmini-nel_macbook-flo_macmini-zoe_macbook-rae/_users/run.sh
+++ udm-rack_macmini-liz_macmini-nel_macbook-flo_macmini-zoe_macbook-rae/_users/run.sh
@@ -19,8 +19,10 @@ user_add() {
 ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC17bbhX9GtT/YyDrYO98Q9xzfyhn3WtBbftpFJ1yTm0wkssNMrW6YNjW2dVDm2qPgJUg9pKqw+XdyDUcxKWal1mLecPDYNAJgU0mJkFehDAxW91YNzjCH+kY70mVgZCzhi6XAx8pX5TDDHRMNnp76OyblMlge8g21tf3AwrzvJIQuC7UTrJYRWsxAIxTQBKqPW96JfvPLXk9l+vs31xC1y+wbWlKRey8LpYi4v/dePkkpQaac4R2DR4AlJNPRsoSn+W1zYYMi34bw4smpglrH83fA42rWClPkth/X2RzXPrQMyBNPFLalMbDe+xXMq6ExdfTlU6gE4s8dW4Gi3b1J1 graham.gear@gmail.com
 EOF
       fi
-      chown -R ${1} "${3}${1}" 2>/dev/null || true
-      chgrp -R ${2} "${3}${1}" 2>/dev/null || true
+      chown ${1} "${3}${1}" 2>/dev/null || true
+      chgrp ${2} "${3}${1}" 2>/dev/null || true
+      chown -R ${1} "${3}${1}/.ssh" 2>/dev/null || true
+      chgrp -R ${2} "${3}${1}/.ssh" 2>/dev/null || true
     fi
   fi
 }
