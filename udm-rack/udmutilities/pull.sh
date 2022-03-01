db96f06771028225ecda1df3459e07d70b83ce63
Graham Gear <graham.gear@gmail.com>
2022-03-01 11:19:20 +0800

    Update asystem-10.100.2645

--- udm-rack/udmutilities/pull.sh
+++ udm-rack/udmutilities/pull.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-rm -rf src/main/resources/config/udm-utilities
-mkdir -p src/main/resources/config/udm-utilities &&
-  cp -rvf ../../../asystem-external/udmutilities/udm-utilities src/main/resources/config &&
-  rm -rf src/main/resources/config/udm-utilities/.github
+# Install remotely
+#rm -rf src/main/resources/config/udm-utilities
+#mkdir -p src/main/resources/config/udm-utilities &&
+#  cp -rvf ../../../asystem-external/udmutilities/udm-utilities src/main/resources/config &&
+#  rm -rf src/main/resources/config/udm-utilities/.github
