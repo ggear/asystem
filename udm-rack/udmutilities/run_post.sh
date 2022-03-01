1d3f409727697f97e67c392d09b2c28d7ec90869
Graham Gear <graham.gear@gmail.com>
2022-03-01 11:21:02 +0800

    Update asystem-10.100.2646

--- udm-rack/udmutilities/run_post.sh
+++ udm-rack/udmutilities/run_post.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+curl -fsL "https://raw.githubusercontent.com/boostchicken/udm-utilities/HEAD/on-boot-script/remote_install.sh" | /bin/sh
+
 #curl -fsL "https://raw.githubusercontent.com/boostchicken/udm-utilities/HEAD/on-boot-script/remote_install.sh" | /bin/sh
 
 #[ -d "/var/lib/asystem/install" ] &&
