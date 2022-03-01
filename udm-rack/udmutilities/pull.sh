48393910c300947282f66742947674899769f5b7
Graham Gear <graham.gear@gmail.com>
2022-03-01 11:31:50 +0800

    Update asystem-10.100.2648

--- udm-rack/udmutilities/pull.sh
+++ udm-rack/udmutilities/pull.sh
@@ -2,5 +2,4 @@
 
 rm -rf src/main/resources/config/udm-utilities
 mkdir -p src/main/resources/config/udm-utilities &&
-  cp -rvf ../../../asystem-external/udmutilities/udm-utilities src/main/resources/config &&
-  rm -rf src/main/resources/config/udm-utilities/.github
+  cp -rvf ../../../asystem-external/udmutilities/udm-utilities/on-boot-script src/main/resources/config/udm-utilities
