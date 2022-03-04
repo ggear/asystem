92b7152718b36b87a1356caa0f311b1815b1d522
Graham Gear <graham.gear@gmail.com>
2022-03-04 16:02:44 +0800

    Update asystem-10.100.2709

--- udm-rack/udmutilities/src/main/resources/config/udm-dnsmasq/01-dnsmasq.sh
+++ udm-rack/udmutilities/src/main/resources/config/udm-dnsmasq/01-dnsmasq.sh
@@ -8,16 +8,20 @@ CONF_CUSTOM_FILES="${CONF_CUSTOM_DIR}/dhcp.dhcpServers"*"custom.conf"
 
 rm -vf ${CONF_CUSTOM_FILES}
 for CONF_SOURCE_FILE in ${CONF_SOURCE_DIR}-*Management*-custom.conf ${CONF_SOURCE_DIR}-*Unfettered*-custom.conf ${CONF_SOURCE_DIR}-*Isolated*-custom.conf; do
-  cp -rvf ${CONF_SOURCE_FILE} ${CONF_CUSTOM_DIR}
   while read CONF_SOURCE_LINE; do
     CONF_CMD_NET=$(echo "${CONF_SOURCE_LINE}" | cut -d',' -f1)
     CONF_MAC=$(echo "${CONF_SOURCE_LINE}" | cut -d',' -f2 | awk '{print tolower($0)}')
     CONF_IP=$(echo "${CONF_SOURCE_LINE}" | cut -d',' -f3)
     CONF_HOST=$(echo "${CONF_SOURCE_LINE}" | cut -d',' -f4)
     echo "${CONF_CMD_NET},${CONF_MAC},${CONF_IP},${CONF_HOST}" >>"${CONF_CUSTOM_FILE}"
-    # Disable killing current DHCP leases, just rely on rebooting clients to pick up new details
-    #    sed -i /".* ${CONF_MAC} .*"/d ${CONF_CURRENT_FILE}
+
+    #Disable killing current DHCP leases, just rely on rebooting clients to pick up new details, perhaps flush only if details have changed?
+    #sed -i /".* ${CONF_MAC} .*"/d ${CONF_CURRENT_FILE}
+
   done <${CONF_SOURCE_FILE}
 done
 
+if [ $(dnsmasq --conf-dir=/run/dnsmasq.conf.d --test) ]; then
+  rm -vf ${CONF_CUSTOM_FILES}
+fi
 kill -9 $(cat /run/dnsmasq.pid)
