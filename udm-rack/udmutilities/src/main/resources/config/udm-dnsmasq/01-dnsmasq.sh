770f120883c2b02fad7a5e4795c147553491dbe8
Graham Gear <graham.gear@gmail.com>
2022-03-04 16:59:14 +0800

    Update asystem-10.100.2720

--- udm-rack/udmutilities/src/main/resources/config/udm-dnsmasq/01-dnsmasq.sh
+++ udm-rack/udmutilities/src/main/resources/config/udm-dnsmasq/01-dnsmasq.sh
@@ -16,8 +16,8 @@ for CONF_SOURCE_FILE in $(ls ${CONF_SOURCE_DIR}-*Management*-custom.conf ${CONF_
     echo "${CONF_CMD_NET},${CONF_MAC},${CONF_IP},${CONF_HOST}" >>"${CONF_CUSTOM_FILE}"
 
     CONF_CURRENT=$(grep ${CONF_MAC} ${CONF_CURRENT_FILE})
-    if [ $(echo -n ${CONF_CURRENT} | wc -w) -eq 1 ]; then
-      if [ $(echo -n ${CONF_CURRENT} | grep -v ${CONF_IP} | wc -w) -eq 1 ] || [ $(echo -n ${CONF_CURRENT} | grep -v ${CONF_HOST} | wc -w) -eq 1 ]; then
+    if [ $(echo -n ${CONF_CURRENT} | wc -w) ]; then
+      if [ $(echo -n ${CONF_CURRENT} | grep -v ${CONF_IP} | wc -w) ] || [ $(echo -n ${CONF_CURRENT} | grep -v ${CONF_HOST} | wc -w) ]; then
         echo sed -i /".* ${CONF_MAC} .*"/d ${CONF_CURRENT_FILE}
         echo $CONF_HOST
       fi
