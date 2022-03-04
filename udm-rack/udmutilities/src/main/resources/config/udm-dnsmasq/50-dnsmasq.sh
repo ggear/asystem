6faef717734fee9ebc13a3ba32cf3f76437f01f4
Graham Gear <graham.gear@gmail.com>
2022-03-04 11:06:23 +0800

    Update asystem-10.100.2701

--- udm-rack/udmutilities/src/main/resources/config/udm-dnsmasq/50-dnsmasq.sh
+++ udm-rack/udmutilities/src/main/resources/config/udm-dnsmasq/50-dnsmasq.sh
@@ -20,4 +20,6 @@ for CONF_SOURCE_FILE in ${CONF_SOURCE_DIR}-*Management*-custom.conf ${CONF_SOURC
   #  done <${CONF_SOURCE_FILE}
 done
 
-kill -9 $(cat /run/dnsmasq.pid)
+killall -9 dnsmasq
+#pkill dnsmasq
+#kill -9 $(cat /run/dnsmasq.pid)
