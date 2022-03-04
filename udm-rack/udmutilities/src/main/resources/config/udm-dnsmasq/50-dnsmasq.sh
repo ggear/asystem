c70158c082cf096d62f4f70aa1ec40755e1570af
Graham Gear <graham.gear@gmail.com>
2022-03-04 10:17:44 +0800

    Update asystem-10.100.2694

--- udm-rack/udmutilities/src/main/resources/config/udm-dnsmasq/50-dnsmasq.sh
+++ udm-rack/udmutilities/src/main/resources/config/udm-dnsmasq/50-dnsmasq.sh
@@ -11,7 +11,7 @@ for CONF_SOURCE_FILE in ${CONF_SOURCE_DIR}-*Management*-custom.conf ${CONF_SOURC
     CONF_MAC=$(echo "${CONF_SOURCE_LINE}" | cut -d',' -f2 | awk '{print tolower($0)}')
     CONF_IP=$(echo "${CONF_SOURCE_LINE}" | cut -d',' -f3)
     CONF_HOST=$(echo "${CONF_SOURCE_LINE}" | cut -d',' -f4)
-    echo "dhcp-CONF_HOST=set:${CONF_NET},${CONF_MAC},${CONF_IP},${CONF_HOST}"
+    echo "dhcp-host=set:${CONF_NET},${CONF_MAC},${CONF_IP},${CONF_HOST}"
     #    echo "dhcp-CONF_HOST=set:${CONF_NET},${CONF_MAC},${CONF_IP},${CONF_HOST}" >>"${CONF_CUSTOM_FILE}"
     echo sed -i /".* ${CONF_MAC} .*"/d ${CONF_CURRENT_FILE}
   done <${CONF_SOURCE_FILE}
