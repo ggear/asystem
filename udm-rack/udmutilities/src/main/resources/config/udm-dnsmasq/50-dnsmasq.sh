c42872a52d32d854bc4244289fe661df67cb73dc
Graham Gear <graham.gear@gmail.com>
2022-03-04 10:45:52 +0800

    Update asystem-10.100.2699

--- udm-rack/udmutilities/src/main/resources/config/udm-dnsmasq/50-dnsmasq.sh
+++ udm-rack/udmutilities/src/main/resources/config/udm-dnsmasq/50-dnsmasq.sh
@@ -2,18 +2,22 @@
 
 CONF_SOURCE_DIR="/mnt/data/udm-dnsmasq/dhcp.dhcpServers"
 CONF_CURRENT_FILE="/mnt/data/udapi-config/dnsmasq.lease"
-CONF_CUSTOM_FILE="/run/dnsmasq.conf.d/dhcp.dhcpServers-custom.conf"
+CONF_CUSTOM_DIR="/run/dnsmasq.conf.d"
+CONF_CUSTOM_FILE="${CONF_CUSTOM_DIR}/dhcp.dhcpServers-custom.conf"
+CONF_CUSTOM_FILES="${CONF_CUSTOM_DIR}/dhcp.dhcpServers*custom.conf"
 
-rm -f "${CONF_CUSTOM_FILE}"
+rm -f "${CONF_CUSTOM_FILES}"
 for CONF_SOURCE_FILE in ${CONF_SOURCE_DIR}-*Management*-custom.conf ${CONF_SOURCE_DIR}-*Unfettered*-custom.conf ${CONF_SOURCE_DIR}-*Isolated*-custom.conf; do
-  while read CONF_SOURCE_LINE; do
-    CONF_NET=$(echo "${CONF_SOURCE_LINE}" | cut -d',' -f1)
-    CONF_MAC=$(echo "${CONF_SOURCE_LINE}" | cut -d',' -f2 | awk '{print tolower($0)}')
-    CONF_IP=$(echo "${CONF_SOURCE_LINE}" | cut -d',' -f3)
-    CONF_HOST=$(echo "${CONF_SOURCE_LINE}" | cut -d',' -f4)
-    echo "dhcp-host=set:${CONF_NET},${CONF_MAC},${CONF_IP},${CONF_HOST}" >>"${CONF_CUSTOM_FILE}"
-    sed -i /".* ${CONF_MAC} .*"/d ${CONF_CURRENT_FILE}
-  done <${CONF_SOURCE_FILE}
+  # Disable killing current DHCP leases, just rely on rebooting clients to pick up new details
+  cp -rvf ${CONF_SOURCE_FILE} ${CONF_CUSTOM_DIR}
+  #  while read CONF_SOURCE_LINE; do
+  #    CONF_NET=$(echo "${CONF_SOURCE_LINE}" | cut -d',' -f1)
+  #    CONF_MAC=$(echo "${CONF_SOURCE_LINE}" | cut -d',' -f2 | awk '{print tolower($0)}')
+  #    CONF_IP=$(echo "${CONF_SOURCE_LINE}" | cut -d',' -f3)
+  #    CONF_HOST=$(echo "${CONF_SOURCE_LINE}" | cut -d',' -f4)
+  #    echo "dhcp-host=set:${CONF_NET},${CONF_MAC},${CONF_IP},${CONF_HOST}" >>"${CONF_CUSTOM_FILE}"
+  #    sed -i /".* ${CONF_MAC} .*"/d ${CONF_CURRENT_FILE}
+  #  done <${CONF_SOURCE_FILE}
 done
 
 kill -9 $(cat /run/dnsmasq.pid)
