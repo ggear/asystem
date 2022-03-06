ad4c058af78fd78561a5a1f4d1b9eb7f15e68d15
Graham Gear <graham.gear@gmail.com>
2022-03-06 15:34:56 +0800

    Update asystem-10.100.2782

--- macbook-flo/homeassistant/deploy.sh
+++ macbook-flo/homeassistant/deploy.sh
@@ -6,6 +6,8 @@ HOST=$(basename $(dirname $PWD))
 HOME=$(ssh root@${HOST} "find /home/asystem/homeassistant -maxdepth 1 -mindepth 1 ! -name latest 2>/dev/null | sort | tail -n 1")
 INSTALL=$(ssh root@${HOST} "find /var/lib/asystem/install/${HOST}/homeassistant -maxdepth 1 -mindepth 1 ! -name latest ! -name latest 2>/dev/null | sort | tail -n 1")
 
-ssh root@${HOST} "rm -rf ${HOME}/custom_components && rm -rf ${HOME}/custom_packages && rm -rf ${HOME}/ui-lovelace && rm -rf ${HOME}/www"
+# TODO: Fix
+#ssh root@${HOST} "rm -rf ${HOME}/custom_components && rm -rf ${HOME}/custom_packages && rm -rf ${HOME}/ui-lovelace && rm -rf ${HOME}/www"
+
 scp -r $PWD/src/main/resources/config/* root@${HOST}:${HOME}
 ssh root@${HOST} "cd ${INSTALL} && docker-compose --compatibility restart" 2>/dev/null
