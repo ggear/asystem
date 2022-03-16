0f378a1207092b93df0f75d0b3ac17770220833d
Graham Gear <graham@macmini-zoe.janeandgraham.com>
2022-03-16 13:56:39 +0800

    Update asystem-10.100.2864

--- macmini-zoe_macbook-rae/_macos/run.sh
+++ macmini-zoe_macbook-rae/_macos/run.sh
@@ -3,6 +3,7 @@
 ################################################################################
 # Normalise
 ################################################################################
+mkdir -p ~/Temp ~/Code ~/Backup
 rm -rf .zprofile .zsh_history .zsh_sessions
 cat <<EOF >/Users/graham/.bash_profile
 # .bash_profile
@@ -23,7 +24,50 @@ function sshcopyid_func() { cat ~/.ssh/id_rsa.pub | ssh $1 'mkdir .ssh ; cat >>.
 export PATH=/Users/graham/.conda/envs/python2/bin:/Library/Conda/anaconda2/bin:/opt/homebrew/bin:/usr/local/sbin:/usr/local/bin:${PATH}
 
 EOF
+. /Users/graham/.bash_profile
 sed -i '' 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config
 mkdir -p /Users/graham/Backup /Users/graham/Code /Users/graham/Temp
 chown graham /Users/graham/Backup /Users/graham/Code /Users/graham/Temp
 chgrp staff /Users/graham/Backup /Users/graham/Code /Users/graham/Temp
+
+################################################################################
+# Brew
+################################################################################
+! which brew && /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+brew update
+brew upgrade
+brew install \
+	htop \
+	wget \
+	watch
+
+################################################################################
+# Python
+################################################################################
+if [ ! -d /Library/Conda/anaconda2/bin/conda ]; then
+	cd ~/Temp
+	rm -rf /Library/Conda && mkdir -p /Library/Conda && chmod 777 /Library/Conda
+	wget https://repo.anaconda.com/miniconda/Miniconda3-4.5.12-MacOSX-x86_64.sh
+	chmod +x Miniconda3-4.5.12-MacOSX-x86_64.sh
+	./Miniconda3-4.5.12-MacOSX-x86_64.sh -p /Library/Conda/anaconda2 -b
+	rm Miniconda3-4.5.12-MacOSX-x86_64.sh
+	conda config --set channel_priority false
+	conda config --append envs_dirs $HOME/.conda/envs
+	conda create -y -n python2 python=2.7.18
+	conda create -y -n python3 python=3.10
+fi
+
+################################################################################
+# Asystem
+################################################################################
+if [ ! -d ~/Code/asystem ]; then
+	brew install pkg-config poppler
+	pip install fabric
+	cd ~/Code
+	git clone https://github.com/ggear/asystem.git
+	cd asystem
+	fab purge setup
+fi
+
+
+
--- macmini-zoe_macbook-rae/_macos/run_bootstrap.sh
+++ macmini-zoe_macbook-rae/_macos/run_bootstrap.sh
@@ -4,6 +4,7 @@
 # Settings
 ################################################################################
 # Sharing -> Computer Name
+# Sharing -> Computer Name -> Edit
 # Sharing -> Remote Login
 # Security & Privacy -> Firewall -> Off
 # Security & Privacy -> File Vault -> Turn off
@@ -11,19 +12,19 @@
 # Security & Privacy -> Privacy -> Full Disk Access -> Terminal
 # Dock & Menu Bar -> Automatically hide and show the Dock
 # Displays -> More space
-# Displays -> Nightshift -> Sunset to Sunrise
-# Keyboard -> Key repeat
-# Keyboard -> Delay until repeat
+# Displays -> Nightshift -> Sunset to Sunrise -> 75%
+# Keyboard -> Key repeat -> 100%
+# Keyboard -> Delay until repeat -> 80%
 # Keyboard -> Shortcuts -> App Shortcuts -> Show Previous Tab
 # Keyboard -> Shortcuts -> App Shortcuts -> Show Next Tab
-# Sound -> Play sound on startup -> Off
-# Sound -> Play user interface sounds -> Off
-# Sound -> Alert volume -> 0
-# Time Machine -> On
+# Sound -> Alert volume -> 0%
+# Sound -> Play user interface sounds effects -> Off
+# Sound -> Show sound in menu bar -> Always
+# Time Machine -> Back automatically -> On
+# Time Machine -> Show time machine in menu bar -> On
 # Time Machine -> Options -> Ignore
-#   ~/.m2
 #   ~/.conda
-#   ~/.docker
+#   ~/Backup
 #   ~/Code
 #   ~/Music
 #   ~/Temp
@@ -36,7 +37,7 @@
 ################################################################################
 # Terminal
 ################################################################################
-# Profiles -> When the shell exits -> Close the window
+# Profiles -> Shell -> When the shell exits -> Close the window
 # Text -> Change -> SF Mono Regular 14
 # Window -> Columns -> 200
 # Window -> Rows -> 60
@@ -47,7 +48,6 @@
 # Install Calca from App Store
 # Install Infuse from App Store
 # Install WhatsApp from App Store
-# Install Adblock Plus from App Store
 # Install from https://dl.google.com/drive-file-stream/GoogleDrive.dmg
 # Install from https://dl.devmate.com/com.macpaw.CleanMyMac4/CleanMyMacX.dmg
 # Install from https://cdn.bjango.com/files/istatmenus6/istatmenus6.60.zip
@@ -56,31 +56,17 @@
 # Install from https://mirror.aarnet.edu.au/pub/videolan/vlc/3.0.16/macosx/vlc-3.0.16-intel64.dmg
 # Install from https://update-software.sonos.com/software/mac/mdcr/SonosDesktopController1341.dmg
 # Install from https://github.com/newmarcel/KeepingYouAwake/releases/download/1.6.1/KeepingYouAwake-1.6.1.zip
+# Install from https://desktop.docker.com/mac/main/amd64/Docker.dmg
 # Remove all unnecessary apps using Clean My Mac
 
 ################################################################################
 # Init Environment
 ################################################################################
 sudo passwd root
-sudo scutil --set HostName $HOSTNAME
-sudo scutil --set LocalHostName $HOSTNAME
-sudo scutil --set ComputerName $HOSTNAME
 chsh -s /bin/bash
 
-################################################################################
-# Install Python
-################################################################################
-sudo rm -rf /Library/Conda && sudo mkdir -p /Library/Conda && sudo chmod 777 /Library/Conda
-wget https://repo.anaconda.com/miniconda/Miniconda3-4.5.12-MacOSX-x86_64.sh
-chmod +x Miniconda3-4.5.12-MacOSX-x86_64.sh
-./Miniconda3-4.5.12-MacOSX-x86_64.sh -p /Library/Conda/anaconda2 -b
-rm Miniconda3-4.5.12-MacOSX-x86_64.sh
-conda config --set channel_priority false
-conda config --append envs_dirs $HOME/.conda/envs
-conda create -y -n python2 python=2.7.18
-conda create -y -n python3 python=3.10
+# Kiwi settings - notifcations
+# Sublime settings - find/replace/text
+# Docker settings
 
-################################################################################
-# Install Brew
-################################################################################
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
