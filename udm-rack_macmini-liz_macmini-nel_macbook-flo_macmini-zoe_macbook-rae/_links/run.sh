fe03f9a8d9e98b75d11622773dbc2e481184628e
Graham Gear <graham.gear@nowhere.org>
2022-03-17 11:08:32 +0800

    Update asystem-10.100.2880

--- udm-rack_macmini-liz_macmini-nel_macbook-flo_macmini-zoe_macbook-rae/_links/run.sh
+++ udm-rack_macmini-liz_macmini-nel_macbook-flo_macmini-zoe_macbook-rae/_links/run.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-[ ! -L ${$HOME}/home ] && ln -s /home/asystem ${$HOME}/home || true
-[ ! -L ${$HOME}/install ] && ln -s /var/lib/asystem/install ${$HOME}/install || true
+[ ! -L ${HOME}/home ] && ln -s /home/asystem ${HOME}/home || true
+[ ! -L ${HOME}/install ] && ln -s /var/lib/asystem/install ${HOME}/install || true
