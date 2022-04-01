f6bb92ef07d3f24241acd8ea81ecbb09eb171645
Graham Gear <graham.gear@nowhere.org>
2022-04-01 12:33:22 +0800

    Update asystem-10.100.2907

--- macbook-flo/homeassistant/run_pre.sh
+++ macbook-flo/homeassistant/run_pre.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-SERVICE_HOME=/home/asystem/${SERVICE_NAME}/${SERVICE_VERSION_ABSOLUTE}
-SERVICE_INSTALL=/var/lib/asystem/install/*$(hostname)*/${SERVICE_NAME}/${SERVICE_VERSION_ABSOLUTE}
+SERVICE_HOME=/home/asystem/homeassistant/latest
+SERVICE_INSTALL=/var/lib/asystem/install/*$(hostname)*/homeassistant/latest
 
 rm -rf ${SERVICE_HOME}/custom_components && cp -rvf ${SERVICE_INSTALL}/config/custom_components ${SERVICE_HOME}
 rm -rf ${SERVICE_HOME}/custom_packages && cp -rvf ${SERVICE_INSTALL}/config/custom_packages ${SERVICE_HOME}
