067d7d281ec64947279ea3b944712d73c1cd8419
Graham Gear <graham.gear@nowhere.org>
2022-04-28 10:19:43 +0800

    Update asystem-10.100.2940-SNAPSHOT

--- macbook-flo/wrangle/deploy.sh
+++ macbook-flo/wrangle/deploy.sh
@@ -4,4 +4,4 @@ HOSTS_STRING=$(basename $(dirname $(pwd)))
 HOSTS_ARRAY=(${HOSTS_STRING//_/ })
 HOST=$(basename $(dirname $PWD))
 
-ssh root@${HOST} docker exec -e WRANGLE_REPROCESS_ALL_FILES=true --user telegraf wrangle telegraf --debug --once
+ssh root@${HOST} docker exec -e WRANGLE_REPROCESS_ALL_FILES=true --user root wrangle telegraf --debug --once
