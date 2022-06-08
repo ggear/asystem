cdb522491c53664b415166b8546da438b48801c5
Graham Gear <graham.gear@nowhere.org>
2022-06-08 10:54:14 +0800

    Update asystem-10.100.2971-SNAPSHOT

--- macbook-flo/influxdb/src/main/resources/config/delete.sh
+++ macbook-flo/influxdb/src/main/resources/config/delete.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-for MEASUREMENT in W Wh; do
+for MEASUREMENT in sensor__carbon_dioxide; do
     influx delete --org ${INFLUXDB_ORG} --bucket ${INFLUXDB_BUCKET_HOME_PRIVATE} --predicate '_measurement="'${MEASUREMENT}'"' --start '1970-01-01T00:00:00Z' --stop $(date +"%Y-%m-%dT%H:%M:%SZ") -t ${INFLUXDB_TOKEN}
 done
 
