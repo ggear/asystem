22c7497f95742a394d55fc15b3715bf16eade29d
Graham Gear <graham.gear@nowhere.org>
2022-06-13 13:30:00 +0800

    Update influxdb purge

--- macbook-flo/influxdb/src/main/resources/config/delete.sh
+++ macbook-flo/influxdb/src/main/resources/config/delete.sh
@@ -1,10 +1,6 @@
 #!/bin/sh
 
-for FIELD in media_artist_float media_album_name_float; do
-    influx delete --org ${INFLUXDB_ORG} --bucket ${INFLUXDB_BUCKET_HOME_PRIVATE} --predicate '_field="'${FIELD}'"' --start '1970-01-01T00:00:00Z' --stop $(date +"%Y-%m-%dT%H:%M:%SZ") -t ${INFLUXDB_TOKEN}
-done
-
-for MEASUREMENT in sensor__carbon_dioxide; do
+for MEASUREMENT in media_player; do
     influx delete --org ${INFLUXDB_ORG} --bucket ${INFLUXDB_BUCKET_HOME_PRIVATE} --predicate '_measurement="'${MEASUREMENT}'"' --start '1970-01-01T00:00:00Z' --stop $(date +"%Y-%m-%dT%H:%M:%SZ") -t ${INFLUXDB_TOKEN}
 done
 
