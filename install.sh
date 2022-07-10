24ea9080fc3802f90fc47b28e079c069921d9cce
Graham Gear <graham.gear@nowhere.org>
2022-07-10 11:25:50 +0800

    Update asystem-10.100.3039

--- install.sh
+++ install.sh
@@ -42,10 +42,10 @@ if [ -f "docker-compose.yml" ]; then
   echo "----------" && docker ps -f name="${SERVICE_NAME}" && echo "----------"
   sleep 5 && docker logs "${SERVICE_NAME}" && echo "----------"
   if [ $(docker ps -f name="${SERVICE_NAME}" | grep -c "$SERVICE_NAME") -eq 0 ]; then
-    echo && echo "Container failed to start" && echo && exit 1
+    echo && echo "Container failed to start" && echo "" && exit 1
   else
-    echo && echo "Container started ..." && echo && exit 1
     docker system prune --volumes -f -a 2>&1 >/dev/null
+    echo && echo "Container started ..." && echo "----------"
   fi
 fi
 [ -f "./install_post.sh" ] && chmod +x ./install_post.sh && ./install_post.sh
