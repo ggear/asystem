f33e79d2283f0c739089bda7f0173f0b34b0277b
Graham Gear <graham.gear@nowhere.org>
2022-07-18 14:11:22 +0800

    Update asystem-10.100.3121

--- macbook-flo/homeassistant/deploy.sh
+++ macbook-flo/homeassistant/deploy.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
 
-HOST=$(basename $(dirname ${PWD}))
-HOME=$(ssh root@${HOST} "find /home/asystem/$(basename ${PWD}) -maxdepth 1 -mindepth 1 ! -name latest 2>/dev/null | sort | tail -n 1")
-INSTALL=$(ssh root@${HOST} "find /var/lib/asystem/install/${HOST}/$(basename ${PWD}) -maxdepth 1 -mindepth 1 ! -name latest ! -name latest 2>/dev/null | sort | tail -n 1")
+ROOT_DIR="$(
+  cd -- "$(dirname "$0")" >/dev/null 2>&1
+  pwd -P
+)"
+
+HOST=$(basename $(dirname ${ROOT_DIR}))
+HOME=$(ssh root@${HOST} "find /home/asystem/$(basename ${ROOT_DIR}) -maxdepth 1 -mindepth 1 ! -name latest 2>/dev/null | sort | tail -n 1")
+INSTALL=$(ssh root@${HOST} "find /var/lib/asystem/install/${HOST}/$(basename ${ROOT_DIR}) -maxdepth 1 -mindepth 1 ! -name latest ! -name latest 2>/dev/null | sort | tail -n 1")
 
 ssh root@${HOST} "rm -rf ${HOME}/custom_components && rm -rf ${HOME}/custom_packages && rm -rf ${HOME}/ui-lovelace && rm -rf ${HOME}/www"
-scp -r ${PWD}/src/main/resources/config/* root@${HOST}:${HOME}
+scp -r ${ROOT_DIR}/src/main/resources/config/* root@${HOST}:${HOME}
 ssh root@${HOST} "cd ${INSTALL} && docker-compose --compatibility restart"
--- macmini-nel/zigbee2mqtt/deploy.sh
+++ macmini-nel/zigbee2mqtt/deploy.sh
@@ -8,9 +8,12 @@ ROOT_DIR="$(
 export $(xargs <${ROOT_DIR}/.env)
 
 HOST=$(basename $(dirname ${ROOT_DIR}))
+HOME=$(ssh root@${HOST} "find /home/asystem/$(basename ${ROOT_DIR}) -maxdepth 1 -mindepth 1 ! -name latest 2>/dev/null | sort | tail -n 1")
 INSTALL=$(ssh root@${HOST} "find /var/lib/asystem/install/${HOST}/$(basename ${ROOT_DIR}) -maxdepth 1 -mindepth 1 ! -name latest ! -name latest 2>/dev/null | sort | tail -n 1")
 export VERNEMQ_HOST=${VERNEMQ_HOST_PROD}
 
 ${ROOT_DIR}/src/main/resources/config/mqtt.sh
 
+scp -r ${ROOT_DIR}/src/main/resources/config/devices.yaml root@${HOST}:${HOME}
+scp -r ${ROOT_DIR}/src/main/resources/config/groups.yaml root@${HOST}:${HOME}
 ssh root@${HOST} "cd ${INSTALL} && docker-compose --compatibility restart"
