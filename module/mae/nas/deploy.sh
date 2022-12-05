f3e373ccd236653c130f9a55630646467296ccc6
Graham Gear <graham.gear@nowhere.org>
2022-12-05 19:44:41 +0800

    Update asystem-10.100.3311

--- module/mae/nas/deploy.sh
+++ module/mae/nas/deploy.sh
@@ -10,6 +10,6 @@ HOSTS=$(echo $(basename $(dirname $(pwd))) | tr "_" "\n")
 for HOST in ${HOSTS}; do
   HOST="$(grep ${HOST} ${ROOT_DIR}/../../../.hosts | tr '=' ' ' | tr ',' ' ' | awk '{ print $2 }')-${HOST}"
   echo "------------------------------------------------------------" && echo "IMPORT MEDIA: ${HOST}" && echo ""
-  ssh -o StrictHostKeyChecking=no root@${HOST} "\$(find /root/install/usbmedia -maxdepth 1 -mindepth 1 ! -name latest 2>/dev/null | sort | tail -n 1)/config/import.sh"
+  ssh -o StrictHostKeyChecking=no root@${HOST} "/root/install/nas/latest/config/import.sh"
   echo "------------------------------------------------------------"
 done
