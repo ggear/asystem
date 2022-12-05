9932be04b140b056ebf88738ab34171e278a41c2
Graham Gear <graham.gear@nowhere.org>
2022-12-05 15:34:52 +0800

    Update asystem-10.100.3304

--- module/mae_meg_nel_liz_flo/_debian-amd64/install.sh
+++ module/mae_meg_nel_liz_flo/_debian-amd64/install.sh
@@ -5,4 +5,4 @@
 ################################################################################
 apt-get update
 apt-get install -y --allow-downgrades 'intel-microcode=3.20220510.1~deb11u1'
-apt-get install -y --allow-downgrades 'libc6-i386=2.31-13+deb11u3'
+apt-get install -y --allow-downgrades 'libc6-i386=2.31-13+deb11u5'
