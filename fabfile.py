b310eeeb0a07f3bc64329bc3a2d2196587b19a8b
Graham Gear <graham.gear@nowhere.org>
2022-12-07 16:14:21 +0800

    Update asystem-10.100.3334

--- fabfile.py
+++ fabfile.py
@@ -248,8 +248,8 @@ def _package(context, filter_module=None, is_release=False):
     for module in _get_modules(context, "Dockerfile", filter_module=filter_module):
         _print_header(module, "package")
 
-
-        print(HOSTS[_get_hosts(context, module)[0]])
+        print(HOSTS)
+        print(_get_hosts(context, module)[0])
         sys.exit()
         # docker buildx build --platform linux/arm64 --output type=docker --tag weewx:10.100.3331 .
         _run_local(context, "docker image build --build-arg PYTHON_VERSION -t {}:{} .".format(_name(module), _get_versions()[0]), module)
