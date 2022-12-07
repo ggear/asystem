cd0e0426b63f4a9ef943d67bf421e43be4114749
Graham Gear <graham.gear@nowhere.org>
2022-12-07 21:05:26 +0800

    Update asystem-10.100.3351

--- module/mae/letsencrypt/src/main/resources/pushcerts_copy.sh
+++ module/mae/letsencrypt/src/main/resources/pushcerts_copy.sh
@@ -9,10 +9,10 @@ cd "${SERVICE_HOME}" || exit
 
 scp -o "StrictHostKeyChecking=no" ./certificates/privkey.pem root@${NGINX_HOST}:/home/asystem/nginx/latest/.key.pem
 scp -o "StrictHostKeyChecking=no" ./certificates/fullchain.pem root@${NGINX_HOST}:/home/asystem/nginx/latest/certificate.pem
-ssh -qo "StrictHostKeyChecking=no" root@${NGINX_HOST} "cd /var/lib/asystem/install/nginx/latest && docker-compose --compatibility restart"
+ssh -qno "StrictHostKeyChecking=no" root@${NGINX_HOST} "cd /var/lib/asystem/install/nginx/latest && docker-compose --compatibility restart"
 logger -t pushcerts "Loaded new nginx certificates on ${NGINX_HOST}"
 
 scp -o "StrictHostKeyChecking=no" ./certificates/privkey.pem root@${UDMUTILITIES_HOST}:/mnt/data/unifi-os/unifi-core/config/unifi-core.key
 scp -o "StrictHostKeyChecking=no" ./certificates/fullchain.pem root@${UDMUTILITIES_HOST}:/mnt/data/unifi-os/unifi-core/config/unifi-core.crt
-ssh -qo "StrictHostKeyChecking=no" root@${UDMUTILITIES_HOST} "unifi-os restart"
+ssh -qno "StrictHostKeyChecking=no" root@${UDMUTILITIES_HOST} "unifi-os restart"
 logger -t pushcerts "Loaded new unifi certificates on ${UDMUTILITIES_HOST}"
