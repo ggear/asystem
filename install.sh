06b1aafb34170f9e96de8e2b3ea0470d57ea826c
Graham Gear <graham.gear@nowhere.org>
2022-12-08 17:00:11 +0800

    Update asystem-10.100.3380

--- install.sh
+++ install.sh
@@ -11,7 +11,7 @@ SERVICE_HOME_OLDEST=$(find $(dirname ${SERVICE_HOME}) -maxdepth 1 -mindepth 1 ! 
 SERVICE_INSTALL=/var/lib/asystem/install/${SERVICE_NAME}/${SERVICE_VERSION_ABSOLUTE}
 
 cd ${SERVICE_INSTALL} || exit
-#[ -f "./install_prep.sh" ] && chmod +x ./install_prep.sh && ./install_prep.sh || true
+[ -f "./install_prep.sh" ] && chmod +x ./install_prep.sh && ./install_prep.sh || true
 #cd ${SERVICE_INSTALL} || exit
 #[ -f "${SERVICE_NAME}-${SERVICE_VERSION_ABSOLUTE}.tar.gz" ] && docker image load -i ${SERVICE_NAME}-${SERVICE_VERSION_ABSOLUTE}.tar.gz
 #if [ -f "docker-compose.yml" ]; then
