d5f583c1d5e0447f1ef89d76e50164bfba111b9b
Graham Gear <graham.gear@nowhere.org>
2022-12-08 19:56:06 +0800

    Update asystem-10.100.3390

--- module/net/udmutilities/src/main/resources/config/udm-cloudflare-ddns/13-cloudflare-ddns.sh
+++ module/net/udmutilities/src/main/resources/config/udm-cloudflare-ddns/13-cloudflare-ddns.sh
@@ -1,12 +1,3 @@
 #!/bin/sh
 
-CONTAINER=cloudflare-ddns
-
-podman stop "${CONTAINER}" 2>/dev/null
-podman rm "${CONTAINER}" 2>/dev/null
-podman create --restart always \
-  --name "${CONTAINER}" \
-  -e TZ="Australia/Perth" \
-  -v "/mnt/data/udm-cloudflare-ddns/config.json:/config.json" \
-  --security-opt=no-new-privileges \
-  timothyjmiller/cloudflare-ddns:latest
+cp -rvf /root/install/udmutilities/latest/config/udm-cloudflare-ddns/ddns-eth8-inadyn.conf /run/ddns-eth8-inadyn.conf
