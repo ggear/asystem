eec21a84efa80b937d0abd2ff29d16baa620ddd4
Graham Gear <graham.gear@nowhere.org>
2022-12-08 17:03:01 +0800

    Update asystem-10.100.3382

--- module/net/udmutilities/install_prep.sh
+++ module/net/udmutilities/install_prep.sh
@@ -9,9 +9,9 @@ mkdir -p /var/lib/asystem
 mkdir -p /mnt/data/asystem/install
 if [[ ! -L /var/lib/asystem/install ]]; then
   if [[ -d /var/lib/asystem/install ]]; then
-    cp -rvf /var/lib/asystem/install/ /mnt/data/asystem
+ #   cp -rvf /var/lib/asystem/install/ /mnt/data/asystem
     rm -rf /var/lib/asystem/install
   fi
   ln -s /mnt/data/asystem/install /var/lib/asystem/install
 fi
-cp -rvf /mnt/data/asystem/install/ /var/lib/asystem
+#cp -rvf /mnt/data/asystem/install/ /var/lib/asystem
