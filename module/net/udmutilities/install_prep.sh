69b8c88b30387ddad739cf8cd57b39ed8b4d9628
Graham Gear <graham.gear@nowhere.org>
2022-12-08 16:01:55 +0800

    Update asystem-10.100.3370

--- module/net/udmutilities/install_prep.sh
+++ module/net/udmutilities/install_prep.sh
@@ -6,11 +6,11 @@ SERVICE_INSTALL=/var/lib/asystem/install/${SERVICE_NAME}/${SERVICE_VERSION_ABSOL
 cd ${SERVICE_INSTALL} || exit
 
 mkdir -p /mnt/data/asystem
-if [[ ! -L /var/lib/asystem ]]; then
-  if [[ -d /var/lib/asystem ]]; then
-    mv /var/lib/asystem/* /mnt/data/asystem
-    rm -r /mnt/data/asystem
-  fi
-  ln -s /mnt/data/asystem /var/lib/asystem
-fi
-cp -rvf /mnt/data/asystem/* /var/lib/asystem
+#if [[ ! -L /var/lib/asystem ]]; then
+#  if [[ -d /var/lib/asystem ]]; then
+#    mv /var/lib/asystem/* /mnt/data/asystem
+#    rm -r /mnt/data/asystem
+#  fi
+#  ln -s /mnt/data/asystem /var/lib/asystem
+#fi
+#cp -rvf /mnt/data/asystem/* /var/lib/asystem
