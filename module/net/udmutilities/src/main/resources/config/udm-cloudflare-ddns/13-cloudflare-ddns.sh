c3371a761faff0aefd815479ded6119042eec67b
Graham Gear <graham.gear@nowhere.org>
2022-12-08 20:02:50 +0800

    Update asystem-10.100.3392-SNAPSHOT

--- module/net/udmutilities/src/main/resources/config/udm-cloudflare-ddns/13-cloudflare-ddns.sh
+++ module/net/udmutilities/src/main/resources/config/udm-cloudflare-ddns/13-cloudflare-ddns.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 
 cp -rvf /root/install/udmutilities/latest/config/udm-cloudflare-ddns/ddns-eth8-inadyn.conf /run/ddns-eth8-inadyn.conf
+rm -rf /.inadyn
+rm -rf /root/.inadyn
+time /usr/sbin/inadyn -n -s -C -f /run/ddns-eth8-inadyn.conf -1 -l debug --foreground
