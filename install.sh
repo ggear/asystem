917697c48d67f1138e8a0864aa7f474d5c7a4523
Graham Gear <graham.gear@nowhere.org>
2022-12-08 17:09:10 +0800

    Update asystem-10.100.3386

--- install.sh
+++ install.sh
@@ -12,42 +12,42 @@ SERVICE_INSTALL=/var/lib/asystem/install/${SERVICE_NAME}/${SERVICE_VERSION_ABSOL
 
 cd ${SERVICE_INSTALL} || exit
 [ -f "./install_prep.sh" ] && chmod +x ./install_prep.sh && ./install_prep.sh || true
-#cd ${SERVICE_INSTALL} || exit
-#[ -f "${SERVICE_NAME}-${SERVICE_VERSION_ABSOLUTE}.tar.gz" ] && docker image load -i ${SERVICE_NAME}-${SERVICE_VERSION_ABSOLUTE}.tar.gz
-#if [ -f "docker-compose.yml" ]; then
-#  docker stop "${SERVICE_NAME}" >/dev/null 2>&1
-#  docker stop "${SERVICE_NAME}" >/dev/null 2>&1
-#  docker wait "${SERVICE_NAME}" >/dev/null 2>&1
-#  docker wait "${SERVICE_NAME}" >/dev/null 2>&1
-#  docker system prune --volumes -f 2>&1 >/dev/null
-#fi
-#if [ ! -d "$SERVICE_HOME" ]; then
-#  if [ -d "$SERVICE_HOME_OLD" ]; then
-#    echo "Copying old home to new ... "
-#    cp -rfp "$SERVICE_HOME_OLD" "$SERVICE_HOME"
-#  else
-#    mkdir -p "${SERVICE_HOME}"
-#    chmod 777 "${SERVICE_HOME}"
-#  fi
-#  rm -rf $SERVICE_HOME_OLDEST
-#fi
-#rm -f ${SERVICE_HOME}/../latest && ln -sfv ${SERVICE_HOME} ${SERVICE_HOME}/../latest
-#[ "$(ls -A config | wc -l)" -gt 0 ] && cp -rfp $(find config -mindepth 1 -maxdepth 1) "${SERVICE_HOME}"
-#touch .env
-#chmod 600 .env
-#[ -f "./install_pre.sh" ] && chmod +x ./install_pre.sh && ./install_pre.sh || true
-#if [ -f "docker-compose.yml" ]; then
-#  docker-compose --compatibility --no-ansi up --force-recreate -d
-#  if [ $(docker ps | grep "${SERVICE_NAME}_bootstrap" | wc -l) -eq 1 ]; then
-#    docker logs "${SERVICE_NAME}_bootstrap" -f
-#  fi
-#  echo "----------" && docker ps -f name="${SERVICE_NAME}" && echo "----------"
-#  sleep 5 && docker logs "${SERVICE_NAME}" && echo "----------"
-#  if [ $(docker ps -f name="${SERVICE_NAME}" | grep -c "$SERVICE_NAME") -eq 0 ]; then
-#    echo && echo "Container failed to start" && echo "" && exit 1
-#  else
-#    docker system prune --volumes -f -a 2>&1 >/dev/null
-#    echo "Container started successfully ..." && echo "----------"
-#  fi
-#fi
-#[ -f "./install_post.sh" ] && chmod +x ./install_post.sh && ./install_post.sh || true
+cd ${SERVICE_INSTALL} || exit
+[ -f "${SERVICE_NAME}-${SERVICE_VERSION_ABSOLUTE}.tar.gz" ] && docker image load -i ${SERVICE_NAME}-${SERVICE_VERSION_ABSOLUTE}.tar.gz
+if [ -f "docker-compose.yml" ]; then
+  docker stop "${SERVICE_NAME}" >/dev/null 2>&1
+  docker stop "${SERVICE_NAME}" >/dev/null 2>&1
+  docker wait "${SERVICE_NAME}" >/dev/null 2>&1
+  docker wait "${SERVICE_NAME}" >/dev/null 2>&1
+  docker system prune --volumes -f 2>&1 >/dev/null
+fi
+if [ ! -d "$SERVICE_HOME" ]; then
+  if [ -d "$SERVICE_HOME_OLD" ]; then
+    echo "Copying old home to new ... "
+    cp -rfp "$SERVICE_HOME_OLD" "$SERVICE_HOME"
+  else
+    mkdir -p "${SERVICE_HOME}"
+    chmod 777 "${SERVICE_HOME}"
+  fi
+  rm -rf $SERVICE_HOME_OLDEST
+fi
+rm -f ${SERVICE_HOME}/../latest && ln -sfv ${SERVICE_HOME} ${SERVICE_HOME}/../latest
+[ "$(ls -A config | wc -l)" -gt 0 ] && cp -rfp $(find config -mindepth 1 -maxdepth 1) "${SERVICE_HOME}"
+touch .env
+chmod 600 .env
+[ -f "./install_pre.sh" ] && chmod +x ./install_pre.sh && ./install_pre.sh || true
+if [ -f "docker-compose.yml" ]; then
+  docker-compose --compatibility --no-ansi up --force-recreate -d
+  if [ $(docker ps | grep "${SERVICE_NAME}_bootstrap" | wc -l) -eq 1 ]; then
+    docker logs "${SERVICE_NAME}_bootstrap" -f
+  fi
+  echo "----------" && docker ps -f name="${SERVICE_NAME}" && echo "----------"
+  sleep 5 && docker logs "${SERVICE_NAME}" && echo "----------"
+  if [ $(docker ps -f name="${SERVICE_NAME}" | grep -c "$SERVICE_NAME") -eq 0 ]; then
+    echo && echo "Container failed to start" && echo "" && exit 1
+  else
+    docker system prune --volumes -f -a 2>&1 >/dev/null
+    echo "Container started successfully ..." && echo "----------"
+  fi
+fi
+[ -f "./install_post.sh" ] && chmod +x ./install_post.sh && ./install_post.sh || true
