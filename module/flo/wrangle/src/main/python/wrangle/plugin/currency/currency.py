862eea2bc78cc6c4aef72842fd26da9afa76ffc7
Graham Gear <graham.gear@nowhere.org>
2023-02-05 12:52:52 +0800

    Update asystem-10.100.3491

--- module/flo/wrangle/src/main/python/wrangle/plugin/currency/currency.py
+++ module/flo/wrangle/src/main/python/wrangle/plugin/currency/currency.py
@@ -139,7 +139,7 @@ class Currency(library.Library):
                         self.add_counter(library.CTR_SRC_FILES, library.CTR_ACT_SKIPPED)
                 else:
                     self.add_counter(library.CTR_SRC_FILES, library.CTR_ACT_ERRORED)
-            if datetime.datetime.now().year > 2022:
+            if datetime.datetime.now().year > 2023:
                 self.print_log("Error processing RBA data, need to increment RBA_YEARS for new current file")
                 self.add_counter(library.CTR_SRC_FILES, library.CTR_ACT_ERRORED, 1)
                 return
