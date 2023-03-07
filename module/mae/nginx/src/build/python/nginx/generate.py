4b584b06c5c32529d3696a1469c58a99417f88b2
Graham Gear <graham.gear@nowhere.org>
2023-03-07 13:07:30 +0800

    Update asystem-10.100.3590

--- module/mae/nginx/src/build/python/nginx/generate.py
+++ module/mae/nginx/src/build/python/nginx/generate.py
@@ -41,10 +41,10 @@ http {
   access_log on;
   server_tokens off;
 
-  client_body_buffer_size 1k
-  client_header_buffer_size 1k
-  client_max_body_size 1k
-  large_client_header_buffers 2 1k
+  client_body_buffer_size 1k;
+  client_header_buffer_size 1k;
+  client_max_body_size 1k;
+  large_client_header_buffers 2 1k;
 
   gzip on;
   gzip_vary on;
