277889d743da178e7cb17c42f5db571d2ee04608
Graham Gear <graham.gear@nowhere.org>
2023-06-16 16:03:40 +0800

    Update asystem-10.100.3849

--- src/meg/_macmini/install.sh
+++ src/meg/_macmini/install.sh
@@ -1,2 +1,9 @@
 #!/bin/sh
 
+################################################################################
+# Intel network card
+################################################################################
+if [ -f /etc/default/grub ] && [ $(grep "intel_iommu=on iommu=pt" /etc/default/grub | wc -l) -eq 0 ]; then
+  sed -i 's/GRUB_CMDLINE_LINUX_DEFAULT="quiet/GRUB_CMDLINE_LINUX_DEFAULT="quiet intel_iommu=on iommu=pt/' /etc/default/grub
+  update-grub
+fi
