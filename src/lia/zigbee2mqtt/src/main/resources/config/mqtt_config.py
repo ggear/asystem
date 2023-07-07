9b11391630c02b98fc969ea689e4f1559dd22db3
Graham Gear <graham.gear@nowhere.org>
2023-07-07 09:12:41 +0800

    Update asystem-10.100.3986

--- src/lia/zigbee2mqtt/src/main/resources/config/mqtt_config.py
+++ src/lia/zigbee2mqtt/src/main/resources/config/mqtt_config.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
 
-import time
 import json
 import os
 import sys
 import threading
+import time
 from typing import Any
 from typing import Optional
 
@@ -25,7 +25,7 @@ if __name__ == "__main__":
     mqtt_port = int(os.environ["VERNEMQ_PORT"])
 
 
-    def _set(topic: str, message: str, **mqtt_kwargs):
+    def _mqtt_pub(topic: str, message: str, **mqtt_kwargs):
 
         def on_publish(client, userdata):
             client.disconnect()
@@ -51,7 +51,7 @@ if __name__ == "__main__":
         return code[0]
 
 
-    def _get(topic: str, **mqtt_kwargs):
+    def _mqtt_sub(topic: str, **mqtt_kwargs):
 
         def on_connect(client, userdata, flags, rc):
             client.subscribe(userdata["topic"])
@@ -84,45 +84,69 @@ if __name__ == "__main__":
         return None if userdata["messages"] is None else json.loads(userdata["messages"].payload)
 
 
-    device_availability_dict = _get("zigbee/{}/availability".format(device_name))
-    if device_availability_dict is not None and device_availability_dict["state"] == "online":
+    def _device_available():
+        device_availability_dict = _mqtt_sub("zigbee/{}/availability".format(device_name))
+        return device_availability_dict is not None and device_availability_dict["state"] == "online"
 
-        def _config():
-            grouped = False
-            group_dict = None
-            for group_dict in _get("zigbee/bridge/groups"):
-                if "friendly_name" in group_dict and group_dict["friendly_name"] == device_group:
+
+    def _device_config():
+        grouped = False
+        group_dict = None
+        for group_dict in _mqtt_sub("zigbee/bridge/groups"):
+            if "friendly_name" in group_dict and group_dict["friendly_name"] == device_group:
+                break
+        if group_dict is not None and "members" in group_dict:
+            for group_member_dict in group_dict["members"]:
+                if "ieee_address" in group_member_dict and group_member_dict["ieee_address"] == device_address:
+                    grouped = True
                     break
-            if group_dict is not None and "members" in group_dict:
-                for group_member_dict in group_dict["members"]:
-                    if "ieee_address" in group_member_dict and group_member_dict["ieee_address"] == device_address:
-                        grouped = True
-                        break
-            if grouped:
-                print("Device [{}] configured, no action required [SUCCESS]".format(device_name))
-                return
-            else:
-                if device_config != "":
-                    print("Device [{}] config pushed [{}]"
-                          .format(device_name,
-                                  "SUCCESS" if _set("zigbee/{}/set".format(device_name), device_config) == 0 else "FAILED"))
-                print("Device [{}] added to group [{}] [{}]"
-                      .format(device_name, device_group,
-                              "SUCCESS" if _set("zigbee/bridge/request/group/members/add"
-                                                .format(device_name), device_config_group) == 0 else "FAILED"))
-                time.sleep(2)
-                return _config()
+        if grouped:
+            print("[INFO] Device [{}] configured, no action required".format(device_name))
+            return
+        else:
+            if device_config != "":
+                print("[{}] Device [{}] config pushed".format(
+                    "INFO" if _mqtt_pub("zigbee/{}/set".format(device_name), device_config) == 0 else "FAIL",
+                    device_name,
+                ))
+            print("[{}] Device [{}] added to group [{}]".format(
+                "INFO" if _mqtt_pub("zigbee/bridge/request/group/members/add".format(device_name), device_config_group) == 0 else "FAIL",
+                device_name,
+                device_group,
+            ))
+            time.sleep(2)
+            return _device_config()
 
 
-        _config()
-        for group_dict in _get("zigbee/bridge/groups"):
+    def _device_clean():
+        for group_dict in _mqtt_sub("zigbee/bridge/groups"):
             if "friendly_name" in group_dict and group_dict["friendly_name"] != device_group:
                 if "members" in group_dict:
                     for group_member_dict in group_dict["members"]:
                         if "ieee_address" in group_member_dict and group_member_dict["ieee_address"] == device_address:
-                            print("Device [{}] removed from group [{}] [{}]"
-                                  .format(device_name, device_group,
-                                          "SUCCESS" if _set("zigbee2mqtt/bridge/request/group/members/remove"
-                                                            .format(device_name), device_config_group) == 0 else "FAILED"))
+                            print("[{}] Device [{}] removed from group [{}]".format(
+                                "INFO" if _mqtt_pub("zigbee2mqtt/bridge/request/group/members/remove"
+                                                    .format(device_name), device_config_group) == 0 else "FAIL",
+                                device_name,
+                                device_group,
+                            ))
+                            _device_clean()
+
+
+    def _device_skip():
+        print("[INFO] Device [{}] not available currently, skipping config".format(device_name))
+
+
+    def _device_verify():
+        for group_dict in _mqtt_sub("zigbee/bridge/groups"):
+            if "friendly_name" in group_dict and group_dict["friendly_name"] == device_group:
+                if "members" in group_dict and len(group_dict["members"]) == 0:
+                    print("[FAIL] Device [{}] group [{}] does not have any member devices".format(device_name, group_dict["friendly_name"]))
+
+
+    if _device_available():
+        _device_clean()
+        _device_config()
     else:
-        print("Device [{}] not available currently, skipping config [SUCCESS]".format(device_name))
+        _device_skip()
+    _device_verify()
