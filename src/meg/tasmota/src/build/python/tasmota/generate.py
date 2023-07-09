3690217a7c4c28fca8d90526242b7dc6578ef63d
Graham Gear <graham.gear@nowhere.org>
2023-07-09 21:51:51 +0800

    Update tasmota

--- src/meg/tasmota/src/build/python/tasmota/generate.py
+++ src/meg/tasmota/src/build/python/tasmota/generate.py
@@ -9,11 +9,13 @@ for dir_module in glob.glob("{}/../../*/*".format(DIR_ROOT)):
     if dir_module.endswith("homeassistant"):
         sys.path.insert(0, "{}/src/build/python".format(dir_module))
 
+from homeassistant.generate import load_env
 from homeassistant.generate import load_entity_metadata
 
 pd.options.mode.chained_assignment = None
 
 if __name__ == "__main__":
+    env = load_env(DIR_ROOT)
     metadata_df = load_entity_metadata()
 
     metadata_tasmota_df = metadata_df[
@@ -22,6 +24,9 @@ if __name__ == "__main__":
         (metadata_df["device_via_device"] == "Tasmota") &
         (metadata_df["entity_namespace"] == "switch") &
         (metadata_df["unique_id"].str.len() > 0) &
+        (metadata_df["device_model"].str.len() > 0) &
+        (metadata_df["device_manufacturer"].str.len() > 0) &
+        (metadata_df["custom_config"].str.len() > 0) &
         (metadata_df["connection_ip"].str.len() > 0)
         ].sort_values("connection_ip")
     metadata_tasmota_dicts = [row.dropna().to_dict() for index, row in metadata_tasmota_df.iterrows()]
@@ -34,16 +39,49 @@ if __name__ == "__main__":
 #######################################################################################
         """.strip() + "\n\n")
         for metadata_tasmota_dict in metadata_tasmota_dicts:
-            tasmota_device_path = os.path.join(DIR_ROOT, "src/build/resources/devices/", metadata_tasmota_dict["unique_id"] + ".json")
-            if not os.path.exists(tasmota_device_path):
-                os.system("decode-config.py -s {} -o {} --json-indent 2".format(
+            tasmota_device_path = os.path.join(DIR_ROOT, "src/build/resources/devices/", metadata_tasmota_dict["unique_id"])
+            with open(tasmota_device_path + ".json", "wt") as tasmota_device_file:
+                tasmota_device_file.write("""
+{{
+  "templatename": "{} {}",
+  "user_template": {},
+  "devicename": "{}",
+  "friendlyname": [ "{}" ],
+  "mqtt_host": "{}",
+  "mqtt_port": {},
+  "mqtt_client": "DVES_%06X",
+  "mqtt_grptopic": "tasmotas",
+  "mqtt_topic": "{}",
+  "mqtt_fulltopic": "tasmota/device/%topic%/%prefix%/",
+  "mqtt_prefix": [ "cmnd", "stat", "tele" ],
+  "mqtt_retry": 10,
+  "mqtt_keepalive": 30,
+  "mqtt_socket_timeout": 4,
+  "mqtt_user": "DVES_USER",
+  "mqtt_pwd": "DVES_PASS"
+}}
+                """.format(
+                    metadata_tasmota_dict["device_manufacturer"],
+                    metadata_tasmota_dict["device_model"],
+                    metadata_tasmota_dict["custom_config"],
+                    metadata_tasmota_dict["unique_id"],
+                    metadata_tasmota_dict["friendly_name"],
+                    env["VERNEMQ_IP_PROD"],
+                    env["VERNEMQ_PORT"],
+                    metadata_tasmota_dict["unique_id"],
+                ).strip() + "\n\n")
+            tasmota_config_file.write(
+                "echo 'Processing config for device [{}] at [http://{}/cn] ... '\n".format(
+                    metadata_tasmota_dict["unique_id"],
+                    metadata_tasmota_dict["connection_ip"],
+                ))
+            tasmota_config_file.write(
+                "decode-config.py -s {} -o {}-backup.json --json-indent 2\n".format(
                     metadata_tasmota_dict["connection_ip"],
                     tasmota_device_path,
                 ))
             tasmota_config_file.write(
-                "echo 'Processing config for device [{}] at [http://{}/cn] ... ' && decode-config.py -s {} -i {}\n".format(
-                    metadata_tasmota_dict["unique_id"],
-                    metadata_tasmota_dict["connection_ip"],
+                "decode-config.py -s {} -i {}.json\n".format(
                     metadata_tasmota_dict["connection_ip"],
                     tasmota_device_path,
                 ))
--- src/meg/tasmota/src/build/resources/tasmota_config.sh
+++ src/meg/tasmota/src/build/resources/tasmota_config.sh
@@ -3,5 +3,6 @@
 # WARNING: This file is written to by the build process, any manual edits will be lost!
 #######################################################################################
 
-echo 'Processing config for device [rack_fans] at [http://10.0.6.93/cn] ... ' && decode-config.py -s 10.0.6.93 -i /Users/graham/Code/asystem/src/meg/tasmota/src/build/resources/devices/rack_fans.json
-echo 'Processing config for device [roof_water_heater_booster] at [http://10.0.6.94/cn] ... ' && decode-config.py -s 10.0.6.94 -i /Users/graham/Code/asystem/src/meg/tasmota/src/build/resources/devices/roof_water_heater_booster.json
+echo 'Processing config for device [rack_fans] at [http://10.0.6.93/cn] ... '
+decode-config.py -s 10.0.6.93 -o /Users/graham/Code/asystem/src/meg/tasmota/src/build/resources/devices/rack_fans-backup.json --json-indent 2
+decode-config.py -s 10.0.6.93 -i /Users/graham/Code/asystem/src/meg/tasmota/src/build/resources/devices/rack_fans.json
