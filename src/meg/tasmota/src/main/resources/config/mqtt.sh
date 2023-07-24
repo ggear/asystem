ab35e233cb3e5bdf0cc40855ee0ae403f5fa8d1f
Graham Gear <graham.gear@nowhere.org>
2023-07-24 20:28:15 +0800

    Update

--- src/meg/tasmota/src/main/resources/config/mqtt.sh
+++ src/meg/tasmota/src/main/resources/config/mqtt.sh
@@ -4,9 +4,7 @@ ROOT_DIR="$(dirname $(readlink -f "$0"))/mqtt"
 
 printf "Entity Metadata publish script dropping topics:\n"
 mosquitto_sub -h ${VERNEMQ_HOST} -p ${VERNEMQ_PORT} --remove-retained -F '%t' -t 'tasmota/#' -W 1 2>/dev/null
-mosquitto_sub -h ${VERNEMQ_HOST} -p ${VERNEMQ_PORT} --remove-retained -F '%t' -t 'haas/entity/light/tasmota/#' -W 1 2>/dev/null
-mosquitto_sub -h ${VERNEMQ_HOST} -p ${VERNEMQ_PORT} --remove-retained -F '%t' -t 'haas/entity/switch/tasmota/#' -W 1 2>/dev/null
-mosquitto_sub -h ${VERNEMQ_HOST} -p ${VERNEMQ_PORT} --remove-retained -F '%t' -t 'haas/entity/sensor/tasmota/#' -W 1 2>/dev/null
+mosquitto_sub -h ${VERNEMQ_HOST} -p ${VERNEMQ_PORT} --remove-retained -F '%t' -t 'haas/entity/+/tasmota/#' -W 1 2>/dev/null
 printf "Entity Metadata publish script dropping topics complete\n\n"
 
 printf "Entity Metadata publish script sleeping before publishing ... " && sleep 2  && printf "done\n\n"
