c5888570a36ab82017d2cdef376cfad6b7117df7
Graham Gear <graham.gear@nowhere.org>
2023-07-28 11:28:10 +0800

    Update asystem-10.100.4067-SNAPSHOT

---
+++ src/meg_flo_lia/digitemp/src/build/python/digitemp/generate.py
@@ -0,0 +1,28 @@
+import glob
+import json
+import os
+import shutil
+import sys
+
+DIR_ROOT = os.path.abspath("{}/../../../..".format(os.path.dirname(os.path.realpath(__file__))))
+for dir_module in glob.glob("{}/../../*/*".format(DIR_ROOT)):
+    if dir_module.endswith("homeassistant"):
+        sys.path.insert(0, "{}/src/build/python".format(dir_module))
+
+from homeassistant.generate import load_entity_metadata
+from homeassistant.generate import write_entity_metadata
+
+if __name__ == "__main__":
+    metadata_df = load_entity_metadata()
+
+    # Build metadata publish JSON
+    metadata_publish_df = metadata_df[
+        (metadata_df["index"] > 0) &
+        (metadata_df["entity_status"] == "Enabled") &
+        (metadata_df["device_via_device"] == "Internet") &
+        (metadata_df["unique_id"].str.len() > 0) &
+        (metadata_df["name"].str.len() > 0) &
+        (metadata_df["discovery_topic"].str.len() > 0)
+        ]
+    write_entity_metadata("internet", DIR_ROOT, metadata_publish_df)
+
