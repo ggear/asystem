b828ebc78532f1ac511054590a48f86434dc36c2
Graham Gear <graham.gear@nowhere.org>
2023-07-28 11:24:25 +0800

    Update asystem-10.100.4066

--- src/flo/vernemq/deploy.sh
+++ src/flo/vernemq/deploy.sh
@@ -8,7 +8,7 @@ printf "Entity Metadata publish script dropping topics:\n"
 mosquitto_sub -h ${VERNEMQ_IP_PROD} -p ${VERNEMQ_PORT} --remove-retained -F '%t' -t '#' -W 1 2>/dev/null
 printf "Entity Metadata publish script dropping topics complete\n\n"
 
-printf "Entity Metadata publish script sleeping before publishing ... " && sleep 2  && printf "done\n\n"
+printf "Entity Metadata publish script sleeping before publishing ... " && sleep 2 && printf "done\n\n"
 
 ${ROOT_DIR}/../../lia/weewx/deploy.sh
 ${ROOT_DIR}/../../meg/tasmota/deploy.sh
--- src/lia/weewx/src/main/resources/config/mqtt.sh
+++ src/lia/weewx/src/main/resources/config/mqtt.sh
@@ -7,7 +7,7 @@ mosquitto_sub -h ${VERNEMQ_HOST} -p ${VERNEMQ_PORT} --remove-retained -F '%t' -t
 mosquitto_sub -h ${VERNEMQ_HOST} -p ${VERNEMQ_PORT} --remove-retained -F '%t' -t 'haas/entity/sensor/weewx/#' -W 1 2>/dev/null
 printf "Entity Metadata publish script dropping topics complete\n\n"
 
-printf "Entity Metadata publish script sleeping before publishing ... " && sleep 2  && printf "done\n\n"
+printf "Entity Metadata publish script sleeping before publishing ... " && sleep 2 && printf "done\n\n"
 
 printf "Entity Metadata publish script publishing topics:\n"
 find ${ROOT_DIR} -name "*.json" -print0 | while read -d $'\0' METADATA_FILE; do
