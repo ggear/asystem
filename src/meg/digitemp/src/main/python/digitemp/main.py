07ddd2d9fadee2e07ea34b4b7a7d90259de7fc6d
Graham Gear <graham.gear@nowhere.org>
2023-07-30 08:58:39 +0800

    Update asystem-10.100.4075

---
+++ src/meg/digitemp/src/main/python/digitemp/main.py
@@ -0,0 +1,20 @@
+import time
+
+from digitemp.device import TemperatureSensor
+from digitemp.master import UART_Adapter
+
+FORMAT_TEMPLATE = "digitemp,metric=temperature,rom_id={},run_code={}{},run_ms={} {}"
+
+if __name__ == "__main__":
+    bus = UART_Adapter('/dev/ttyS0')
+    sensor = TemperatureSensor(bus, rom='108739A80208006F')
+    sensor.info()
+    print(sensor.get_temperature())
+
+    print(FORMAT_TEMPLATE.format(
+        "108739A80208006F",
+        0,
+        ",temp_cel={}".format(10),
+        "10",
+        int(time.time() * 1000000000),
+    ))
