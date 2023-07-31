8d0f1bc09cb3d029f5f76b655237ca8a272c54c7
Graham Gear <graham.gear@nowhere.org>
2023-07-31 15:59:53 +0800

    Update asystem-10.100.4102

--- src/meg/digitemp/src/main/python/digitemp/main.py
+++ src/meg/digitemp/src/main/python/digitemp/main.py
@@ -1,15 +1,16 @@
 import json
+import traceback
 from os.path import *
+
 import sys
 import time
 from digitemp.device import TemperatureSensor
 from digitemp.master import UART_Adapter
-import traceback
 
 SERIAL_DEVICE = "/dev/ttyUSBTempProbe"
 STACKTRACE_REFERENCE_LIMIT = 1
 
-FORMAT_TEMPLATE = "digitemp,metric={},rom_id={},run_code={},run_ms={},temp_cel={} {}"
+FORMAT_TEMPLATE = "digitemp,metric={},rom_id={},run_code={}{},run_ms={} {}"
 
 if __name__ == "__main__":
     metadata_digitemp_path = abspath(join("/asystem/runtime/sensors.json"))
@@ -20,16 +21,20 @@ if __name__ == "__main__":
             run_time_start = time.time()
             try:
                 temperature = TemperatureSensor(UART_Adapter(SERIAL_DEVICE),
-                                                rom=metadata_digitemp_dict["connection_mac"].replace("0x", "")).get_temperature()
+                                                metadata_digitemp_dict["connection_mac"].replace("0x", "")).get_temperature()
             except Exception as error:
-                print("Error getting temperature sensor value - ", end="", file=sys.stderr)
+                print("Error getting temperature sensor with name [{}], ID [{}] and serial device [{}] value:".format(
+                    metadata_digitemp_dict["unique_id"],
+                    metadata_digitemp_dict["connection_mac"],
+                    SERIAL_DEVICE,
+                ), end="", file=sys.stderr)
                 traceback.print_exc(limit=STACKTRACE_REFERENCE_LIMIT)
                 run_code = 1
             print(FORMAT_TEMPLATE.format(
                 metadata_digitemp_dict["unique_id"],
                 metadata_digitemp_dict["connection_mac"],
                 run_code,
+                ",temp_celcius={}".format(temperature) if run_code == 0 else "",
                 int((time.time() - run_time_start) * 1000),
-                temperature,
                 int(time.time() * 1000000000),
             ))
