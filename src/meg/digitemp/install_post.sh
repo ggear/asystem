cbbb486bd28228f83ca7dba3d4c973dfeeb74d8d
Graham Gear <graham.gear@nowhere.org>
2023-08-01 13:09:17 +0800

    Update asystem-10.100.4130

--- src/meg/digitemp/install_post.sh
+++ src/meg/digitemp/install_post.sh
@@ -2,7 +2,5 @@
 
 docker exec digitemp /asystem/runtime/mqtt.sh
 
-printf "Entity Metadata publish script sleeping before running for first time ... " && sleep 2 && printf "done\n\n"
-printf "Entity Metadata publish script publishing data:\n"
-docker exec digitemp telegraf --debug --once || True
-printf "Entity Metadata publish script publishing data complete\n"
+printf "Entity Metadata publish script sleeping before publishing data ... " && sleep 2 && printf "done\n\nEntity Metadata publish script publishing data:\n"
+docker exec digitemp telegraf --debug --once
