e49ab4d4a8e4846f685740aa144ca19f01b7592f
Graham Gear <graham.gear@nowhere.org>
2023-08-01 13:12:00 +0800

    Update asystem-10.100.4131

--- src/meg/internet/install_post.sh
+++ src/meg/internet/install_post.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 
 docker exec internet /asystem/runtime/mqtt.sh
+
+printf "Entity Metadata publish script sleeping before publishing data ... " && sleep 2 && printf "done\n\nEntity Metadata publish script publishing data:\n"
+docker exec internet telegraf --debug --once
