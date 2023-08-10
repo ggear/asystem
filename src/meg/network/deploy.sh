f5f9f90f8a52f2dc927000b70a7808ddb0c8daf5
Graham Gear <graham.gear@nowhere.org>
2023-08-10 21:18:19 +0800

    Update asystem-10.100.4221

--- src/meg/network/deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-ROOT_DIR=$(dirname $(readlink -f "$0"))
-
-export $(xargs <${ROOT_DIR}/.env)
-
-export VERNEMQ_HOST=${VERNEMQ_HOST_PROD}
-
-${ROOT_DIR}/src/main/resources/config/mqtt.sh
