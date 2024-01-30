6572f854e0a948a4e2a249488a6b21a9ff1cd174
Graham Gear <graham.gear@nowhere.org>
2024-01-30 15:46:52 +0800

    Update asystem-10.100.4364

--- src/net/udmutilities/src/main/resources/config/udm-dnsmasq/24-dnsmasq.sh
+++ src/net/udmutilities/src/main/resources/config/udm-dnsmasq/24-dnsmasq.sh
@@ -12,14 +12,14 @@ CONF_CUSTOM_FILES="dhcp.dhcpServers"*"custom.conf"
 
 rm -rf ${CONF_BUILD_DIR}
 cp -rvf ${CONF_CUSTOM_DIR} ${CONF_BUILD_DIR}
-rm -rf ${CONF_BUILD_DIR}/${CONF_CUSTOM_FILES}
+rm -rf "${CONF_BUILD_DIR}/${CONF_CUSTOM_FILES}"
 for CONF_SOURCE_FILE in $(ls \
   ${CONF_SOURCE_FILE_PREFIX}-*Default*-custom.conf \
   ${CONF_SOURCE_FILE_PREFIX}-*Unfettered*-custom.conf \
   ${CONF_SOURCE_FILE_PREFIX}-*Controlled*-custom.conf \
   ${CONF_SOURCE_FILE_PREFIX}-*Isolated*-custom.conf \
   2>/dev/null); do
-  while read CONF_SOURCE_LINE; do
+  while read -r CONF_SOURCE_LINE; do
     CONF_MAC=$(echo "${CONF_SOURCE_LINE}" | cut -d',' -f1 | cut -d'=' -f2 | awk '{print tolower($0)}')
     CONF_IP=$(echo "${CONF_SOURCE_LINE}" | cut -d',' -f2)
     CONF_HOST=$(echo "${CONF_SOURCE_LINE}" | cut -d',' -f3)
@@ -27,23 +27,13 @@ for CONF_SOURCE_FILE in $(ls \
       CONF_HOST=${CONF_IP}
       CONF_IP=""
     fi
-    CONF_BUILD=$(echo "dhcp-host=${CONF_MAC},${CONF_IP},${CONF_HOST}")
-    CONF_CURRENT=$(grep ${CONF_MAC} ${CONF_CURRENT_FILE})
-
-    #    echo "CONF_MAC=$CONF_MAC"
-    #    echo "CONF_IP=$CONF_IP"
-    #    echo "CONF_HOST=$CONF_HOST"
-    #    echo "CONF_BUILD=$CONF_BUILD"
-    #    echo "CONF_CURRENT=$CONF_CURRENT"
-    #    echo "1="$(echo -n "${CONF_CURRENT}" | wc -w)
-    #    echo "2="$(echo -n "${CONF_CURRENT}" | grep -v "${CONF_IP}" | wc -w)
-    #    echo "3="$(echo -n "${CONF_CURRENT}" | grep -v "${CONF_HOST}" | wc -w)
-
+    CONF_BUILD="dhcp-host=${CONF_MAC},${CONF_IP},${CONF_HOST}"
+    CONF_CURRENT=$(grep "${CONF_MAC}" "${CONF_CURRENT_FILE}")
     echo "${CONF_BUILD}" >>"${CONF_BUILD_FILE}"
-    if [ $(echo -n "${CONF_CURRENT}" | wc -w) -gt 0 ]; then
-      if [ ! -z "${CONF_IP}" ]; then
-        if [ $(echo -n "${CONF_CURRENT}" | grep -v "${CONF_IP}" | wc -w) -gt 0 ] ||
-          [ $(echo -n "${CONF_CURRENT}" | grep -v "${CONF_HOST}" | wc -w) -gt 1 ]; then
+    if [ "$(echo -n "${CONF_CURRENT}" | wc -w)" -gt 0 ]; then
+      if [ -n "${CONF_IP}" ]; then
+        if [ "$(echo -n "${CONF_CURRENT}" | grep -v "${CONF_IP}" | wc -w)" -gt 0 ] ||
+          [ "$(echo -n "${CONF_CURRENT}" | grep -v "${CONF_HOST}" | wc -w)" -gt 1 ]; then
           echo "Host [${CONF_HOST}] with MAC [${CONF_MAC}] and IP [${CONF_IP}] config metadata and DHCP lease out of sync, flushing lease"
           sed -i /".* ${CONF_MAC} .*"/d ${CONF_CURRENT_FILE}
           CONF_FLUSHED_LEASES="true"
@@ -56,7 +46,7 @@ for CONF_SOURCE_FILE in $(ls \
     else
       echo "Host [${CONF_HOST}] with MAC [${CONF_MAC}] and IP [${CONF_IP}] config metadata found but no DHCP lease"
     fi
-  done <${CONF_SOURCE_FILE}
+  done <"${CONF_SOURCE_FILE}"
 done
 if [ -f "${CONF_SOURCE_FILE_PREFIX}-aliases.conf" ]; then
   cat "${CONF_SOURCE_FILE_PREFIX}-aliases.conf" >>${CONF_BUILD_FILE}
@@ -68,7 +58,7 @@ if [ ${CONF_FLUSHED_LEASES} == "true" ] || [ ! -f ${CONF_CUSTOM_FILE} ] ||
   if dnsmasq --conf-dir=${CONF_BUILD_DIR} --test; then
     cp -rvf ${CONF_BUILD_FILE} ${CONF_CUSTOM_FILE}
     echo "applied new dnsmasq config"
-    kill -9 $(cat /run/dnsmasq.pid) 2>/dev/null
+    kill -9 "$(cat /run/dnsmasq.pid)" 2>/dev/null
     echo "killed and restarted dnsmasq"
   else
     echo "new dnsmasq config failed to parse, leaving old config in place"
