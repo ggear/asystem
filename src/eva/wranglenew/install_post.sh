610092784d1eb6240344e13ab2ab8ec8d6b0724c
Graham Gear <graham.gear@nowhere.org>
2024-02-11 17:20:59 +0800

    Update asystem-10.100.4445

--- src/eva/wranglenew/install_post.sh
+++ src/eva/wranglenew/install_post.sh
@@ -5,5 +5,6 @@ SERVICE_INSTALL=/var/lib/asystem/install/${SERVICE_NAME}/${SERVICE_VERSION_ABSOL
 
 cd ${SERVICE_INSTALL} || exit
 
+mkdir -p ${SERVICE_HOME}/data
 chmod -R 777 ${SERVICE_HOME}/data
 docker exec -e WRANGLE_ENABLE_DATA_TRUNC=true -e WRANGLE_DISABLE_DATA_DELTA=true --user root wrangle telegraf --debug --once
