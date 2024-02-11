e367d9e4612a21940b35df5fc969e8956f9c4dc7
Graham Gear <graham.gear@nowhere.org>
2024-02-11 10:40:06 +0800

    Update asystem-10.100.4440

--- src/meg_eva/_debian-amd64/install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-################################################################################
-# Packages
-################################################################################
-apt-get update
-apt-get install -y --allow-downgrades 'intel-microcode=3.20230512.1'
-apt-get install -y --allow-downgrades 'libc6-i386=2.36-9'
