b52a215e5acd15992de5ab39fa65a1b6c27ff60f
Graham Gear <graham.gear@nowhere.org>
2024-02-14 14:48:27 +0800

    Update asystem-10.100.4466

--- src/meg_eva/media/deploy.sh
+++ src/meg_eva/media/deploy.sh
@@ -6,7 +6,8 @@ HOSTS=$(echo $(basename $(dirname $(pwd))) | tr "_" "\n")
 
 for HOST in ${HOSTS}; do
   HOST="$(grep ${HOST} ${ROOT_DIR}/../../../.hosts | tr '=' ' ' | tr ',' ' ' | awk '{ print $2 }')-${HOST}"
-  echo "------------------------------------------------------------" && echo "Importing data on ${HOST} ..."
+  echo "------------------------------------------------------------" && echo "Operating media on ${HOST}"
   ssh -o StrictHostKeyChecking=no root@${HOST} "/root/install/media/latest/config/import.sh"
   ssh -o StrictHostKeyChecking=no root@${HOST} "/root/install/media/latest/config/normalise.sh"
+  echo "Completed media operations on ${HOST}" && echo "------------------------------------------------------------"
 done
--- src/meg_eva/media/src/main/resources/config/normalise.sh
+++ src/meg_eva/media/src/main/resources/config/normalise.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-echo -n "Normalising /data ... "
 for SHARE_DIR in $(grep /share /etc/fstab | grep ext4 | awk 'BEGIN{FS=OFS=" "}{print $2}'); do
+  echo -n "Normalising ${SHARE_DIR} ... "
   setfacl -bR ${SHARE_DIR}
   find ${SHARE_DIR} -type f -exec chmod 640 {} \;
   find ${SHARE_DIR} -type d -exec chmod 750 {} \;
   find ${SHARE_DIR} -type d -exec chown graham:users {} \;
   find ${SHARE_DIR} -type f -name nohup -exec rm -f {} \;
   find ${SHARE_DIR} -type f -name .DS_Store -exec rm -f {} \;
+  echo "done"
 done
-echo "done"
