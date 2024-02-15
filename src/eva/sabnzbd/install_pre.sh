2a2f0232984cd5268e54dc95fd1305ba4071fc68
Graham Gear <graham.gear@nowhere.org>
2024-02-15 12:13:10 +0800

    Update asystem-10.100.4488

--- src/eva/sabnzbd/install_pre.sh
+++ src/eva/sabnzbd/install_pre.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-. .env
+. ~/.env
 
 for DIR in "enqueued" "finished" "staging"; do
   mkdir -p "${SABNZBD_SHARE_DIR}/$DIR"
-  chown -R graham:users "${SABNZBD_DOWNLOAD_DIR}/$DIR"
+  chown -R graham:users "${SABNZBD_SHARE_DIR}/$DIR"
 done
