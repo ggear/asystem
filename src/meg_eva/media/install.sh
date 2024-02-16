35c7cbb9b5faed27b4d32083236ed8a5bd26f271
Graham Gear <graham.gear@nowhere.org>
2024-02-16 09:38:27 +0800

    Update asystem-10.100.4500

--- src/meg_eva/media/install.sh
+++ src/meg_eva/media/install.sh
@@ -1,1 +1,8 @@
 #!/bin/bash
+
+SERVICE_HOME=/home/asystem/${SERVICE_NAME}/${SERVICE_VERSION_ABSOLUTE}
+SERVICE_INSTALL=/var/lib/asystem/install/${SERVICE_NAME}/${SERVICE_VERSION_ABSOLUTE}
+
+cd ${SERVICE_INSTALL} || exit
+
+chmod +x config/*.sh
