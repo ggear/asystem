8cafedd40716b18cb2807938fda5164ad6ed1fbf
Graham Gear <graham.gear@nowhere.org>
2024-02-17 14:03:18 +0800

    Update asystem-10.100.4503

--- src/meg_eva/samba/install.sh
+++ src/meg_eva/samba/install.sh
@@ -38,9 +38,7 @@ EOF
 for SHARE_DIR in $(grep /share /etc/fstab | grep ext4 | awk 'BEGIN{FS=OFS=" "}{print $2}'); do
   SHARE_INDEX=$(echo ${SHARE_DIR} | awk 'BEGIN{FS=OFS="/"}{print $3}')
   mkdir -p ${SHARE_DIR}/tmp
-  mkdir -p ${SHARE_DIR}/media/audio
-  mkdir -p ${SHARE_DIR}/media/series
-  mkdir -p ${SHARE_DIR}/media/movies
+  mkdir -p ${SHARE_DIR}/media
   mkdir -p ${SHARE_DIR}/backup/service
   mkdir -p ${SHARE_DIR}/backup/timemachine
   rm -rf ${SHARE_DIR}/lost+found
