ec216f8d8ace0a52b78abe95a911ea3c3101ac00
Graham Gear <graham.gear@nowhere.org>
2024-02-17 16:17:54 +0800

    Update asystem-10.100.4507

--- src/meg_eva/media/src/main/resources/config/normalise.sh
+++ src/meg_eva/media/src/main/resources/config/normalise.sh
@@ -3,7 +3,6 @@
 for SHARE_DIR in $(grep /share /etc/fstab | grep ext4 | awk 'BEGIN{FS=OFS=" "}{print $2}'); do
   echo -n "Normalising ${SHARE_DIR} ... "
   setfacl -bR ${SHARE_DIR}
-  df -h ${SHARE_DIR}
   find ${SHARE_DIR} -type f -exec chmod 640 {} \;
   find ${SHARE_DIR} -type d -exec chmod 750 {} \;
   find ${SHARE_DIR} -type d -exec chown graham:users {} \;
