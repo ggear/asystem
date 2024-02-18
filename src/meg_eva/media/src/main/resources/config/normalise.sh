f87190af6d46d553e1c33a5104cd0847fe2253c2
Graham Gear <graham.gear@nowhere.org>
2024-02-18 10:04:46 +0800

    Update asystem-10.100.4511

--- src/meg_eva/media/src/main/resources/config/normalise.sh
+++ src/meg_eva/media/src/main/resources/config/normalise.sh
@@ -5,7 +5,7 @@ for SHARE_DIR in $(grep /share /etc/fstab | grep ext4 | awk 'BEGIN{FS=OFS=" "}{p
   setfacl -bR ${SHARE_DIR}
   find ${SHARE_DIR} -type f -exec chmod 640 {} \;
   find ${SHARE_DIR} -type d -exec chmod 750 {} \;
-  find ${SHARE_DIR} -type d -exec chown graham:users {} \;
+  find ${SHARE_DIR} -exec chown graham:users {} \;
   find ${SHARE_DIR} -type f -name nohup -exec rm -f {} \;
   find ${SHARE_DIR} -type f -name .DS_Store -exec rm -f {} \;
   echo "done"
