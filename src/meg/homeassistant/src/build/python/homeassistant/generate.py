81952c334d8950cf8deacf16dfa475bc659a8a91
Graham Gear <graham.gear@nowhere.org>
2024-02-21 13:08:06 +0800

    Update asystem-10.100.4530

--- src/meg/homeassistant/src/build/python/homeassistant/generate.py
+++ src/meg/homeassistant/src/build/python/homeassistant/generate.py
@@ -115,19 +115,19 @@ def write_entity_metadata(module_name, module_root_dir, metadata_df, topics_disc
 ROOT_DIR="$(dirname $(readlink -f "$0"))/mqtt"
 
 printf "\\nEntity Metadata publish script [{}] dropping discovery topics:\\n"
-mosquitto_sub -h $VERNEMQ_HOST -p $VERNEMQ_PORT --remove-retained -F '%t' -t '{}' -W 1 2>/dev/null
+mosquitto_sub -h $VERNEMQ_SERVICE -p $VERNEMQ_PORT --remove-retained -F '%t' -t '{}' -W 1 2>/dev/null
 
 printf "\\nEntity Metadata publish script [{}] sleeping before dropping data topics ... " && sleep 2 && printf "done\\n\\n"
 
 printf "Entity Metadata publish script [{}] dropping data topics:\\n"
-mosquitto_sub -h $VERNEMQ_HOST -p $VERNEMQ_PORT --remove-retained -F '%t' -t '{}' -W 1 2>/dev/null
+mosquitto_sub -h $VERNEMQ_SERVICE -p $VERNEMQ_PORT --remove-retained -F '%t' -t '{}' -W 1 2>/dev/null
 
 printf "\\nEntity Metadata publish script [{}] sleeping before publishing discovery topics ... " && sleep 2 && printf "done\\n\\n"
 
 printf "Entity Metadata publish script [{}] publishing discovery topics:\\n"
 find $ROOT_DIR -name "*.json" -print0 | while read -d $'\\0' METADATA_FILE; do
   METADATA_TOPIC=$(dirname "${{METADATA_FILE/$ROOT_DIR\\//}}")
-  mosquitto_pub -h $VERNEMQ_HOST -p $VERNEMQ_PORT -t $METADATA_TOPIC -f $METADATA_FILE -r
+  mosquitto_pub -h $VERNEMQ_SERVICE -p $VERNEMQ_PORT -t $METADATA_TOPIC -f $METADATA_FILE -r
   printf "$METADATA_TOPIC\\n"
 done
 printf "\\n"
@@ -161,7 +161,7 @@ if __name__ == "__main__":
         try:
             state_response = get(
                 "http://{}:{}/api/states/{}.{}".format(
-                    env["HOMEASSISTANT_IP_PROD"],
+                    env["HOMEASSISTANT_SERVICE_PROD"],
                     env["HOMEASSISTANT_HTTP_PORT"],
                     metadata_verify_dict["entity_namespace"],
                     metadata_verify_dict["unique_id"]
