22c2411b9e2efb8e5ac83bd0023f2cbf84f2e608
Graham Gear <graham.gear@nowhere.org>
2024-02-21 10:56:31 +0800

    Update asystem-10.100.4518

--- src/eva/influxdb/src/main/resources/config/backup.sh
+++ src/eva/influxdb/src/main/resources/config/backup.sh
@@ -5,7 +5,7 @@ TIMESTAMP=$(date +"%Y-%m-%d_%H-%M-%S")
 for BUCKET in ${INFLUXDB_BUCKET_HOME_PRIVATE}; do
   DIR="${DIR_BACKUP}/${TIMESTAMP}/${BUCKET}"
   mkdir -p ${DIR}
-  influx backup -b ${BUCKET} ${DIR} -t ${INFLUXDB_TOKEN}
+  influx backup -b ${BUCKET} ${DIR} -t ${INFLUXDB_TOKEN} --host ${INFLUXDB_SERVICE}
   echo "Completed backup for timestamp [$TIMESTAMP] to [${DIR}]"
 done
 find ${DIR_BACKUP} -depth -empty -delete -type d
