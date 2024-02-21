e8722cda27967a1a2859f16e6dc65fd285204152
Graham Gear <graham.gear@nowhere.org>
2024-02-21 11:16:42 +0800

    Update asystem-10.100.4523

--- src/eva/influxdb/src/main/resources/config/delete.sh
+++ src/eva/influxdb/src/main/resources/config/delete.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+influx config set --config-name remote
+
 for MEASUREMENT in disk; do
     influx delete --org ${INFLUXDB_ORG} --bucket ${INFLUXDB_BUCKET_HOST_PRIVATE} --predicate '_measurement="'${MEASUREMENT}'"' --start '1970-01-01T00:00:00Z' --stop $(date +"%Y-%m-%dT%H:%M:%SZ") -t ${INFLUXDB_TOKEN}
 done
