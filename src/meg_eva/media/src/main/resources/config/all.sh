8b13e734962b5b675206183626ed761908b4cc17
Graham Gear <graham.gear@nowhere.org>
2024-03-03 12:32:50 +0800

    Update asystem-10.100.4612

--- src/meg_eva/media/src/main/resources/config/all.sh
+++ src/meg_eva/media/src/main/resources/config/all.sh
@@ -2,6 +2,6 @@
 
 /root/install/media/latest/config/import.sh
 for SHARE_DIR in $(grep /share /etc/fstab | grep ext4 | awk 'BEGIN{FS=OFS=" "}{print $2}'); do
-  python3 /root/install/media/latest/config/rename.py ${SHARE_DIR}
+  echo python3 /root/install/media/latest/config/rename.py ${SHARE_DIR}/tmp
 done
 /root/install/media/latest/config/normalise.sh
--- src/meg_eva/media/src/test/python/unit/unit_test.py
+++ src/meg_eva/media/src/test/python/unit/unit_test.py
@@ -10,19 +10,31 @@ from media import rename
 from os.path import *
 
 DIR_ROOT = abspath(join(dirname(realpath(__file__)), "../../../.."))
-DIR_TEST = join(DIR_ROOT, "target/runtime-unit/share_tmp_example_1")
-DIR_TEST_SRC = join(DIR_ROOT, "src/test/resources/share_tmp_example_1")
+DIR_TEST_1 = join(DIR_ROOT, "target/runtime-unit/share_tmp_example_1")
+DIR_TEST_SRC_1 = join(DIR_ROOT, "src/test/resources/share_tmp_example_1")
+DIR_TEST_2 = join(DIR_ROOT, "target/runtime-unit/share_tmp_example_2")
+DIR_TEST_SRC_2 = join(DIR_ROOT, "src/test/resources/share_tmp_example_2")
 
 
 class InternetTest(unittest.TestCase):
 
-    def test_rename(self):
+    def test_rename_1(self):
         print("")
         sys.stdout.flush()
-        shutil.rmtree(DIR_TEST, ignore_errors=True)
-        os.makedirs(abspath(join(DIR_TEST, "..")), exist_ok=True)
-        shutil.copytree(DIR_TEST_SRC, DIR_TEST)
-        rename.rename(DIR_TEST)
+        shutil.rmtree(DIR_TEST_1, ignore_errors=True)
+        os.makedirs(abspath(join(DIR_TEST_1, "..")), exist_ok=True)
+        shutil.copytree(DIR_TEST_SRC_1, DIR_TEST_1)
+        rename.rename(DIR_TEST_1)
+        rename.rename(DIR_TEST_1)
+
+    def test_rename_2(self):
+        print("")
+        sys.stdout.flush()
+        shutil.rmtree(DIR_TEST_2, ignore_errors=True)
+        os.makedirs(abspath(join(DIR_TEST_2, "..")), exist_ok=True)
+        shutil.copytree(DIR_TEST_SRC_2, DIR_TEST_2)
+        rename.rename(DIR_TEST_2)
+        rename.rename(DIR_TEST_2)
 
 
 if __name__ == '__main__':
