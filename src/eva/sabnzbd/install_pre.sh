afaf035f2aeac236219ca2d6ce0f8f70c6638401
Graham Gear <graham.gear@nowhere.org>
2024-03-03 12:50:55 +0800

    Update asystem-10.100.4614

--- src/eva/sabnzbd/install_pre.sh
+++ src/eva/sabnzbd/install_pre.sh
@@ -6,3 +6,6 @@ for DIR in "enqueued" "finished" "reencoding" "staging"; do
   mkdir -p "${SABNZBD_SHARE_DIR}/$DIR"
 done
 chown -R graham:users "${SABNZBD_SHARE_DIR}"
+mkdir -p /home/asystem/sabnzbd/latest/scripts
+cp -rvf /var/lib/asystem/install/media/latest/config/all.sh /home/asystem/sabnzbd/latest/scripts
+chmod +x /home/asystem/sabnzbd/latest/scripts/*.sh
