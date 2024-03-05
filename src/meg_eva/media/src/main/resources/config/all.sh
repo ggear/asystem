fb3ae59f1e96ee5311d4b3f3c3c17b920b7b9be0
Graham Gear <graham.gear@nowhere.org>
2024-03-05 10:49:05 +0800

    Update asystem-10.100.4623

--- src/meg_eva/media/src/main/resources/config/all.sh
+++ src/meg_eva/media/src/main/resources/config/all.sh
@@ -5,4 +5,5 @@ for SHARE_DIR in $(grep /share /etc/fstab | grep ext4 | awk 'BEGIN{FS=OFS=" "}{p
   python3 /root/install/media/latest/config/rename.py ${SHARE_DIR}/tmp
   /root/install/media/latest/config/normalise.sh ${SHARE_DIR}
 done
-df -h /share/*
+echo  "" && find /share -type f -path *__RENAMED* ! -name .DS_Store
+echo  "" && df -h /share/* && echo ""
