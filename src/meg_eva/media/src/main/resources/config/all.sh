c832a80c1d1f96454e78a463e28df54967e1dc3f
Graham Gear <graham.gear@nowhere.org>
2024-03-09 10:59:33 +0800

    Update asystem-10.100.4641

--- src/meg_eva/media/src/main/resources/config/all.sh
+++ src/meg_eva/media/src/main/resources/config/all.sh
@@ -5,5 +5,5 @@ for SHARE_DIR in $(grep /share /etc/fstab | grep ext4 | awk 'BEGIN{FS=OFS=" "}{p
   python3 /root/install/media/latest/config/rename.py ${SHARE_DIR}/tmp
   /root/install/media/latest/config/normalise.sh ${SHARE_DIR}
 done
-echo "" && find /share -type f -path *__RENAMED* ! -name .DS_Store
-echo "" && df -h $(grep /share /etc/fstab | grep ext4 | awk 'BEGIN{FS=OFS=" "}{print $2}' | tr '\n' ' ') && echo ""
+find /share -type f -path *__RENAMED* ! -name .DS_Store
+echo "Storage status:" && df -h $(grep /share /etc/fstab | grep ext4 | awk 'BEGIN{FS=OFS=" "}{print $2}' | tr '\n' ' ')
