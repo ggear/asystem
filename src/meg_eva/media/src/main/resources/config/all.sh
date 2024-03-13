fb8118cbade2ca4256b0feb3cb9d9a53ed5f27c1
Graham Gear <graham.gear@nowhere.org>
2024-03-13 11:16:57 +0800

    Update asystem-10.100.4659

--- src/meg_eva/media/src/main/resources/config/all.sh
+++ src/meg_eva/media/src/main/resources/config/all.sh
@@ -5,5 +5,5 @@ for SHARE_DIR in $(grep /share /etc/fstab | grep ext4 | awk 'BEGIN{FS=OFS=" "}{p
   python3 /root/install/media/latest/config/rename.py ${SHARE_DIR}/tmp
   /root/install/media/latest/config/normalise.sh ${SHARE_DIR}
 done
-/root/install/media/latest/config/metadata.sh
+/root/install/media/latest/config/library.sh
 echo "Storage status:" && df -h $(grep /share /etc/fstab | grep ext4 | awk 'BEGIN{FS=OFS=" "}{print $2}' | tr '\n' ' ')
