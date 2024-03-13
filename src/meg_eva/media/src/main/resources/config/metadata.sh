98298a733ebd78a46ddf9bf565899c08919aba86
Graham Gear <graham.gear@nowhere.org>
2024-03-13 11:09:55 +0800

    Update asystem-10.100.4658

--- src/meg_eva/media/src/main/resources/config/metadata.sh
+++ src/meg_eva/media/src/main/resources/config/metadata.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-. ./../.env
+ROOT_DIR=$(dirname $(readlink -f "$0"))
+
+. ${ROOT_DIR}/../.env
 
 echo -n "Refreshing metadata ... "
 for PLEX_LIBRARY_KEY in $(curl -sf http://${PLEX_SERVICE}:${PLEX_HTTP_PORT}/library/sections?X-Plex-Token=${PLEX_TOKEN} | xq -x '/MediaContainer/Directory/@key'); do
