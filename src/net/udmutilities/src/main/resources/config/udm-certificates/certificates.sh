29910c9645a6aa23264d5b164b918ef22d3163f4
Graham Gear <graham.gear@nowhere.org>
2024-03-14 13:14:08 +0800

    Update asystem-10.100.4664

--- src/net/udmutilities/src/main/resources/config/udm-certificates/certificates.sh
+++ src/net/udmutilities/src/main/resources/config/udm-certificates/certificates.sh
@@ -2,16 +2,21 @@
 
 ROOT_DIR=$(dirname $(readlink -f "$0"))
 
-if [ -z "$1" ] || [ "$1" = "pull" ]; then
-  scp -qpr "root@$2:/home/asystem/letsencrypt/latest/letsencrypt/live/janeandgraham.com/privkey.pem" "$ROOT_DIR/.key.pem"
-  scp -qpr "root@$2:/home/asystem/letsencrypt/latest/letsencrypt/live/janeandgraham.com/fullchain.pem" "$ROOT_DIR/certificate.pem"
+if [ "$#" -ne 3 ]; then
+  echo "Usage: $0 <mode> <host-pull> <host-push>"
+  exit 1
+fi
+if [ "$1" = "pull" ]; then
+  scp -q -o "StrictHostKeyChecking=no" -o "HostKeyAlgorithms=+ssh-rsa" -o "PubkeyAcceptedAlgorithms=+ssh-rsa" -pr "root@$2:/home/asystem/letsencrypt/latest/letsencrypt/live/janeandgraham.com/privkey.pem" "$ROOT_DIR/.key.pem"
+  scp -q -o "StrictHostKeyChecking=no" -o "HostKeyAlgorithms=+ssh-rsa" -o "PubkeyAcceptedAlgorithms=+ssh-rsa" -pr "root@$2:/home/asystem/letsencrypt/latest/letsencrypt/live/janeandgraham.com/fullchain.pem" "$ROOT_DIR/certificate.pem"
   echo "$2:/home/asystem/letsencrypt/latest/letsencrypt/live/janeandgraham.com -> localhost:$ROOT_DIR"
 elif [ "$1" = "push" ]; then
   for DIR in "/home/asystem/udmutilities/latest/udm-certificates/" "/var/lib/asystem/install/udmutilities/latest/config/udm-certificates/"; do
-    scp -qpr "$ROOT_DIR/.key.pem" "root@$3:$DIR"
-    scp -qpr "$ROOT_DIR/certificate.pem" "root@$3:$DIR"
+    scp -q -o "StrictHostKeyChecking=no" -o "HostKeyAlgorithms=+ssh-rsa" -o "PubkeyAcceptedAlgorithms=+ssh-rsa" -pr "$ROOT_DIR/.key.pem" "root@$3:$DIR"
+    scp -q -o "StrictHostKeyChecking=no" -o "HostKeyAlgorithms=+ssh-rsa" -o "PubkeyAcceptedAlgorithms=+ssh-rsa" -pr "$ROOT_DIR/certificate.pem" "root@$3:$DIR"
     echo "localhost:$ROOT_DIR -> $3:$DIR"
   done
   echo "Certificates pushed, restarting service on [$3] ... "
-  ssh "root@$3" "/var/lib/asystem/install/udmutilities/latest/install.sh"
+  ssh -q -o "StrictHostKeyChecking=no" -o "HostKeyAlgorithms=+ssh-rsa" -o "PubkeyAcceptedAlgorithms=+ssh-rsa" "root@$3" "/var/lib/asystem/install/udmutilities/latest/install.sh"
 fi
+exit 0
