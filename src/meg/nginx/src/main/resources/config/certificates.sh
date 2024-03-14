e0c298b6611356f1c6b9aa9e171a1b534defa1c9
Graham Gear <graham.gear@nowhere.org>
2024-03-14 13:09:57 +0800

    Update asystem-10.100.4663

--- src/meg/nginx/src/main/resources/config/certificates.sh
+++ src/meg/nginx/src/main/resources/config/certificates.sh
@@ -7,16 +7,16 @@ if [ "$#" -ne 3 ]; then
   exit 1
 fi
 if [ "$1" = "pull" ]; then
-  scp -qpr "root@$2:/home/asystem/letsencrypt/latest/letsencrypt/live/janeandgraham.com/privkey.pem" "$ROOT_DIR/.key.pem"
-  scp -qpr "root@$2:/home/asystem/letsencrypt/latest/letsencrypt/live/janeandgraham.com/fullchain.pem" "$ROOT_DIR/certificate.pem"
+  scp -q -o "StrictHostKeyChecking=no" -o "HostKeyAlgorithms=+ssh-rsa" -o "PubkeyAcceptedAlgorithms=+ssh-rsa" -pr "root@$2:/home/asystem/letsencrypt/latest/letsencrypt/live/janeandgraham.com/privkey.pem" "$ROOT_DIR/.key.pem"
+  scp -q -o "StrictHostKeyChecking=no" -o "HostKeyAlgorithms=+ssh-rsa" -o "PubkeyAcceptedAlgorithms=+ssh-rsa" -pr "root@$2:/home/asystem/letsencrypt/latest/letsencrypt/live/janeandgraham.com/fullchain.pem" "$ROOT_DIR/certificate.pem"
   echo "$2:/home/asystem/letsencrypt/latest/letsencrypt/live/janeandgraham.com -> localhost:$ROOT_DIR"
 elif [ "$1" = "push" ]; then
   for DIR in "/home/asystem/nginx/latest/" "/var/lib/asystem/install/nginx/latest/config/"; do
-    scp -qpr "$ROOT_DIR/.key.pem" "root@$3:$DIR"
-    scp -qpr "$ROOT_DIR/certificate.pem" "root@$3:$DIR"
+    scp -q -o "StrictHostKeyChecking=no" -o "HostKeyAlgorithms=+ssh-rsa" -o "PubkeyAcceptedAlgorithms=+ssh-rsa" -pr "$ROOT_DIR/.key.pem" "root@$3:$DIR"
+    scp -q -o "StrictHostKeyChecking=no" -o "HostKeyAlgorithms=+ssh-rsa" -o "PubkeyAcceptedAlgorithms=+ssh-rsa" -pr "$ROOT_DIR/certificate.pem" "root@$3:$DIR"
     echo "localhost:$ROOT_DIR -> $3:$DIR"
   done
   echo "Certificates pushed, restarting service on [$3] ... "
-  ssh "root@$3" "/var/lib/asystem/install/nginx/latest/install.sh"
+  ssh -q -o "StrictHostKeyChecking=no" -o "HostKeyAlgorithms=+ssh-rsa" -o "PubkeyAcceptedAlgorithms=+ssh-rsa" "root@$3" "/var/lib/asystem/install/nginx/latest/install.sh"
 fi
 exit 0
