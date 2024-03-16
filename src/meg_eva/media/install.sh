f3ecdc0b87d527e74b0801d966e46a8c93346f1a
Graham Gear <graham.gear@nowhere.org>
2024-03-16 18:41:38 +0800

    Update asystem-10.100.4688

--- src/meg_eva/media/install.sh
+++ src/meg_eva/media/install.sh
@@ -8,7 +8,7 @@ cd ${SERVICE_INSTALL} || exit
 chmod +x config/*.sh
 
 for SHARE_DIR in $(grep /share /etc/fstab | grep ext4 | awk 'BEGIN{FS=OFS=" "}{print $2}'); do
-  for SHARE_DIR_SCOPE in "kids" "parents" "documentaries"; do
+  for SHARE_DIR_SCOPE in "kids" "parents" "docos"; do
     for SHARE_DIR_MEDIA in "audio" "movies" "series"; do
       mkdir -p ${SHARE_DIR}/media/${SHARE_DIR_SCOPE}/${SHARE_DIR_MEDIA}
     done
