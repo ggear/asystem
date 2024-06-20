ea1d6b6ee5c1b7be283d47df60e69159be67e6e4
Graham Gear <graham.gear@nowhere.org>
2024-06-20 13:14:05 +0800

    Update asystem-10.100.4755

--- src/meg_eva/media/src/main/resources/config/all.sh
+++ src/meg_eva/media/src/main/resources/config/all.sh
@@ -3,7 +3,11 @@
 /root/install/media/latest/config/import.sh /share/2/tmp
 for SHARE_DIR in $(grep /share /etc/fstab | grep ext4 | awk 'BEGIN{FS=OFS=" "}{print $2}'); do
   python3 /root/install/media/latest/config/rename.py ${SHARE_DIR}/tmp
+done
+for SHARE_DIR in $(grep /share /etc/fstab | grep ext4 | awk 'BEGIN{FS=OFS=" "}{print $2}'); do
   /root/install/media/latest/config/normalise.sh ${SHARE_DIR}
+done
+for SHARE_DIR in $(grep /share /etc/fstab | grep ext4 | awk 'BEGIN{FS=OFS=" "}{print $2}'); do
   python3 /root/install/media/latest/config/analyse.py ${SHARE_DIR}/media
 done
 /root/install/media/latest/config/library.sh
