cc8f68f60c86da65d9ada62a5922b9c1a57cc9fc
Graham Gear <graham.gear@nowhere.org>
2024-06-22 14:18:07 +0800

    Update asystem-10.100.4769

--- src/meg_eva/media/src/main/resources/config/all.sh
+++ src/meg_eva/media/src/main/resources/config/all.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 SHARE_DIRS=$(grep /share /etc/fstab | grep ext4 | awk 'BEGIN{FS=OFS=" "}{print $2}')
+INSTALL_DIR="/root/install/media/latest/config"
+PYTHON_DIR="/root/.pyenv/versions/${PYTHON_VERSION}/bin"
 
-/root/install/media/latest/config/import.sh /share/2/tmp
-for SHARE_DIR in ${SHARE_DIRS}; do /root/install/media/latest/config/normalise.sh ${SHARE_DIR}; done
-for SHARE_DIR in ${SHARE_DIRS}; do python3 /root/install/media/latest/config/rename.py ${SHARE_DIR}/tmp; done
-for SHARE_DIR in ${SHARE_DIRS}; do python3 /root/install/media/latest/config/analyse.py ${SHARE_DIR}/media; done
-/root/install/media/latest/config/library.sh
+${INSTALL_DIR}/import.sh /share/2/tmp
+for SHARE_DIR in ${SHARE_DIRS}; do ${INSTALL_DIR}/normalise.sh ${SHARE_DIR}; done
+for SHARE_DIR in ${SHARE_DIRS}; do ${PYTHON_DIR}/python ${INSTALL_DIR}/rename.py ${SHARE_DIR}/tmp; done
+for SHARE_DIR in ${SHARE_DIRS}; do ${PYTHON_DIR}/python ${INSTALL_DIR}/analyse.py ${SHARE_DIR}/media; done
+${INSTALL_DIR}/library.sh
 echo "Storage status ... done" && df -h $(echo ${SHARE_DIRS} | tr '\n' ' ') | cut -c 40-
