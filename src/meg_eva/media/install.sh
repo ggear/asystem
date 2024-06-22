ff4b0b83e17f067d1f79d8d03115093c718f54c6
Graham Gear <graham.gear@nowhere.org>
2024-06-22 14:12:48 +0800

    Update asystem-10.100.4769-SNAPSHOT

--- src/meg_eva/media/install.sh
+++ src/meg_eva/media/install.sh
@@ -18,8 +18,8 @@ done
 
 if [ ! -d /root/.pyenv/versions/${PYTHON_VERSION}/bin ]; then
   pyenv install ${PYTHON_VERSION}
-  /root/.pyenv/versions/${PYTHON_VERSION}/bin/pip install -r config/.python_reqs.txt
 fi
+/root/.pyenv/versions/${PYTHON_VERSION}/bin/pip install -r config/.python_reqs.txt
 
 rm -rf /usr/bin/asystem-media
 ln -s /var/lib/asystem/install/media/latest/config/all.sh /usr/bin/asystem-media
