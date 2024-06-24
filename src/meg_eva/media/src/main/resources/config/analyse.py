336bbfecef681cd1523c3762c95b7bcc5ae4f1b2
Graham Gear <graham.gear@nowhere.org>
2024-06-24 17:01:53 +0800

    Update asystem-10.100.4783

--- src/meg_eva/media/src/main/resources/config/analyse.py
+++ src/meg_eva/media/src/main/resources/config/analyse.py
@@ -35,29 +35,29 @@ def _analyse(file_path_root, sheet_guid, verbose=False, refresh=False):
     sys.stdout.flush()
     for file_dir_path, _, file_names in os.walk(file_path_root):
         for file_name in file_names:
-            file_source_path = os.path.join(file_dir_path, file_name)
-            file_source_relative_dir = "." + file_dir_path.replace(file_path_root, "")
-            file_source_relative_dir_path_tokens = file_source_relative_dir.split(os.sep)
-            file_source_extension = os.path.splitext(file_name)[1].replace(".", "")
+            file_path = os.path.join(file_dir_path, file_name)
+            file_relative_dir = "." + file_dir_path.replace(file_path_root, "")
+            file_relative_dir_tokens = file_relative_dir.split(os.sep)
+            file_extension = os.path.splitext(file_name)[1].replace(".", "")
             file_metadata_path = os.path.join(file_dir_path, "{}.yaml".format(os.path.splitext(file_name)[0]))
-            file_media_scope = file_source_relative_dir_path_tokens[1] \
-                if len(file_source_relative_dir_path_tokens) > 3 else ""
-            file_media_type = file_source_relative_dir_path_tokens[2] \
-                if len(file_source_relative_dir_path_tokens) > 3 else ""
-            if file_source_extension in {"yaml"}:
+            file_media_scope = file_relative_dir_tokens[1] \
+                if len(file_relative_dir_tokens) > 3 else ""
+            file_media_type = file_relative_dir_tokens[2] \
+                if len(file_relative_dir_tokens) > 3 else ""
+            if file_extension in {"yaml"}:
                 continue;
             if verbose:
-                print("{} ... ".format(os.path.join(file_source_relative_dir, file_name)), end='')
+                print("{} ... ".format(os.path.join(file_relative_dir, file_name)), end='')
             if file_media_type not in {"movies", "series"}:
                 if verbose:
                     print("ignoring library type [{}]".format(file_media_type))
                 continue;
-            if file_source_extension not in {"mkv", "mp4", "avi", "m2ts"}:
+            if file_extension not in {"avi", "m2ts", "mkv", "mov", "mp4", "wmv"}:
                 if verbose:
-                    print("ignoring unknown file extension [{}]".format(file_source_extension))
+                    print("ignoring unknown file extension [{}]".format(file_extension))
                 continue;
             if refresh or not os.path.isfile(file_metadata_path):
-                file_probe = ffmpeg.probe(file_source_path)
+                file_probe = ffmpeg.probe(file_path)
                 file_probe_streams_filtered = {
                     "video": [],
                     "audio": [],
@@ -83,9 +83,9 @@ def _analyse(file_path_root, sheet_guid, verbose=False, refresh=False):
                             file_probe_stream_video_codec = file_probe_stream["codec_name"].upper() \
                                 if "codec_name" in file_probe_stream else ""
                             if "H264" in file_probe_stream_video_codec or "AVC" in file_probe_stream_video_codec:
-                                file_probe_stream_video_codec = "AVC (H264)"
+                                file_probe_stream_video_codec = "AVC"
                             if "H265" in file_probe_stream_video_codec or "HEVC" in file_probe_stream_video_codec:
-                                file_probe_stream_video_codec = "HEVC (H265)"
+                                file_probe_stream_video_codec = "HEVC"
                             file_probe_stream_filtered.append({"codec": file_probe_stream_video_codec})
                             file_probe_stream_video_field_order = ""
                             if "field_order" in file_probe_stream:
@@ -135,17 +135,52 @@ def _analyse(file_path_root, sheet_guid, verbose=False, refresh=False):
                             file_probe_stream_filtered.append({"language": file_probe_stream["tags"]["language"].lower() \
                                 if ("tags" in file_probe_stream and "language" in file_probe_stream["tags"]) else ""})
 
-                    file_target_quality = "Low"
-                    file_target_quality = "High"
-                    file_target_quality = ""
-
-                    file_probe_plex_mode = "Transcode"
-                    file_probe_plex_mode = "Direct Play"
-
-                    file_probe_transcode_priority = "1"
-                    file_probe_transcode_priority = "2"
                     file_probe_transcode_priority = ""
+                    file_probe_plex_mode = "Transcode"
+                    file_probe_plex_direct_play_video = False
+                    file_probe_plex_direct_play_audio = False
+                    file_probe_messy_metadata = False
+                    file_probe_bit_rate = round(int(file_probe["format"]["bit_rate"]) / 10 ** 3) \
+                        if ("format" in file_probe and "bit_rate" in file_probe["format"]) else -1
 
+                    # TODO: Provide greater coverage, non-eng audio/subtitles, multiple video etc
+                    for file_probe_subtitles in file_probe_streams_filtered["subtitle"]:
+                        for file_probe_subtitle in file_probe_subtitles.values():
+                            if (
+                                    "PGS" in file_probe_subtitle[2]["codec"]
+                            ):
+                                file_probe_messy_metadata = True
+
+                    for file_probe_videos in file_probe_streams_filtered["video"]:
+                        for file_probe_video in file_probe_videos.values():
+                            if (
+                                    file_extension == "avi" and file_probe_video[2]["codec"] in {"MPEG4", "MJPEG"} or \
+                                    file_extension == "m2ts" and file_probe_video[2]["codec"] in {"AVC", "HVEC", "MPEG2VIDEO"} or \
+                                    file_extension == "mkv" and file_probe_video[2]["codec"] in {"AVC", "HVEC", "MPEG2VIDEO", "MPEG4"} or \
+                                    file_extension == "mov" and file_probe_video[2]["codec"] in {"AVC", "MPEG4"} or \
+                                    file_extension == "mp4" and file_probe_video[2]["codec"] in {"AVC", "HVEC"} or \
+                                    file_extension == "wmv" and file_probe_video[2]["codec"] in {"WMV3", "VC1"}
+                            ):
+                                file_probe_plex_direct_play_video = True
+                    for file_probe_audios in file_probe_streams_filtered["audio"]:
+                        for file_probe_audio in file_probe_audios.values():
+                            if (
+                                    file_extension == "avi" and file_probe_audio[2]["codec"] in {"AAC", "AC3", "DTS", "MP3", "PCM"} or \
+                                    file_extension == "m2ts" and file_probe_audio[2]["codec"] in {"AAC", "AC3", "DTS", "MP3", "PCM"} or \
+                                    file_extension == "mkv" and file_probe_audio[2]["codec"] in {"AAC", "AC3", "DTS", "MP3", "PCM"} or \
+                                    file_extension == "mov" and file_probe_audio[2]["codec"] in {"AAC", "AC3", "DTS"} or \
+                                    file_extension == "mp4" and file_probe_audio[2]["codec"] in {"AAC", "AC3", "DTS", "MP3"} or \
+                                    file_extension == "wmv" and file_probe_audio[2]["codec"] in {"AC3", "DTS", "WMAPRO", "WMAV2"}
+                            ):
+                                file_probe_plex_direct_play_audio = True
+                    if file_probe_plex_direct_play_video and file_probe_plex_direct_play_audio:
+                        file_probe_plex_mode = "Direct Play"
+                        if file_probe_bit_rate > 12000:
+                            file_probe_transcode_priority = "2"
+                        elif file_probe_messy_metadata:
+                            file_probe_transcode_priority = "3"
+                    else:
+                        file_probe_transcode_priority = "1"
                     file_probe_duration_h = float(file_probe["format"]["duration"]) / 60 ** 2 \
                         if ("format" in file_probe and "duration" in file_probe["format"]) else -1
                     file_probe_size_gb = int(file_probe["format"]["size"]) / 10 ** 9 \
@@ -159,16 +194,17 @@ def _analyse(file_path_root, sheet_guid, verbose=False, refresh=False):
                         {"version_directory": os.path.dirname(file_probe["format"]["filename"]) \
                             .replace(os.path.join(file_path_root, file_media_scope, file_media_type) + "/", "") \
                             if ("format" in file_probe and "filename" in file_probe["format"]) else ""},
-                        {"target_quality": file_target_quality},
-                        {"transcode_priority": file_probe_transcode_priority},
                         {"plex_mode": file_probe_plex_mode},
-                        {"file_extension": file_source_extension},
+                        {"plex_mode_video": "Direct Play" if file_probe_plex_direct_play_video else "Transcode"},
+                        {"plex_mode_audio": "Direct Play" if file_probe_plex_direct_play_audio else "Transcode"},
+                        {"metadata_state": "Messy" if file_probe_messy_metadata else "Clean"},
+                        {"transcode_priority": file_probe_transcode_priority},
+                        {"file_extension": file_extension},
                         {"container_format": file_probe["format"]["format_name"].lower() \
                             if ("format" in file_probe and "format_name" in file_probe["format"]) else ""},
                         {"duration__hours)": round(file_probe_duration_h, 2 if file_probe_duration_h > 1 else 4)},
                         {"size__GB": round(file_probe_size_gb, 2 if file_probe_duration_h > 1 else 4)},
-                        {"bit_rate__Kbps": round(int(file_probe["format"]["bit_rate"]) / 10 ** 3) \
-                            if ("format" in file_probe and "bit_rate" in file_probe["format"]) else -1},
+                        {"bit_rate__Kbps": file_probe_bit_rate},
                         {"stream_count": sum(map(len, file_probe_streams_filtered.values()))}
                     ]
                     for file_probe_stream_type in file_probe_streams_filtered:
@@ -228,6 +264,7 @@ def _analyse(file_path_root, sheet_guid, verbose=False, refresh=False):
     metadata_original_pl = pl.DataFrame(
         metadata_spread._fix_merge_values(metadata_spread.sheet.get_all_values())[0:])
 
+    # TODO: Merge, drop missing items from same 'media_dir' right-join
     metadata_updated_pl = metadata_cache_pl
 
     if len(metadata_updated_pl) > 0:
