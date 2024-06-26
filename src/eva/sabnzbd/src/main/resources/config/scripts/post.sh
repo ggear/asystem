7a8a0ed1376cb73fe30a6458c2c28dd8d9e9ea44
Graham Gear <graham.gear@nowhere.org>
2024-06-26 12:43:04 +0800

    Update asystem-10.100.4819

--- src/eva/sabnzbd/src/main/resources/config/scripts/post.sh
+++ src/eva/sabnzbd/src/main/resources/config/scripts/post.sh
@@ -4,5 +4,5 @@ RETURN_STRING="failed"
 if python3 /config/scripts/rename.py /downloads; then
   RETURN_STRING="done"
 fi
-/config/scripts/normalise.sh /downloads/finished
+/config/scripts/normalise.sh /downloads
 echo "Post processing ... ${RETURN_STRING}"
