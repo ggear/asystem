f79727b561517274a0df3b1194b6daab3386662c
Graham Gear <graham.gear@nowhere.org>
2024-06-29 12:35:08 +0800

    Update asystem-10.100.4823

--- src/meg_eva/media/src/main/resources/config/bin/lib/refresh.sh
+++ src/meg_eva/media/src/main/resources/config/bin/lib/refresh.sh
@@ -2,7 +2,7 @@
 
 ROOT_DIR=$(dirname $(readlink -f "$0"))
 
-. ${ROOT_DIR}/../.env
+. ${ROOT_DIR}/../../../.env
 
 echo -n "Refreshing libraries ... "
 for PLEX_LIBRARY_KEY in $(curl -sf http://${PLEX_SERVICE}:${PLEX_HTTP_PORT}/library/sections?X-Plex-Token=${PLEX_TOKEN} | xq -x '/MediaContainer/Directory/@key'); do
