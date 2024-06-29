3218eae32c772d117bea6bed53ab3678fde1e58f
Graham Gear <graham.gear@nowhere.org>
2024-06-29 19:32:08 +0800

    Update asystem-10.100.4830

--- src/meg_eva/media/src/main/resources/config/bin/media-clean.sh
+++ src/meg_eva/media/src/main/resources/config/bin/media-clean.sh
@@ -5,4 +5,4 @@ ROOT_DIR=$(dirname $(readlink -f "$0"))
 . ${ROOT_DIR}/.env
 
 find /share -name "*_metatdata.yaml" -delete
-${PYTHON_DIR}/python ${ROOT_DIR}/analyse.py /share "14W6B2404_e1JKftOvHE4moV5w6VP5aitHVpX3Qcgcl8" --clean
+${PYTHON_DIR}/python ${ROOT_DIR}/lib/analyse.py /share "14W6B2404_e1JKftOvHE4moV5w6VP5aitHVpX3Qcgcl8" --clean
