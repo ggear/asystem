b62b41053dbfcc075a3d104c24f4e327896ab4cd
Graham Gear <graham.gear@nowhere.org>
2024-07-05 11:09:02 +0800

    Update asystem-10.100.4862

--- src/meg_eva/media/deploy.sh
+++ src/meg_eva/media/deploy.sh
@@ -4,8 +4,6 @@ ROOT_DIR=$(dirname $(readlink -f "$0"))
 
 HOSTS=$(echo $(basename $(dirname $(pwd))) | tr "_" "\n")
 
-
-
 HOST="$(grep $(echo "${HOSTS}" | head -1) ${ROOT_DIR}/../../../.hosts | tr '=' ' ' | tr ',' ' ' | awk '{ print $2 }')-$(echo "${HOSTS}" | head -1)"
 echo "------------------------------------------------------------" && echo "Server [${HOST}] media clean starting"
 ssh -o StrictHostKeyChecking=no root@${HOST} "/root/install/media/latest/config/bin/media-clean.sh"
