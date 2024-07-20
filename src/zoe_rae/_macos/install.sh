3403cb6afa65d0e4ccaf20790d31e8b9911675e5
Graham Gear <graham.gear@nowhere.org>
2024-07-20 11:37:58 +0800

    Update asystem-10.100.4940

--- src/zoe_rae/_macos/install.sh
+++ src/zoe_rae/_macos/install.sh
@@ -33,10 +33,9 @@ function sshcopyid_func() { cat ~/.ssh/id_rsa.pub | ssh $1 'mkdir .ssh ; cat >>.
 for SHARE in macmini-eva,1 macmini-eva,2 macmini-eva,3 macmini-meg,4 macmini-meg,5; do
   IFS=","
   set -- \${SHARE}
-  echo $1 and $2;
   mkdir -p ~/Desktop/share/\${2} >/dev/null 2>&1
   diskutil unmount force ~/Desktop/share/\${2} >/dev/null 2>&1
-  mount_smbfs //GUEST:@\${1}/${2} ~/Desktop/share/\${1} >/dev/null 2>&1
+  mount_smbfs //GUEST:@\${1}/share-\${2} ~/Desktop/share/\${2} >/dev/null 2>&1
 done
 
 #for SHARE in "macmini-eva/share-1" "macmini-eva/share-2" "macmini-eva/share-3" "macmini-meg/share-4" "macmini-meg/share-5"; do
