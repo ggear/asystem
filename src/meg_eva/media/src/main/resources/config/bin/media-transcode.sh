74f5d94eb22a5ef5424abee29e961d03da1902c4
Graham Gear <graham.gear@nowhere.org>
2024-07-20 15:16:46 +0800

    Update asystem-10.100.4946

--- src/meg_eva/media/src/main/resources/config/bin/media-transcode.sh
+++ src/meg_eva/media/src/main/resources/config/bin/media-transcode.sh
@@ -4,10 +4,10 @@ ROOT_DIR=$(dirname $(readlink -f "$0"))
 
 . ${ROOT_DIR}/.env
 
-find . -name transcode.sh
-#for SHARE_DIR in ${SHARE_DIRS}; do ${PYTHON_DIR}/python --version && echo $SHARE_DIR; done
-
-
-# outside of /share or inside /share - $SHARES/tmp/script/transcode.sh
-# inside /share/idx - tmp/script/transcode.sh
-# inside /share/idx/media - find transcode.sh
+if [ ! -z "${SHARE_DIR_MEDIA}" ]; then
+  find . -name transcode.sh -exec "{}" +
+elif [ ! -z "${SHARE_DIR}" ]; then
+  "${SHARE_DIR}/tmp/script/transcode.sh"
+else
+  for SHARE_DIRS_ITEM in ${SHARE_DIRS}; do "${SHARE_DIRS_ITEM}/tmp/script/transcode.sh"; done
+fi
