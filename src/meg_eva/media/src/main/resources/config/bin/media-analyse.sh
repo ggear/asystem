2d9942fc5095d7d994fd4e8bbbe9851a925fbecb
Graham Gear <graham.gear@nowhere.org>
2024-07-23 14:30:39 +0800

    Update asystem-10.100.4958

--- src/meg_eva/media/src/main/resources/config/bin/media-analyse.sh
+++ src/meg_eva/media/src/main/resources/config/bin/media-analyse.sh
@@ -8,7 +8,7 @@ SHEET_GUID="14W6B2404_e1JKftOvHE4moV5w6VP5aitHVpX3Qcgcl8"
 
 if [ -n "${SHARE_DIR_MEDIA}" ]; then
   "${ROOT_DIR}/lib/clean.sh" "${PWD}"
-  "${PYTHON_DIR}/python" "${ROOT_DIR}/lib/analyse.py" "${PWD}" "${SHEET_GUID}"
+  "${PYTHON_DIR}/python" "${ROOT_DIR}/lib/analyse.py" "${PWD}" "${SHEET_GUID}" --verbose
 elif [ -n "${SHARE_DIR}" ]; then
   "${PYTHON_DIR}/python" "${ROOT_DIR}/lib/analyse.py" "${SHARE_DIR}" "${SHEET_GUID}"
 else
