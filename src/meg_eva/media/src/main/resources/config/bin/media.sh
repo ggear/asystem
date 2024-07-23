57a653528f909dc0f5f8efb5e5ce070153f64b8d
Graham Gear <graham.gear@nowhere.org>
2024-07-23 16:59:16 +0800

    Update asystem-10.100.4965

--- src/meg_eva/media/src/main/resources/config/bin/media.sh
+++ src/meg_eva/media/src/main/resources/config/bin/media.sh
@@ -5,7 +5,6 @@ ROOT_DIR=$(dirname $(readlink -f "$0"))
 . "${ROOT_DIR}/.env_media"
 
 "${ROOT_DIR}/media-normalise.sh"
-"${ROOT_DIR}/media-rename.sh"
 "${ROOT_DIR}/media-analyse.sh"
 "${ROOT_DIR}/media-refresh.sh"
-"${ROOT_DIR}/media-report.sh"
+"${ROOT_DIR}/media-space.sh"
