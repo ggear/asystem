0f7b4611625ffd232cbd5b2a0a38f27506e9f21c
Graham Gear <graham.gear@nowhere.org>
2024-07-23 16:21:13 +0800

    Update asystem-10.100.4963

--- src/meg_eva/media/src/main/resources/config/bin/lib/_transcribe.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-ROOT_DIR=$(dirname $(readlink -f "$0"))
-
-# start in tmp dir, rename once finished
--- src/meg_eva/media/src/main/resources/config/bin/lib/import.sh
+++ src/meg_eva/media/src/main/resources/config/bin/lib/import.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+ROOT_DIR=$(dirname $(readlink -f "$0"))
+
 . "${ROOT_DIR}/../.env_media"
 
 WORKING_DIR=${1}
