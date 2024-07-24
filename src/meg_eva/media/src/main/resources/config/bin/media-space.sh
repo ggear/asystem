f292798ac2de48642e5a016556bf4667bbfd43a2
Graham Gear <graham.gear@nowhere.org>
2024-07-24 08:56:26 +0800

    Update asystem-10.100.4971

--- src/meg_eva/media/src/main/resources/config/bin/media-space.sh
+++ src/meg_eva/media/src/main/resources/config/bin/media-space.sh
@@ -10,4 +10,5 @@ if [ -n "${SHARE_DIR_MEDIA}" ]; then
 elif [ -n "${SHARE_DIR}" ]; then
   SHARE_DIRS_SPACE="${SHARE_DIR}"
 fi
-$(echo $([[ $(uname) == "Darwin" ]] && echo "g")"df") -h --output=target,size,used,avail,pcent ${SHARE_DIRS_SPACE}
+#$(echo $([[ $(uname) == "Darwin" ]] && echo "g")"df") -h --output=target,size,used,avail,pcent ${SHARE_DIRS_SPACE}
+duf --output  mountpoint,size,used,avail,usage ${SHARE_DIRS_SPACE}
