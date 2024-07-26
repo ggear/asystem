aa98fcd1c27486b25986bf88f2bd7e3e6b19c1ad
Graham Gear <graham.gear@nowhere.org>
2024-07-26 17:35:56 +0800

    Update asystem-10.100.4998-SNAPSHOT

--- src/meg_eva/media/src/main/resources/config/bin/lib/analyse.py
+++ src/meg_eva/media/src/main/resources/config/bin/lib/analyse.py
@@ -895,14 +895,19 @@ def _analyse(file_path_root, sheet_guid, clean=False, verbose=False):
             (pl.col("Action Index Base") + pl.col("Action Index Count"))
             .alias("Action Index")
         ).drop(["Action Index Sort", "Action Index Base", "Action Index Count"]).sort("Action Index")
-        metadata_merged_pl = metadata_merged_pl.with_columns([
-            pl.when(pl.col(pl.Utf8).str.len_bytes() == 0) \
-                .then(None).otherwise(pl.col(pl.Utf8)).name.keep()
-        ])
-        metadata_merged_pl = metadata_merged_pl[[
-            column.name for column in metadata_merged_pl \
-            if not (column.null_count() == metadata_merged_pl.height)
-        ]]
+
+
+        # TODO: Causing issues?
+        # metadata_merged_pl = metadata_merged_pl.with_columns([
+        #     pl.when(pl.col(pl.Utf8).str.len_bytes() == 0) \
+        #         .then(None).otherwise(pl.col(pl.Utf8)).name.keep()
+        # ])
+        # metadata_merged_pl = metadata_merged_pl[[
+        #     column.name for column in metadata_merged_pl \
+        #     if not (column.null_count() == metadata_merged_pl.height)
+        # ]]
+
+
     if verbose:
         print("done", flush=True)
     if len(metadata_merged_pl) > 0:
