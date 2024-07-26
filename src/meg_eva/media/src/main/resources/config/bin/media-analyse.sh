1389f9e261065a674a5c13e2b68a16d6f2fa9c21
Graham Gear <graham.gear@nowhere.org>
2024-07-26 09:33:58 +0800

    Update asystem-10.100.4987

--- src/meg_eva/media/src/main/resources/config/bin/media-analyse.sh
+++ src/meg_eva/media/src/main/resources/config/bin/media-analyse.sh
@@ -10,7 +10,7 @@ if [ -n "${SHARE_DIR_MEDIA}" ]; then
   "${ROOT_DIR}/lib/clean.sh" "${PWD}"
   "${PYTHON_DIR}/python" "${ROOT_DIR}/lib/analyse.py" "${PWD}" "${SHEET_GUID}" --verbose
 elif [ -n "${SHARE_DIR}" ]; then
-  "${PYTHON_DIR}/python" "${ROOT_DIR}/lib/analyse.py" "${SHARE_DIR}" "${SHEET_GUID}"
+  "${PYTHON_DIR}/python" "${ROOT_DIR}/lib/analyse.py" "${SHARE_DIR}" "${SHEET_GUID}" --verbose
 else
   for _SHARE_DIR in ${SHARE_DIRS_LOCAL}; do "${PYTHON_DIR}/python" "${ROOT_DIR}/lib/analyse.py" "${_SHARE_DIR}" "${SHEET_GUID}"; done
 fi
