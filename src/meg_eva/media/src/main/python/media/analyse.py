39a0636052221505277b01b78468316c558c2156
Graham Gear <graham.gear@nowhere.org>
2024-07-27 10:06:34 +0800

    Update asystem-10.100.5012

--- src/meg_eva/media/src/main/python/media/analyse.py
+++ src/meg_eva/media/src/main/python/media/analyse.py
@@ -603,7 +603,7 @@ def _analyse(file_path_root, sheet_guid, clean=False, verbose=False):
             .filter(pl.col("file_name") == "Any Given Sunday (1999).mkv") \
             .select("file_name", "^audio.*$")
         print(test)
-
+    return 0
 
 
     metadata_local_pl = _format_columns(pl.DataFrame(metadata_enriched_list))
