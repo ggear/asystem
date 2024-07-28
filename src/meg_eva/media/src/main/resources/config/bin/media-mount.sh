4615456a1c534eb10d0af39e106f1926918422ff
Graham Gear <graham.gear@nowhere.org>
2024-07-28 12:59:38 +0800

    Update asystem-10.100.5022

--- src/meg_eva/media/src/main/resources/config/bin/media-mount.sh
+++ src/meg_eva/media/src/main/resources/config/bin/media-mount.sh
@@ -4,17 +4,20 @@ ROOT_DIR=$(dirname $(readlink -f "$0"))
 
 . "${ROOT_DIR}/.env_media"
 
-echo -n "Mounting '${SHARE_ROOT}' ... "
 if [ $(uname) == "Darwin" ]; then
+  echo -n "Mounting '${SHARE_ROOT}' ... "
   for _SHARE in macmini-eva,1 macmini-eva,2 macmini-eva,3 macmini-meg,4 macmini-meg,5; do
     IFS=","
     set -- ${_SHARE}
     mkdir -p ~/Desktop/share/${2}
     diskutil unmount force ~/Desktop/share/${2} 2>&1 >/dev/null
     mount_smbfs //GUEST:@${1}/share-${2} ~/Desktop/share/${2}
+    echo "done"
   done
 else
-  umount -fq /media/usbdrive
-  mount -t exfat "/dev/"$(lsblk -ro name,label | grep GRAHAM | awk 'BEGIN{FS=OFS=" "}{print $1}') /media/usbdrive
+  IMPORT_MEDIA_DEV="/dev/"$(lsblk -ro name,label | grep GRAHAM | awk 'BEGIN{FS=OFS=" "}{print $1}')
+  echo -n "Mounting '${IMPORT_MEDIA_DEV}' ... "
+  umount -fq /media/usbdrive 2>&1 >/dev/null
+  mount -t exfat ${IMPORT_MEDIA_DEV} /media/usbdrive
+  echo "done"
 fi
-echo "done"
