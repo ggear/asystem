5a150d9f343c0e9b73ab0ffb5202e9c400129ab1
Graham Gear <graham.gear@nowhere.org>
2024-08-02 12:43:27 +0800

    Update

--- src/meg_eva/media/src/main/resources/config/bin/media-transcode.sh
+++ src/meg_eva/media/src/main/resources/config/bin/media-transcode.sh
@@ -5,7 +5,7 @@ ROOT_DIR=$(dirname $(readlink -f "$0"))
 . "${ROOT_DIR}/.env_media"
 
 if [ -n "${SHARE_DIR_MEDIA}" ]; then
-  find . -name transcode.sh -exec "{}" +
+  find . -name transcode.sh -exec "{}" \;
 elif [ -n "${SHARE_DIR}" ]; then
   "${SHARE_DIR}/tmp/script/transcode.sh"
 else
