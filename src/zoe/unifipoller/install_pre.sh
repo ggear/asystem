6dead9d08f416054c07af32596e2260f2326a04a
Graham Gear <graham.gear@nowhere.org>
2024-12-16 14:50:16 +0800

    Update asystem-10.100.5223-SNAPSHOT

--- src/zoe/unifipoller/install_pre.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-# TODO: Setup user .profile user/tokens in UniFi Portal with 'Limited Admin' Role and 'Read Only' Permissions
-# TODO: Setup system dashboards from https://grafana.com/grafana/dashboards?search=unifi-poller
