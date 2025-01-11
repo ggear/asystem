98976728c4480a7ecee5cfd8bac2c41223eca3b9
Graham Gear <graham.gear@nowhere.org>
2025-01-11 09:53:38 +0800

    Update asystem-10.100.5227

--- src/net/udmutilities/src/main/resources/image/udm-certificates/20-certifcates.sh
+++ src/net/udmutilities/src/main/resources/image/udm-certificates/20-certifcates.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 if ! diff -q /root/install/udmutilities/latest/image/udm-certificates/.key.pem /data/unifi-core/image/unifi-core.key &>/dev/null ||
-  ! diff -q /root/install/udmutilities/latest/image/udm-certificates/certificate.pem /data/unifi-core/image/unifi-core.crt &>/dev/null; then
+  ! diff -q /root/install/udmutilities/latest/image/udm-certificates/certificate.pem /data/unifi-core/config/unifi-core.crt &>/dev/null; then
   echo -n "Installing latest certificates ..."
   cp -uvf /root/install/udmutilities/latest/image/udm-certificates/.key.pem /data/unifi-core/image/unifi-core.key >/dev/null
-  cp -uvf /root/install/udmutilities/latest/image/udm-certificates/certificate.pem /data/unifi-core/image/unifi-core.crt >/dev/null
+  cp -uvf /root/install/udmutilities/latest/image/udm-certificates/certificate.pem /data/unifi-core/config/unifi-core.crt >/dev/null
   echo "done"
   echo -n "Restarting UniFi ..."
   systemctl restart unifi-core
