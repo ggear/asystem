f1bd663cae49ed3577b721f15414c53d0555de53
Graham Gear <graham.gear@nowhere.org>
2025-01-26 11:26:12 +0800

    Update asystem-10.100.5253

--- src/eva/sabnzbd/src/main/resources/data/image/healthcheck.sh
+++ src/eva/sabnzbd/src/main/resources/data/image/healthcheck.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-if STATUS="$(curl -sf --connect-timeout 2 --max-time 2 "http://${SABNZBD_SERVICE_PROD}:${SABNZBD_HTTP_PORT}/sabnzbd/api?output=json&apikey=${SABNZBD_API_KEY}&mode=status&skip_dashboard=0")" && [ "$(jq -er '.status.paused' <<<"${STATUS}")" == "false" ] && [ "$(jq -er '[.status.servers[].serveractiveconn] | add' <<<"${STATUS}")" -gt 0 ]; then exit 0; else exit 1; fi
+if STATUS="$(curl -sf --connect-timeout 2 --max-time 2 "http://${SABNZBD_SERVICE_PROD}:${SABNZBD_HTTP_PORT}/sabnzbd/api?output=json&apikey=${SABNZBD_API_KEY}&mode=status&skip_dashboard=0")" && [ "$(jq -er ".status.paused" <<<"${STATUS}")" == "false" ] && [ "$(jq -er "[.status.servers[].serveractiveconn] | add" <<<"${STATUS}")" -gt 0 ]; then exit 0; else exit 1; fi
