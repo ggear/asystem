fb4713842ab66a2323b95dde57eeab6840831d3e
Graham Gear <graham.gear@nowhere.org>
2025-01-26 11:19:12 +0800

    Update asystem-10.100.5252

--- src/eva/sabnzbd/src/main/resources/data/image/healthcheck.sh
+++ src/eva/sabnzbd/src/main/resources/data/image/healthcheck.sh
@@ -1,13 +1,3 @@
 #!/bin/bash
 
-set -eo pipefail
-
-HEALTHCHECK_VERBOSE=${HEALTHCHECK_VERBOSE:-false}
-if [ "${HEALTHCHECK_VERBOSE}" == true ]; then
-  CURL_CMD="curl -f --connect-timeout 2 --max-time 2"
-  set -o xtrace
-else
-  CURL_CMD="curl -sf --connect-timeout 2 --max-time 2"
-fi
-
 if STATUS="$(curl -sf --connect-timeout 2 --max-time 2 "http://${SABNZBD_SERVICE_PROD}:${SABNZBD_HTTP_PORT}/sabnzbd/api?output=json&apikey=${SABNZBD_API_KEY}&mode=status&skip_dashboard=0")" && [ "$(jq -er '.status.paused' <<<"${STATUS}")" == "false" ] && [ "$(jq -er '[.status.servers[].serveractiveconn] | add' <<<"${STATUS}")" -gt 0 ]; then exit 0; else exit 1; fi
