d1db857555283a711d5fd63401e349edb68ac339
Graham Gear <graham.gear@nowhere.org>
2025-01-31 15:40:38 +0800

    Update asystem-10.100.5280-SNAPSHOT

--- src/eva/appdaemon/src/main/resources/image/healthcheck.sh
+++ src/eva/appdaemon/src/main/resources/image/healthcheck.sh
@@ -11,7 +11,7 @@ else
 fi
 
 function alive() {
-  if ALIVE=$(influx ping --host http://${INFLUXDB_SERVICE}:${INFLUXDB_HTTP_PORT} 2>&1) &&
+  if ALIVE="$(${CURL_CMD} "http://${MLFLOW_SERVICE}:${MLFLOW_HTTP_PORT}/health")" &&
     [ "${ALIVE}" == "OK" ]; then
     return 0
   else
@@ -20,19 +20,9 @@ function alive() {
 }
 
 function ready() {
-  if [ "$(influx ping --host http://${INFLUXDB_SERVICE}:${INFLUXDB_HTTP_PORT})" == "OK" ] &&
-    influx query 'from(bucket:"'"${INFLUXDB_BUCKET_HOME_PUBLIC}"'") |> range(start:-15m) |> filter(fn: (r) => r["_measurement"] == "a_non_existent_metric")' &&
-    influx query 'from(bucket:"'"${INFLUXDB_BUCKET_HOME_PRIVATE}"'") |> range(start:-15m) |> filter(fn: (r) => r["_measurement"] == "a_non_existent_metric")' &&
-    influx query 'from(bucket:"'"${INFLUXDB_BUCKET_DATA_PUBLIC}"'") |> range(start:-15m) |> filter(fn: (r) => r["_measurement"] == "a_non_existent_metric")' &&
-    influx query 'from(bucket:"'"${INFLUXDB_BUCKET_DATA_PRIVATE}"'") |> range(start:-15m) |> filter(fn: (r) => r["_measurement"] == "a_non_existent_metric")' &&
-    influx query 'from(bucket:"'"${INFLUXDB_BUCKET_HOST_PRIVATE}"'") |> range(start:-15m) |> filter(fn: (r) => r["_measurement"] == "a_non_existent_metric")' &&
-    [ "$(${CURL_CMD} "http://${INFLUXDB_SERVICE}:${INFLUXDB_HTTP_PORT}/query" --user "${INFLUXDB_USER_PUBLIC}:${INFLUXDB_TOKEN_PUBLIC_V1}" --data-urlencode "db=${INFLUXDB_BUCKET_HOME_PUBLIC}" --data-urlencode "q=SELECT count(*) FROM a_non_existent_metric WHERE time >= now() - 15m" | jq -er .results[0].statement_id)" == "0" ] &&
-    [ "$(${CURL_CMD} "http://${INFLUXDB_SERVICE}:${INFLUXDB_HTTP_PORT}/query" --user "${INFLUXDB_USER_PUBLIC}:${INFLUXDB_TOKEN_PUBLIC_V1}" --data-urlencode "db=${INFLUXDB_BUCKET_DATA_PUBLIC}" --data-urlencode "q=SELECT count(*) FROM a_non_existent_metric WHERE time >= now() - 15m" | jq -er .results[0].statement_id)" == "0" ] &&
-    [ "$(${CURL_CMD} "http://${INFLUXDB_SERVICE}:${INFLUXDB_HTTP_PORT}/query" --user "${INFLUXDB_USER_PRIVATE}:${INFLUXDB_TOKEN_PRIVATE_V1}" --data-urlencode "db=${INFLUXDB_BUCKET_HOME_PUBLIC}" --data-urlencode "q=SELECT count(*) FROM a_non_existent_metric WHERE time >= now() - 15m" | jq -er .results[0].statement_id)" == "0" ] &&
-    [ "$(${CURL_CMD} "http://${INFLUXDB_SERVICE}:${INFLUXDB_HTTP_PORT}/query" --user "${INFLUXDB_USER_PRIVATE}:${INFLUXDB_TOKEN_PRIVATE_V1}" --data-urlencode "db=${INFLUXDB_BUCKET_HOME_PRIVATE}" --data-urlencode "q=SELECT count(*) FROM a_non_existent_metric WHERE time >= now() - 15m" | jq -er .results[0].statement_id)" == "0" ] &&
-    [ "$(${CURL_CMD} "http://${INFLUXDB_SERVICE}:${INFLUXDB_HTTP_PORT}/query" --user "${INFLUXDB_USER_PRIVATE}:${INFLUXDB_TOKEN_PRIVATE_V1}" --data-urlencode "db=${INFLUXDB_BUCKET_DATA_PUBLIC}" --data-urlencode "q=SELECT count(*) FROM a_non_existent_metric WHERE time >= now() - 15m" | jq -er .results[0].statement_id)" == "0" ] &&
-    [ "$(${CURL_CMD} "http://${INFLUXDB_SERVICE}:${INFLUXDB_HTTP_PORT}/query" --user "${INFLUXDB_USER_PRIVATE}:${INFLUXDB_TOKEN_PRIVATE_V1}" --data-urlencode "db=${INFLUXDB_BUCKET_DATA_PRIVATE}" --data-urlencode "q=SELECT count(*) FROM a_non_existent_metric WHERE time >= now() - 15m" | jq -er .results[0].statement_id)" == "0" ] &&
-    [ "$(${CURL_CMD} "http://${INFLUXDB_SERVICE}:${INFLUXDB_HTTP_PORT}/query" --user "${INFLUXDB_USER_PRIVATE}:${INFLUXDB_TOKEN_PRIVATE_V1}" --data-urlencode "db=${INFLUXDB_BUCKET_HOST_PRIVATE}" --data-urlencode "q=SELECT count(*) FROM a_non_existent_metric WHERE time >= now() - 15m" | jq -er .results[0].statement_id)" == "0" ]; then
+  #  TODO: Provide implementation that relfects on models being served
+  if READY="$(${CURL_CMD} "http://${MLFLOW_SERVICE}:${MLFLOW_HTTP_PORT}/health")" &&
+    [ "${READY}" == "OK" ]; then
     return 0
   else
     return 1
