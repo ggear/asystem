b5717104c156f550ba19af629d89a3a3baf434f0
Graham Gear <graham.gear@nowhere.org>
2025-02-01 13:19:12 +0800

    Update asystem-10.100.5295

--- src/eva/appdaemon/install_pre.sh
+++ src/eva/appdaemon/install_pre.sh
@@ -3,5 +3,5 @@
 SERVICE_HOME=/home/asystem/appdaemon/latest
 SERVICE_INSTALL=/var/lib/asystem/install/appdaemon/latest
 
-rm -rf ${SERVICE_HOME}/apps && cp -rf ${SERVICE_INSTALL}/apps ${SERVICE_HOME}
+rm -rf ${SERVICE_HOME}/apps && cp -rf ${SERVICE_INSTALL}/data/apps ${SERVICE_HOME}
 rm -rf ${SERVICE_HOME}/dashboards && cp -rf ${SERVICE_INSTALL}/data/dashboards ${SERVICE_HOME}
