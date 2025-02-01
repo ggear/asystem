3c891eeeeb50ae2231427efa4bec374b83fe7177
Graham Gear <graham.gear@nowhere.org>
2025-02-01 12:45:53 +0800

    Update asystem-10.100.5291

--- src/eva/appdaemon/src/main/resources/data/apps/hello.py
+++ src/eva/appdaemon/src/main/resources/data/apps/hello.py
@@ -1,6 +1,13 @@
 import hassapi as hass
 
+
 class HelloWorld(hass.Hass):
     def initialize(self):
+        self.register_endpoint(self.my_callback, "hello")
         self.log("Hello from AppDaemon")
         self.log("You are now ready to run Apps!")
+
+    def my_callback(self, json_obj, cb_args):
+        self.log(json_obj)
+        response = {"message": "Hello World"}
+        return response, 200
