43f5abedacfe861d0d1c099d76d87023059c6d0b
Graham Gear <graham.gear@nowhere.org>
2025-02-01 17:18:58 +0800

    Update asystem-10.100.5308

--- src/eva/appdaemon/generate.sh
+++ src/eva/appdaemon/generate.sh
@@ -4,6 +4,11 @@
 
 ROOT_DIR=$(dirname $(readlink -f "$0"))
 
+HOST="$(grep $(basename $(dirname ${ROOT_DIR})) ${ROOT_DIR}/../../../.hosts | tr '=' ' ' | tr ',' ' ' | awk '{ print $2 }')-$(basename $(dirname ${ROOT_DIR}))"
+HOST_LETSENCRYPT="$(grep $(basename $(dirname $(find ${ROOT_DIR}/../../.. -type d -mindepth 3 -maxdepth 3 -name letsencrypt))) ${ROOT_DIR}/../../../.hosts | tr '=' ' ' | tr ',' ' ' | awk '{ print $2 }')-$(basename $(dirname $(find ${ROOT_DIR}/../../.. -type d -mindepth 3 -maxdepth 3 -name letsencrypt)))"
+
+${ROOT_DIR}/src/main/resources/image/certificates.sh pull ${HOST_LETSENCRYPT} ${HOST}
+
 rm -rf ${ROOT_DIR}/src/main/resources/data/apps/*.py
 for PY_FILE in ${ROOT_DIR}/src/main/python/appdaemon/*.py; do
   PY_PATH="${ROOT_DIR}/src/main/resources/data/apps/$(basename "${PY_FILE}")"
--- src/eva/appdaemon/src/build/python/appdaemon/generate.py
+++ src/eva/appdaemon/src/build/python/appdaemon/generate.py
@@ -1,6 +1,28 @@
+import glob
+import os
+import re
+import sys
+from os.path import *
+
+import pandas as pd
+import requests
+import urllib3
+
 from homeassistant.generate import load_entity_metadata
+from homeassistant.generate import load_env
+from homeassistant.generate import load_modules
+from homeassistant.generate import write_certificates
+
+DIR_ROOT = abspath(join(dirname(realpath(__file__)), "../../../.."))
+
+DNSMASQ_CONF_PREFIX = "dhcp.dhcpServers"
+UNIFI_CONTROLLER_URL = "https://unifi.local.janeandgraham.com:443"
 
 if __name__ == "__main__":
+    env = load_env(DIR_ROOT)
+    modules = load_modules(load_disabled=False, load_infrastrcture=False)
     metadata_df = load_entity_metadata()
+
+    write_certificates("appdaemon", join(DIR_ROOT, "src/main/resources/image"))
 
     print("Build generate script [appdaemon] completed".format())
