3d678023b1ec6f115e484085e289f344b60afb2b
Graham Gear <graham.gear@nowhere.org>
2025-02-03 16:42:57 +0800

    Update asystem-10.100.5330

--- src/meg/homeassistant/deploy.sh
+++ src/meg/homeassistant/deploy.sh
@@ -6,6 +6,6 @@ HOME="/home/asystem/$(basename ${ROOT_DIR})/latest"
 INSTALL="/var/lib/asystem/install/$(basename ${ROOT_DIR})/latest"
 HOST="$(grep $(basename $(dirname ${ROOT_DIR})) ${ROOT_DIR}/../../../.hosts | tr '=' ' ' | tr ',' ' ' | awk '{ print $2 }')-$(basename $(dirname ${ROOT_DIR}))"
 
-ssh root@${HOST} "rm -rf ${HOME}/custom_components && rm -rf ${HOME}/custom_packages && rm -rf ${HOME}/ui-lovelace && rm -rf ${HOME}/www"
-scp -r ${ROOT_DIR}/src/main/resources/data/* root@${HOST}:${HOME}
-ssh root@${HOST} "cd ${INSTALL} && docker compose --compatibility restart"
+echo ssh root@${HOST} "rm -rf ${HOME}/custom_components && rm -rf ${HOME}/custom_packages && rm -rf ${HOME}/ui-lovelace && rm -rf ${HOME}/www"
+echo scp -r ${ROOT_DIR}/src/main/resources/data/* root@${HOST}:${HOME}
+echo ssh root@${HOST} "cd ${INSTALL} && docker compose --compatibility restart"
