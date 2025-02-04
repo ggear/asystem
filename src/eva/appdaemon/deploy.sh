0bf87173812fb8175d5b8b1b744fd08854ec52b6
Graham Gear <graham.gear@nowhere.org>
2025-02-04 15:47:44 +0800

    Update asystem-10.100.5351

--- src/eva/appdaemon/deploy.sh
+++ src/eva/appdaemon/deploy.sh
@@ -6,6 +6,5 @@ HOME="/home/asystem/$(basename "${ROOT_DIR}")/latest"
 INSTALL="/var/lib/asystem/install/$(basename "${ROOT_DIR}")/latest"
 HOST="$(grep "$(basename "$(dirname "${ROOT_DIR}")")" "${ROOT_DIR}/../../../.hosts" | tr '=' ' ' | tr ',' ' ' | awk '{ print $2 }')"-"$(basename "$(dirname "${ROOT_DIR}")")"
 
-ssh root@${HOST} "rm -rf ${HOME}/apps && rm -rf ${HOME}/dashboards"
-scp -r ${ROOT_DIR}/src/main/resources/data/apps ${ROOT_DIR}/src/main/resources/data/dashboards root@${HOST}:${HOME}
-
+ssh root@"${HOST}" "rm -rf ${HOME}/apps && rm -rf ${HOME}/dashboards"
+scp -r "${ROOT_DIR}/src/main/resources/data/apps" "${ROOT_DIR}/src/main/resources/data/dashboards" root@"${HOST}":"${HOME}"
