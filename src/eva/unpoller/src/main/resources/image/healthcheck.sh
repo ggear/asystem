2cbca1cec4a2bd17984b3d49e9474639b6c3eaf1
Graham Gear <graham.gear@nowhere.org>
2025-02-11 13:40:10 +0800

    Update asystem-10.100.5374

---
+++ src/eva/unpoller/src/main/resources/image/healthcheck.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+set -eo pipefail
+
+HEALTHCHECK_VERBOSE=${HEALTHCHECK_VERBOSE:-false}
+if [ "${HEALTHCHECK_VERBOSE}" == true ]; then
+  CURL_CMD="curl -f --connect-timeout 2 --max-time 2"
+  set -o xtrace
+else
+  CURL_CMD="curl -sf --connect-timeout 2 --max-time 2"
+fi
+
+function alive() {
+  return 0
+  if ALIVE="$(${CURL_CMD} -LI "https://${APPDAEMON_SERVICE}:${APPDAEMON_HTTP_PORT}/aui/index.html" | tac | tac | head -n 1 | cut -d$' ' -f2)" &&
+    [ "${ALIVE}" == "200" ]; then
+    return 0
+  else
+    return 1
+  fi
+}
+
+function ready() {
+  return 0
+  if READY="$(${CURL_CMD} -H "x-ad-access: ${APPDAEMON_TOKEN}" -H "Content-Type: application/json" "https://${APPDAEMON_SERVICE}:${APPDAEMON_HTTP_PORT}/api/appdaemon/health")" &&
+    [ "$(jq -er .health <<<"${READY}")" == "OK" ]; then
+    return 0
+  else
+    return 1
+  fi
+}
+
+[ "$#" -eq 1 ] && [ "${1}" == "alive" ] && exit $(alive)
+exit $(ready)
