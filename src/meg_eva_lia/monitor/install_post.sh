66a1c15d0604517b50d4fbf285ae6b51ac7109a6
Graham Gear <graham.gear@nowhere.org>
2025-02-11 16:58:31 +0800

    Update asystem-10.100.5394

--- src/meg_eva_lia/monitor/install_post.sh
+++ src/meg_eva_lia/monitor/install_post.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-docker exec monitor /asystem/runtime/mqtt.sh
+docker exec monitor /asystem/etc/mqtt.sh
 
 printf "Entity Metadata publish script [monitor] sleeping before publishing data topics ... " && sleep 2 && printf "done\n\nEntity Metadata publish script [monitor] publishing data topics:\n"
 
---
+++ src/meg_eva_lia/monitor/src/main/resources/image/healthcheck.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+set -eo pipefail
+
+HEALTHCHECK_VERBOSE=${HEALTHCHECK_VERBOSE:-false}
+if [ "${HEALTHCHECK_VERBOSE}" == true ]; then
+  CURL_CMD="curl -f --connect-timeout 2 --max-time 2"
+  set -o xtrace
+else
+  CURL_CMD="curl -sf --connect-timeout 2 --max-time 2"
+fi
+
+function alive() {
+  # TODO
+  return 0
+  if [ "$(${CURL_CMD} "http://${UNPOLLER_SERVICE}:${UNPOLLER_HTTP_PORT}/health")" == "OK" ]; then
+    return 0
+  else
+    return 1
+  fi
+}
+
+function ready() {
+  # TODO
+  return 0
+  if [ "$(${CURL_CMD} "http://${UNPOLLER_SERVICE}:${UNPOLLER_HTTP_PORT}/api/v1/output/influxdb/events" | jq -er .influxdb.latest | cut -d 'T' -f 1)" == "$(date --rfc-3339=ns | sed 's/ /T/' | cut -d 'T' -f 1)" ]; then
+    return 0
+  else
+    return 1
+  fi
+}
+
+[ "$#" -eq 1 ] && [ "${1}" == "alive" ] && exit $(alive)
+exit $(ready)
