8cf583b509eeebd196ef8d1f5a561cfb37edb59c
Graham Gear <graham.gear@nowhere.org>
2025-02-13 12:45:42 +0800

    Update asystem-10.100.5424

--- src/meg/homeassistant/src/main/resources/data/bootstrap.sh
+++ src/meg/homeassistant/src/main/resources/data/bootstrap.sh
@@ -4,15 +4,17 @@ echo "--------------------------------------------------------------------------
 echo "Bootstrap initialising ..."
 echo "--------------------------------------------------------------------------------"
 
-while ! "/asystem/etc/healthcheck.sh"; do
+while ! "/asystem/etc/healthcheck.sh" alive; do
   echo "Waiting for service to come alive ..." && sleep 1
 done
-
-set -eo pipefail
 
 echo "--------------------------------------------------------------------------------"
 echo "Bootstrap starting ..."
 echo "--------------------------------------------------------------------------------"
+
+while ! "/asystem/etc/healthcheck.sh" alive; do
+  echo "Waiting for service to become ready ..." && sleep 1
+done
 
 echo "--------------------------------------------------------------------------------"
 echo "Bootstrap finished"
