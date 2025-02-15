17a31e698cf10c3aefaeefbbbe83a2ae94205bff
Graham Gear <graham.gear@nowhere.org>
2025-02-15 11:04:39 +0800

    Update asystem-10.100.5437

--- src/eva/influxdb3/src/main/resources/image/healthcheck.sh
+++ src/eva/influxdb3/src/main/resources/image/healthcheck.sh
@@ -8,7 +8,7 @@ if [ "${HEALTHCHECK_VERBOSE}" == true ]; then
 fi
 
 function alive() {
-  if influxdb3 show databases >/dev/null 2>&1; then
+  if ! influxdb3 show databases >/dev/null 2>&1; then
     return 0
   else
     return 1
