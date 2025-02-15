6c0fb3394d1321f3d14fc94b5da0b14d0408629c
Graham Gear <graham.gear@nowhere.org>
2025-02-15 12:12:33 +0800

    Update asystem-10.100.5451

--- src/eva/influxdb3/src/main/resources/image/bootstrap.sh
+++ src/eva/influxdb3/src/main/resources/image/bootstrap.sh
@@ -14,7 +14,8 @@ echo "--------------------------------------------------------------------------
 echo "Bootstrap starting ..."
 echo "--------------------------------------------------------------------------------"
 
-#[[ $(influxdb3 show databases | grep -c host_private) -eq 0 ]] && influxdb3 create database host_private
+# TODO: Work out if a REST API exists to create databases, else raise feature request for REST API doc or HTTP_BIND_ADDRESS respect, or both
+[ $(curl -sf "http://${INFLUXDB3_SERVICE}:${INFLUXDB3_PORT}/api/v3/configure/database?format=csv&show_deleted=false" | grep -c host_private) -eq 0 ] && echo "Need to create database from outside of host!"
 
 echo "--------------------------------------------------------------------------------"
 echo "Bootstrap finished"
