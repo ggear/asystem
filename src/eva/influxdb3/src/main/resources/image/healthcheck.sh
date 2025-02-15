6779a550ef7bd783509989c6387497e6d7ffe50d
Graham Gear <graham.gear@nowhere.org>
2025-02-15 11:36:27 +0800

    Update asystem-10.100.5447

--- src/eva/influxdb3/src/main/resources/image/healthcheck.sh
+++ src/eva/influxdb3/src/main/resources/image/healthcheck.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -eo pipefail
+
 HEALTHCHECK_VERBOSE=${HEALTHCHECK_VERBOSE:-false}
 if [ "${HEALTHCHECK_VERBOSE}" == true ]; then
   CURL_CMD="curl -f --connect-timeout 2 --max-time 2"
@@ -9,7 +11,7 @@ else
 fi
 
 function alive() {
-  if [ $(${CURL_CMD} "http://127.0.0.1:8181/api/v3/configure/database?format=pretty&show_deleted=false" 2>/dev/null | wc -l) -gt 0 ]; then
+  if ${CURL_CMD} "http://${INFLUXDB3_SERVICE}:${INFLUXDB3_PORT}/api/v3/configure/database?format=pretty&show_deleted=false" 2>/dev/null; then
     return 0
   else
     return 1
