fa3941f46d0a2703c38197409cc212d533bc3c49
Graham Gear <graham.gear@nowhere.org>
2025-03-12 11:46:17 +0800

    Update asystem-10.100.5462

--- src/eva/rhasspy/src/main/resources/image/healthcheck.sh
+++ src/eva/rhasspy/src/main/resources/image/healthcheck.sh
@@ -4,28 +4,19 @@ set -eo pipefail
 
 HEALTHCHECK_VERBOSE=${HEALTHCHECK_VERBOSE:-false}
 if [ "${HEALTHCHECK_VERBOSE}" == true ]; then
-  CURL_CMD="curl -f --connect-timeout 2 --max-time 2"
   set -o xtrace
-else
-  CURL_CMD="curl -sf --connect-timeout 2 --max-time 2"
 fi
 
 function alive() {
-  # TODO: Provide implementation
-  return 0
-  if ALIVE="$(${CURL_CMD} -LI "https://${APPDAEMON_SERVICE}:${APPDAEMON_HTTP_PORT}/aui/index.html" | tac | tac | head -n 1 | cut -d$' ' -f2)" &&
-    [ "${ALIVE}" == "200" ]; then
-    return 0
+  if netcat -zw 1 ${RHASSPY_SERVICE} ${RHASSPY_API_PORT}; then
+    echo return 0
   else
-    return 1
+    echo return 1
   fi
 }
 
 function ready() {
-  # TODO: Provide implementation
-  return 0
-  if READY="$(${CURL_CMD} -H "x-ad-access: ${APPDAEMON_TOKEN}" -H "Content-Type: application/json" "https://${APPDAEMON_SERVICE}:${APPDAEMON_HTTP_PORT}/api/appdaemon/health")" &&
-    [ "$(jq -er .health <<<"${READY}")" == "OK" ]; then
+  if netcat -zw 1 ${RHASSPY_SERVICE} ${RHASSPY_API_PORT}; then
     return 0
   else
     return 1
