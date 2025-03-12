bc8abf2b2796eae0ca30c5cef373d2a9c60ab52d
Graham Gear <graham.gear@nowhere.org>
2025-03-12 12:36:32 +0800

    Update asystem-10.100.5466

--- src/eva/rhasspy/install_pre.sh
+++ src/eva/rhasspy/install_pre.sh
@@ -3,5 +3,6 @@
 SERVICE_HOME=/home/asystem/${SERVICE_NAME}/latest
 SERVICE_INSTALL=/var/lib/asystem/install/${SERVICE_NAME}/latest
 
-rm -rf ${SERVICE_HOME}/models/*
 rm -rf ${SERVICE_HOME}/train/*
+rm -rf ${SERVICE_HOME}/custom/*
+rm -rf ${SERVICE_HOME}/models/*
