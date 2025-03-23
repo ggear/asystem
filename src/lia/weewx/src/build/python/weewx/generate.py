c0676d6bbefcf329148a3eadb99bf75dea59e9f1
Graham Gear <graham.gear@nowhere.org>
2025-03-23 12:51:55 +0800

    Update asystem-10.100.5493

--- src/lia/weewx/src/build/python/weewx/generate.py
+++ src/lia/weewx/src/build/python/weewx/generate.py
@@ -45,5 +45,5 @@ if __name__ == "__main__":
         (metadata_df["name"].str.len() > 0) &
         (metadata_df["discovery_topic"].str.len() > 0)
         ]
-    write_entity_metadata("weewx", join(DIR_ROOT, "src/main/resources/image/config/mqtt"),
+    write_entity_metadata("weewx", join(DIR_ROOT, "src/main/resources/data/mqtt"),
                           metadata_publish_df, "homeassistant/+/weewx/#", "weewx/#")
