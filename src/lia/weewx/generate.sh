c21417af3a0e36ce997c15ce6a465e04864f49a8
Graham Gear <graham.gear@nowhere.org>
2025-03-23 14:03:20 +0800

    Update asystem-10.100.5500

--- src/lia/weewx/generate.sh
+++ src/lia/weewx/generate.sh
@@ -7,14 +7,14 @@ ROOT_DIR="$(dirname "$(readlink -f "$0")")"
 
 pull_repo "${ROOT_DIR}" "${1}" weewx weewx-core weewx/weewx v${WEEWX_VERSION}
 [ $(ls ${ROOT_DIR}/../../../.deps/weewx/weewx-core/dist/weewx*.whl 2>/dev/null | wc -l) -eq 0 ] && cd ${ROOT_DIR}/../../../.deps/weewx/weewx-core && rm -rf dist && make clean pypi-package
-mkdir -p ${ROOT_DIR}/src/main/resources/image/bin
-cp -nv ${ROOT_DIR}/../../../.deps/weewx/weewx-core/dist/weewx*.whl ${ROOT_DIR}/src/main/resources/image/bin
+mkdir -p ${ROOT_DIR}/src/main/resources/image/install
+cp -nv ${ROOT_DIR}/../../../.deps/weewx/weewx-core/dist/weewx*.whl ${ROOT_DIR}/src/main/resources/image/install
 
 VERSION=master
 pull_repo "${ROOT_DIR}" "${1}" weewx weewx-mqtt matthewwall/weewx-mqtt "${VERSION}"
 [ $(ls ${ROOT_DIR}/../../../.deps/weewx/weewx-mqtt/weewx-mqtt.zip~ 2>/dev/null | wc -l) -eq 0 ] && cd ${ROOT_DIR}/../../../.deps/weewx && zip -x "weewx-mqtt/.git*" -r weewx-mqtt.zip~ weewx-mqtt && mv ${ROOT_DIR}/../../../.deps/weewx/weewx-mqtt.zip~ ${ROOT_DIR}/../../../.deps/weewx/weewx-mqtt
-mkdir -p ${ROOT_DIR}/src/main/resources/image/bin
-cp -nv ${ROOT_DIR}/../../../.deps/weewx/weewx-mqtt/weewx-mqtt.zip~ ${ROOT_DIR}/src/main/resources/image/bin/weewx-mqtt.zip
+mkdir -p ${ROOT_DIR}/src/main/resources/image/install
+cp -nv ${ROOT_DIR}/../../../.deps/weewx/weewx-mqtt/weewx-mqtt.zip~ ${ROOT_DIR}/src/main/resources/image/install/weewx-mqtt.zip
 
 VERSION=ggear-skins_seasons
 pull_repo "${ROOT_DIR}" "${1}" weewx weewx-core-skins ggear/weewx "${VERSION}"
