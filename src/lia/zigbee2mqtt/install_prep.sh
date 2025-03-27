7aff1889504e178d7daeb467bf27725d1ab2a0c9
Graham Gear <graham.gear@nowhere.org>
2025-03-27 15:54:31 +0800

    Update asystem-10.100.5531

--- src/lia/zigbee2mqtt/install_prep.sh
+++ src/lia/zigbee2mqtt/install_prep.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
-# TODO: Re-enable once working
-#docker exec zigbee2mqtt /asystem/etc/mqtt.sh
-
+docker exec zigbee2mqtt /asystem/etc/mqtt.sh
 rm -rf /root/home/zigbee2mqtt/latest/log
