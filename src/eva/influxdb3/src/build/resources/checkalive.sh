351582125e4cc9c33d3bec72c486ca498bb726ad
Graham Gear <graham.gear@nowhere.org>
2025-04-03 14:11:22 +0800

    Update asystem-10.100.5569

--- src/eva/influxdb3/src/build/resources/checkalive.sh
+++ src/eva/influxdb3/src/build/resources/checkalive.sh
@@ -1,1 +1,1 @@
-[ "$(influx ping --host http://${INFLUXDB_SERVICE}:${INFLUXDB_HTTP_PORT} 2>&1)" == "OK" ]
+curl "http://${INFLUXDB3_SERVICE}:${INFLUXDB3_API_PORT}/api/v3/configure/database?format=csv&show_deleted=false" >/dev/null 2>&1
