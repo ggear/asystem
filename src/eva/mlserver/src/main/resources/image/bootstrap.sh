af16b6cd579426036b4564f13ae1d358787b1f00
Graham Gear <graham.gear@nowhere.org>
2025-04-04 08:53:44 +0800

    Update asystem-10.100.5579

--- src/eva/mlserver/src/main/resources/image/bootstrap.sh
+++ src/eva/mlserver/src/main/resources/image/bootstrap.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
 echo "--------------------------------------------------------------------------------"
-echo "Bootstrap initialising ..."
+echo "Service is starting ..."
 echo "--------------------------------------------------------------------------------"
 
-while ! "/asystem/etc/healthcheck.sh" alive; do
-  echo "Waiting for service to come alive ..." && sleep 1
+ASYSTEM_HOME=${ASYSTEM_HOME:-"/asystem/etc"}
+
+MESSAGE="Waiting for service to come alive ..."
+echo "${MESSAGE}"
+while ! "${ASYSTEM_HOME}/checkalive.sh"; do
+ echo "${MESSAGE}" && sleep 1
 done
 
 set -eo pipefail
@@ -14,6 +18,18 @@ echo "--------------------------------------------------------------------------
 echo "Bootstrap starting ..."
 echo "--------------------------------------------------------------------------------"
 
+# TODO: Provide implementation
+echo ''
+
 echo "--------------------------------------------------------------------------------"
 echo "Bootstrap finished"
 echo "--------------------------------------------------------------------------------"
+
+set +eo pipefail
+
+MESSAGE="Waiting for service to become ready ..."
+echo "${MESSAGE}"
+while ! "${ASYSTEM_HOME}/checkready.sh"; do
+  echo "${MESSAGE}" && sleep 1
+done
+echo "----------" && echo "Service has started"
