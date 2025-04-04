cce0dbbd899224e10f4cb237a7ab4c54cc699a5b
Graham Gear <graham.gear@nowhere.org>
2025-04-04 09:05:42 +0800

    Update asystem-10.100.5580-SNAPSHOT

--- src/eva/mlserver/src/build/python/mlserver/generate.py
+++ src/eva/mlserver/src/build/python/mlserver/generate.py
@@ -1,7 +1,5 @@
 from homeassistant.generate import *
 
 if __name__ == "__main__":
-    metadata_df = load_entity_metadata()
-
     write_bootstrap()
     write_healthcheck()
--- src/eva/rhasspy/src/build/python/rhasspy/generate.py
+++ src/eva/rhasspy/src/build/python/rhasspy/generate.py
@@ -2,13 +2,10 @@ from os.path import *
 
 from homeassistant.generate import load_entity_metadata
 from homeassistant.generate import load_env
-from homeassistant.generate import load_modules
+from homeassistant.generate import *
 
 DIR_ROOT = abspath(join(dirname(realpath(__file__)), "../../../.."))
 
 if __name__ == "__main__":
-    env = load_env(DIR_ROOT)
-    modules = load_modules(load_disabled=False, load_infrastrcture=False)
-    metadata_df = load_entity_metadata()
-
-    print("Build generate script [rhasspy] completed".format())
+    write_bootstrap()
+    write_healthcheck()
