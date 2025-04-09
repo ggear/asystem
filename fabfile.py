4a0d1b677ef0e557231ea475c866bffc06c16bea
Graham Gear <graham.gear@nowhere.org>
2025-04-09 10:22:55 +0800

    Update asystem-10.100.5608

--- fabfile.py
+++ fabfile.py
@@ -731,7 +731,7 @@ def _release(context):
                            .format(ssh_pass, host, install, install))
                 # _run_local(context, "{}scp -qprO $(find target/release -maxdepth 1 -type f) root@{}:{}"
                 #            .format(ssh_pass, host, install), module)
-                _run_local(context, "{}scp -qprO target/release/.  root@{}:{}"
+                _run_local(context, "{}scp -qpr target/release/.  root@{}:{}"
                            .format(ssh_pass, host, install), module)
                 print("Installing release to {} ... ".format(host))
                 _run_local(context, "{}ssh -q root@{} 'rm -f {}/../latest && ln -sfv {} {}/../latest'"
