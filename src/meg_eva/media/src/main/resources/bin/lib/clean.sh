0b73acc73e4f77156471bfd34504485b7e53d865
Graham Gear <graham.gear@nowhere.org>
2025-04-11 10:18:12 +0800

    Update asystem-10.100.5638

--- src/meg_eva/media/src/main/resources/bin/lib/clean.sh
+++ src/meg_eva/media/src/main/resources/bin/lib/clean.sh
@@ -16,7 +16,7 @@ echo -n "Cleaning '${WORKING_DIR}' ... "
 rm -rf ${LOG}
 find "${WORKING_DIR}" -name "._metadata_*.yaml" -type f -delete &>${LOG}
 find "${WORKING_DIR}" -name "._defaults_merged_*.yaml" -type f -delete &>${LOG}
-for SCRIPT in "merge" "rename" "reformat" "transcode"; do
+for SCRIPT in "analyse" "merge" "rename" "reformat" "transcode"; do
   find "${WORKING_DIR}" -name "${SCRIPT}.sh" -type f -delete &>${LOG}
   find "${WORKING_DIR}" -name "._${SCRIPT}_*" -type d -exec rm -rf '{}' + &>${LOG}
 done
