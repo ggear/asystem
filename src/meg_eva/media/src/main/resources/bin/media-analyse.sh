b4a2ddae8c250e4a3237d20a87f91b2c772161c8
Graham Gear <graham.gear@nowhere.org>
2025-05-06 14:50:46 +0800

    Update asystem-10.100.5735

--- src/meg_eva/media/src/main/resources/bin/media-analyse.sh
+++ src/meg_eva/media/src/main/resources/bin/media-analyse.sh
@@ -8,7 +8,17 @@ if [ -n "${SHARE_DIR_MEDIA}" ]; then
   "${ROOT_DIR}/lib/clean.sh" "${PWD}"
   "${PYTHON_DIR}/python" "${LIB_ROOT}/analyse.py" "${PWD}" "${MEDIA_GOOGLE_SHEET_GUID}" --verbose
 elif [ -n "${SHARE_DIR}" ]; then
-  "${SHARE_DIR}/tmp/scripts/media/analyse.sh"
+  if [ -f "${SHARE_DIR}/tmp/scripts/media/analyse.sh" ]; then
+    "${SHARE_DIR}/tmp/scripts/media/analyse.sh"
+  else
+    "${PYTHON_DIR}/python" "${LIB_ROOT}/analyse.py" "${SHARE_DIR}/media" "${MEDIA_GOOGLE_SHEET_GUID}" --verbose
+  fi
 else
-  for _SHARE_DIR in ${SHARE_DIRS_LOCAL}; do "${_SHARE_DIR}/tmp/scripts/media/analyse.sh"; done
+  for _SHARE_DIR in ${SHARE_DIRS_LOCAL}; do
+    if [ -f "${_SHARE_DIR}/tmp/scripts/media/analyse.sh" ]; then
+      "${_SHARE_DIR}/tmp/scripts/media/analyse.sh"
+    else
+      "${PYTHON_DIR}/python" "${LIB_ROOT}/analyse.py" "${_SHARE_DIR}" "${MEDIA_GOOGLE_SHEET_GUID}"
+    fi
+  done
 fi
