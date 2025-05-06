7478fecc8203d8b0a5c65b7a67c488873166ba5f
Graham Gear <graham.gear@nowhere.org>
2025-05-06 12:13:47 +0800

    Update asystem-10.100.5724-SNAPSHOT

--- src/meg_eva/media/src/main/resources/bin/lib/analyse.py
+++ src/meg_eva/media/src/main/resources/bin/lib/analyse.py
@@ -1469,14 +1469,13 @@ if [ $(uname) == "Darwin" ]; then
   for LABEL in $(basename "$(realpath $(asystem-media-home)/../../../../..)" | tr "_" "\\n"); do
     HOST_NAME="$(grep "${LABEL}" "$(asystem-media-home)/../../../../../../../.hosts" | cut -d "=" -f 2 | cut -d "," -f 1)""-${LABEL}"
     HOST_DIRS='. $(asystem-media-home)/.env_media; echo ${SHARE_DIRS_LOCAL} | grep ${SHARE_ROOT}/'"$(basename "$(realpath "${ROOT_DIR}/../../..")")"' | wc -l'
-    HOST_CMD='. $(asystem-media-home)/.env_media; cd ${SHARE_ROOT}/'"${SCRIPT_DIR} && ${SCRIPT_CMD}"
+    HOST_CMD='. $(asystem-media-home)/.env_media; ${SHARE_ROOT}/'"${SCRIPT_DIR}/${SCRIPT_CMD}"
     if [ $(ssh "root@${HOST_NAME}" "${HOST_DIRS}") -gt 0 ]; then
         LOG=$(ssh "root@${HOST_NAME}" "${HOST_CMD}" | tee /dev/tty)
     fi
   done
 else
-  cd "${SHARE_ROOT}/${SCRIPT_DIR}"
-  LOG=$(${SCRIPT_CMD} | tee /dev/tty)
+  LOG=$("${SHARE_ROOT}/${SCRIPT_DIR}/${SCRIPT_CMD}" | tee /dev/tty)
 fi
         """
         script_source_exec_refresh = """
