39804fcaad04c72dfd4b9f65dc6ba8c6ae38d5b2
Graham Gear <graham.gear@nowhere.org>
2025-05-08 10:48:58 +0800

    Update asystem-10.100.5761

--- src/meg_eva/media/src/main/python/media/analyse.py
+++ src/meg_eva/media/src/main/python/media/analyse.py
@@ -1470,14 +1470,14 @@ if [ $(uname) == "Darwin" ]; then
   for LABEL in $(basename "$(realpath $(asystem-media-home)/../../../../..)" | tr "_" "\\n"); do
     HOST_NAME="$(grep "${LABEL}" "$(asystem-media-home)/../../../../../../../.hosts" | cut -d "=" -f 2 | cut -d "," -f 1)""-${LABEL}"
     HOST_DIRS='. $(asystem-media-home)/.env_media; echo ${SHARE_DIRS_LOCAL} | grep ${SHARE_ROOT}/'"$(basename "$(realpath "${ROOT_DIR}/../../..")")"' | wc -l'
-    HOST_CMD='. $(asystem-media-home)/.env_media; ${SHARE_ROOT}/'"${SCRIPT_DIR}/${SCRIPT_CMD} ${1}"
+    HOST_CMD='. $(asystem-media-home)/.env_media; ${SHARE_ROOT}/'"${SCRIPT_DIR}/${SCRIPT_CMD} $@"
     if [ $(ssh "root@${HOST_NAME}" "${HOST_DIRS}") -gt 0 ]; then
         echo "Executing remotely ..."
         LOG=$(ssh "root@${HOST_NAME}" "${HOST_CMD}" | tee "${LOG_DEV}")
     fi
   done
 else
-  LOG=$("${SHARE_ROOT}/${SCRIPT_DIR}/${SCRIPT_CMD}" | tee "${LOG_DEV}")
+  LOG=$("${SHARE_ROOT}/${SCRIPT_DIR}/${SCRIPT_CMD}" $@ | tee "${LOG_DEV}")
 fi
         """
         script_source_exec_analyse = """
