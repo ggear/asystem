16047d2b16f7827ea22b87f1814be29f4e021913
Graham Gear <graham.gear@nowhere.org>
2025-05-10 11:22:48 +0800

    Update asystem-10.100.5799

--- src/meg_eva/media/src/main/resources/bin/media-downscale.sh
+++ src/meg_eva/media/src/main/resources/bin/media-downscale.sh
@@ -5,12 +5,11 @@ ROOT_DIR="$(dirname "$(readlink -f "$0")")"
 . "${ROOT_DIR}/.env_media"
 
 SCRIPT_NAME="downscale.sh"
-SCRIPT_PATH="${ROOT_DIR}/lib/${SCRIPT_NAME}"
 SCRIPT_FILE="tmp/scripts/media/${SCRIPT_NAME}"
 if [ -n "${SHARE_DIR_MEDIA}" ]; then
   find . -name ${SCRIPT_NAME} -exec "{}" \; | grep -v "No such file or directory"
 elif [ -n "${SHARE_DIR}" ]; then
-  if [ -f "${SHARE_DIR}/${SCRIPT_FILE}" ]; then "${SHARE_DIR}/${SCRIPT_FILE}"; else "${SCRIPT_PATH}" "${SHARE_DIR}"; fi
+  [[ ! -f "${SHARE_DIR}/${SCRIPT_FILE}" ]] && asystem-media-analyse || "${SHARE_DIR}/${SCRIPT_FILE}"
 else
   for _SHARE_DIR in ${SHARE_DIRS_LOCAL}; do
     if [ -f "${_SHARE_DIR}/${SCRIPT_FILE}" ]; then "${_SHARE_DIR}/${SCRIPT_FILE}"; else "${SCRIPT_PATH}" "${_SHARE_DIR}"; fi
