68eb2dcffb88e3a46963801c45b5bb6ec5e34d52
Graham Gear <graham.gear@nowhere.org>
2025-05-17 17:07:34 +0800

    Update asystem-10.100.5843-SNAPSHOT

--- src/meg_eva/media/src/main/resources/bin/lib/analyse.py
+++ src/meg_eva/media/src/main/resources/bin/lib/analyse.py
@@ -1342,7 +1342,7 @@ def _analyse(file_path_root, sheet_guid, clean=False, verbose=False):
                            " -name \""), pl.col("File Stem"), pl.lit("*\" 2>/dev/null | wc -l) -le 2 ]; then\n"),
                     pl.lit("  ORIGNL_FILE=\"$(find \"${ORIGNL_DIR}\" ! -name *." +
                            " ! -name *.".join(MEDIA_FILE_EXTENSIONS_IGNORE) +
-                           " -name \""), pl.col("File Stem"), pl.lit("\\.*\")\"\n"),
+                           " -name \""), pl.col("File Stem"), pl.lit("\\.*\")\" 2>/dev/null\n"),
                     pl.lit("  TRNSCD_FILE=\"${ROOT_DIR}/../"), pl.col("File Name"), pl.lit("\"\n"),
                     pl.lit("  MERGED_FILE=\"${ORIGNL_DIR}/"), pl.col("File Stem"), pl.lit(".mkv\"\n"),
                     pl.lit("  if [ -f \"${ORIGNL_FILE}\" ] && [ -f \"${TRNSCD_FILE}\" ] &&\n"),
