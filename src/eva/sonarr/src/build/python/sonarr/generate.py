38dca9db9b9802172cccea914a659afe25589e7f
Graham Gear <graham.gear@nowhere.org>
2025-06-03 14:19:23 +0800

    Update asystem-10.100.5920

--- src/eva/sonarr/src/build/python/sonarr/generate.py
+++ src/eva/sonarr/src/build/python/sonarr/generate.py
@@ -3,5 +3,5 @@ from homeassistant.generate import *
 DIR_ROOT = abspath(join(dirname(realpath(__file__)), "../../../.."))
 
 if __name__ == "__main__":
-    write_bootstrap(working_dir=join(DIR_ROOT, "src/main/resources/data"))
+    write_bootstrap()
     write_healthcheck()
