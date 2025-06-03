e19afdedb9520dff297f085dd26b99d81a122f71
Graham Gear <graham.gear@nowhere.org>
2025-06-03 14:25:19 +0800

    Update asystem-10.100.5921

--- src/eva/sonarr/src/build/resources/checkalive.sh
+++ src/eva/sonarr/src/build/resources/checkalive.sh
@@ -1,1 +1,1 @@
-[ "$(curl -X GET http://${SONARR_SERVICE_PROD}:${SONARR_HTTP_PORT}/api  -H "X-Api-Key: ${SONARR_API_KEY}" | jq 'length == 0')" == "false" ]
+[ "$(curl http://${SONARR_SERVICE_PROD}:${SONARR_HTTP_PORT}/api  -H "X-Api-Key: ${SONARR_API_KEY}" | jq 'length == 0')" == "false" ]
