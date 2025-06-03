c16e97cd9b0b4edc54e1a2e2ad24be894f21fe8e
Graham Gear <graham.gear@nowhere.org>
2025-06-03 11:48:51 +0800

    Update asystem-10.100.5900

--- src/eva/sonarr/src/main/resources/image/bootstrap.sh
+++ src/eva/sonarr/src/main/resources/image/bootstrap.sh
@@ -47,20 +47,21 @@ if [ $(curl -sf -X GET http://${SONARR_SERVICE_PROD}:${SONARR_HTTP_PORT}/api/v3/
 fi
 
 if [ $(curl -sf -X GET http://${SONARR_SERVICE_PROD}:${SONARR_HTTP_PORT}/api/v3/downloadclient -H "X-Api-Key: ${SONARR_API_KEY}" | jq 'length == 1 and .[0].name == "SABnzbd"') != "true" ]; then
-  curl -X POST http://${SONARR_SERVICE_PROD}:${SONARR_HTTP_PORT}/api/v3/indexer -H "X-Api-Key: ${SONARR_API_KEY}" -H "Content-Type: application/json" -d '
+  curl -X POST http://${SONARR_SERVICE_PROD}:${SONARR_HTTP_PORT}/api/v3/downloadclient -H "X-Api-Key: ${SONARR_API_KEY}" -H "Content-Type: application/json" -d '
 {
-  "name": "SABnzbd",
+  "name": "SABnzbd12",
   "enable": true,
   "protocol": "usenet",
   "priority": 1,
   "removeCompletedDownloads": true,
   "removeFailedDownloads": false,
   "fields": [
-    { "name": "host", "value": "http://'"${SABNZBD_SERVICE_PROD}"':'"${SABNZBD_HTTP_PORT}"'" },
+    { "name": "host", "value": "'"${SABNZBD_SERVICE_PROD}"'" },
+    { "name": "port", "value": "'"${SABNZBD_HTTP_PORT}"'" },
     { "name": "apiKey", "value": "'"${SABNZBD_API_KEY}"'" },
     { "name": "username", "value": "" },
     { "name": "password", "value": "" },
-    { "name": "category", "value": "" },
+    { "name": "category", "value": "tv" },
     { "name": "recentTvPriority", "value": -100 },
     { "name": "olderTvPriority", "value": -100 },
     { "name": "seasonFolder", "value": true },
