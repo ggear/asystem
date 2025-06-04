5567a367bec920bde106efea46128389bbc68cd2
Graham Gear <graham.gear@nowhere.org>
2025-06-04 12:10:11 +0800

    Update asystem-10.100.5934

---
+++ src/eva/sonarr/generate.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+. ../../../.env_fab
+. ../../../generate.sh
+
+ROOT_DIR="$(dirname "$(readlink -f "$0")")"
+
+# Defined: [/asystem/.env_fab](https://github.com/ggear/asystem/blob/master/.env_fab)
+pull_repo "${ROOT_DIR}" "${1}" "sonarr" "sonarr" "sonarr/sonarr" "v${SONARR_VERSION}"
+if [ ! -f "${ROOT_DIR}/src/main/resources/image/Sonarr.main.${SONARR_VERSION}.linux-x64.tar.gz" ]; then
+  mkdir -p "${ROOT_DIR}/src/main/resources/image"
+  rm -rf "${ROOT_DIR}/src/main/resources/image/"*.gz
+  wget -q -O "${ROOT_DIR}/src/main/resources/image/Sonarr.main.${SONARR_VERSION}.linux-x64.tar.gz" "https://github.com/Sonarr/Sonarr/releases/download/v${SONARR_VERSION}/Sonarr.main.${SONARR_VERSION}.linux-x64.tar.gz"
+fi
