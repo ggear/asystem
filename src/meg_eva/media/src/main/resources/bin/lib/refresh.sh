9d7fdbae97c8302b661b2497a7b36cd2503c9dad
Graham Gear <graham.gear@nowhere.org>
2025-06-09 11:57:58 +0800

    Update asystem-10.100.5960

--- src/meg_eva/media/src/main/resources/bin/lib/refresh.sh
+++ src/meg_eva/media/src/main/resources/bin/lib/refresh.sh
@@ -12,48 +12,50 @@ if [ "${LIBRARY}" == "plex" ]; then
   done
   echo "done"
 fi
-if [ "${LIBRARY}" == "sonarr" ]; then
-  echo -n "Refreshing 'http://${SONARR_SERVICE_PROD}/libraries' ... "
-  auth_header=(-H "X-Api-Key: ${SONARR_API_KEY}")
-  payload='{"name": "DownloadedEpisodesScan"}'
-  status=$(curl -s -o /dev/null -w "%{http_code}" \
-    -X POST "${SONARR_URL}/api/v3/command" \
-    -H "X-Api-Key: ${SONARR_API_KEY}" \
-    -H "Content-Type: application/json" \
-    -d "${payload}")
-  if [[ "$status" != 2* ]]; then
-    echo "failed"
-    curl -s \
-      -X POST "${SONARR_URL}/api/v3/command" \
-      -H "X-Api-Key: ${SONARR_API_KEY}" \
-      -H "Content-Type: application/json" \
-      -d "${payload}" | fold -s -w 250 | pr -to 4
-    echo "" && exit 1
-  fi
-  series_ids=$(curl -s "${SONARR_URL}/api/v3/series" "${auth_header[@]}" | jq -r '.[].id')
-  for series_id in $series_ids; do
-    payload='{"name": "RescanSeries", "seriesId": '"${series_id}"'}'
-    status=$(curl -s -o /dev/null -w "%{http_code}" \
-      -X POST "${SONARR_URL}/api/v3/command" "${auth_header[@]}" -H "Content-Type: application/json" \
-      -d "${payload}")
-    if [[ "$status" != 2* ]]; then
-      echo "failed"
-      curl -s \
-        -X POST "${SONARR_URL}/api/v3/command" "${auth_header[@]}" -H "Content-Type: application/json" \
-        -d "${payload}" | fold -s -w 250 | pr -to 4
-      echo "" && exit 1
-    fi
-    payload='{"name": "RefreshSeries", "seriesId": '"${series_id}"'}'
-    status=$(curl -s -o /dev/null -w "%{http_code}" \
-      -X POST "${SONARR_URL}/api/v3/command" "${auth_header[@]}" -H "Content-Type: application/json" \
-      -d "${payload}")
-    if [[ "$status" != 2* ]]; then
-      echo "failed"
-      curl -s \
-        -X POST "${SONARR_URL}/api/v3/command" "${auth_header[@]}" -H "Content-Type: application/json" \
-        -d "${payload}" | fold -s -w 250 | pr -to 4
-      echo "" && exit 1
-    fi
-  done
-  echo "done"
-fi
+
+# TODO: Only enable if sonarr automatic download does not break
+#if [ "${LIBRARY}" == "sonarr" ]; then
+#  echo -n "Refreshing 'http://${SONARR_SERVICE_PROD}/libraries' ... "
+#  auth_header=(-H "X-Api-Key: ${SONARR_API_KEY}")
+#  payload='{"name": "DownloadedEpisodesScan"}'
+#  status=$(curl -s -o /dev/null -w "%{http_code}" \
+#    -X POST "${SONARR_URL}/api/v3/command" \
+#    -H "X-Api-Key: ${SONARR_API_KEY}" \
+#    -H "Content-Type: application/json" \
+#    -d "${payload}")
+#  if [[ "$status" != 2* ]]; then
+#    echo "failed"
+#    curl -s \
+#      -X POST "${SONARR_URL}/api/v3/command" \
+#      -H "X-Api-Key: ${SONARR_API_KEY}" \
+#      -H "Content-Type: application/json" \
+#      -d "${payload}" | fold -s -w 250 | pr -to 4
+#    echo "" && exit 1
+#  fi
+#  series_ids=$(curl -s "${SONARR_URL}/api/v3/series" "${auth_header[@]}" | jq -r '.[].id')
+#  for series_id in $series_ids; do
+#    payload='{"name": "RescanSeries", "seriesId": '"${series_id}"'}'
+#    status=$(curl -s -o /dev/null -w "%{http_code}" \
+#      -X POST "${SONARR_URL}/api/v3/command" "${auth_header[@]}" -H "Content-Type: application/json" \
+#      -d "${payload}")
+#    if [[ "$status" != 2* ]]; then
+#      echo "failed"
+#      curl -s \
+#        -X POST "${SONARR_URL}/api/v3/command" "${auth_header[@]}" -H "Content-Type: application/json" \
+#        -d "${payload}" | fold -s -w 250 | pr -to 4
+#      echo "" && exit 1
+#    fi
+#    payload='{"name": "RefreshSeries", "seriesId": '"${series_id}"'}'
+#    status=$(curl -s -o /dev/null -w "%{http_code}" \
+#      -X POST "${SONARR_URL}/api/v3/command" "${auth_header[@]}" -H "Content-Type: application/json" \
+#      -d "${payload}")
+#    if [[ "$status" != 2* ]]; then
+#      echo "failed"
+#      curl -s \
+#        -X POST "${SONARR_URL}/api/v3/command" "${auth_header[@]}" -H "Content-Type: application/json" \
+#        -d "${payload}" | fold -s -w 250 | pr -to 4
+#      echo "" && exit 1
+#    fi
+#  done
+#  echo "done"
+#fi
