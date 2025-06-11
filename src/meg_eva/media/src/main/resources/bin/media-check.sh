2776de36fa7de125ca818a81f8aa1e0e41e4d02f
Graham Gear <graham.gear@nowhere.org>
2025-06-11 12:02:26 +0800

    Update asystem-10.100.5968

---
+++ src/meg_eva/media/src/main/resources/bin/media-check.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+ROOT_DIR="$(dirname "$(readlink -f "$0")")"
+
+. "${ROOT_DIR}/.env_media"
+
+SCRIPT_NAME="check.sh"
+SCRIPT_PATH="${ROOT_DIR}/lib/${SCRIPT_NAME}"
+SCRIPT_FILE="tmp/scripts/media/${SCRIPT_NAME}"
+if [ -n "${SHARE_DIR_MEDIA}" ]; then
+  find . -name ${SCRIPT_NAME} -exec "{}" \; | grep -v "No such file or directory"
+elif [ -n "${SHARE_DIR}" ]; then
+  [[ ! -f "${SHARE_DIR}/${SCRIPT_FILE}" ]] && asystem-media-analyse
+  "${SHARE_DIR}/${SCRIPT_FILE}"
+else
+  for _SHARE_DIR in ${SHARE_DIRS_LOCAL}; do
+    [[ ! -f "${_SHARE_DIR}/${SCRIPT_FILE}" ]] && asystem-media-analyse
+    "${_SHARE_DIR}/${SCRIPT_FILE}"
+  done
+fi
+echo "" && asystem-media-analyse
