9fa9515fcd93439392b29d5611bc12da358d4624
Graham Gear <graham.gear@nowhere.org>
2025-06-11 12:20:49 +0800

    Update asystem-10.100.5973

--- src/meg_eva/media/src/main/resources/bin/media-merge.sh
+++ src/meg_eva/media/src/main/resources/bin/media-merge.sh
@@ -10,7 +10,7 @@ SCRIPT_FILE="tmp/scripts/media/${SCRIPT_NAME}"
 if [ -n "${SHARE_DIR_MEDIA}" ]; then
   find . -name ${SCRIPT_NAME} -exec "{}" \; | grep -v "No such file or directory"
 elif [ -n "${SHARE_DIR}" ]; then
-  [[ ! -f "${SHARE_DIR}/${SCRIPT_FILE}" ]] && asystem-media-analyse
+  [[ ! -f "${SHARE_DIR}/${SCRIPT_FILE}" ]] && asystem-media-"${SCRIPT_NAME%.sh}"
   "${SHARE_DIR}/${SCRIPT_FILE}"
 else
   for _SHARE_DIR in ${SHARE_DIRS_LOCAL}; do
@@ -18,4 +18,3 @@ else
     "${_SHARE_DIR}/${SCRIPT_FILE}"
   done
 fi
-
