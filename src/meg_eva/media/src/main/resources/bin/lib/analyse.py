f0bc98072a5f78d9163012fe87540554cd351f28
Graham Gear <graham.gear@nowhere.org>
2025-06-22 14:08:48 +0800

    Update asystem-10.100.6031-SNAPSHOT

--- src/meg_eva/media/src/main/resources/bin/lib/analyse.py
+++ src/meg_eva/media/src/main/resources/bin/lib/analyse.py
@@ -1930,7 +1930,7 @@ for name in "${OP_NAMES[@]}"; do
 done
 
 # Filter log once
-LOG=$(echo "${LOG}" | grep -E "1\. Rename|2\. Check|3\. Merge|4\. Upscale" | grep "/share")
+LOG=$(echo "${LOG}" | grep -E "1\\. Rename|2\\. Check|3\\. Merge|4\\. Upscale" | grep "/share")
 readarray -t LOG_LINES <<< "${LOG}"
 
 # Process each operation
