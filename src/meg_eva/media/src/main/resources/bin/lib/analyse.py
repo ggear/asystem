023b2ec491261f80bc37b8cfda4f4669303d342f
Graham Gear <graham.gear@nowhere.org>
2025-06-27 10:41:19 +0800

    Update asystem-10.100.6051-SNAPSHOT

--- src/meg_eva/media/src/main/resources/bin/lib/analyse.py
+++ src/meg_eva/media/src/main/resources/bin/lib/analyse.py
@@ -1177,7 +1177,7 @@ def _analyse(file_path_root, sheet_guid, clean=False, force=False, defaults=Fals
                 ).then(pl.lit("Downscale High Size Non-HVEC"))
                 .when(
                     (pl.col("Media Type") == "series") &
-                    (pl.col("File Size (GB)").cast(pl.Float32) > 1.6)
+                    (pl.col("File Size (GB)").cast(pl.Float32) > 2)
                 ).then(pl.lit("Downscale High Size Non-HVEC"))
                 #
                 # Reformat
