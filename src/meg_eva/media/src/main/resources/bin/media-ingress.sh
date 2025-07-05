e602c42ba4fb44f057d5f49e1b68db0c1dbbd399
Graham Gear <graham.gear@nowhere.org>
2025-07-05 17:04:46 +0800

    Update asystem-10.100.6078

--- src/meg_eva/media/src/main/resources/bin/media-ingress.sh
+++ src/meg_eva/media/src/main/resources/bin/media-ingress.sh
@@ -5,4 +5,3 @@ ROOT_DIR="$(dirname "$(readlink -f "$0")")"
 . "${ROOT_DIR}/.env_media"
 
 [[ $(uname) == "Linux" ]] && "${ROOT_DIR}/lib/ingress.sh" /share/3
-[[ $(uname) == "Linux" ]] && "${PYTHON_DIR}/python" "${LIB_ROOT}/ingress.py" /share/3/tmp
