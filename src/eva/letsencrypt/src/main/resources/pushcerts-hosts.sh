a0b896acf06a0fb058ea58ea378d98ab0cee4861
Graham Gear <graham.gear@nowhere.org>
2025-07-18 08:14:33 +0800

    Update asystem-10.100.6105

--- src/eva/letsencrypt/src/main/resources/pushcerts-hosts.sh
+++ src/eva/letsencrypt/src/main/resources/pushcerts-hosts.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-ssh -n -q -o "StrictHostKeyChecking=no" root@macmini-eva "find /var/lib/asystem/install/appdaemon/latest/config -name certificates.sh -exec {} pull macmini-eva macmini-eva \;"
-ssh -n -q -o "StrictHostKeyChecking=no" root@macmini-eva "find /var/lib/asystem/install/appdaemon/latest/config -name certificates.sh -exec {} push macmini-eva macmini-eva \;"
-logger -t pushcerts "Pushed new certificates to [macmini-eva]"
 ssh -n -q -o "StrictHostKeyChecking=no" root@udm-net "find /var/lib/asystem/install/udmutilities/latest/config -name certificates.sh -exec {} pull macmini-eva udm-net \;"
 ssh -n -q -o "StrictHostKeyChecking=no" root@udm-net "find /var/lib/asystem/install/udmutilities/latest/config -name certificates.sh -exec {} push macmini-eva udm-net \;"
 logger -t pushcerts "Pushed new certificates to [udm-net]"
 ssh -n -q -o "StrictHostKeyChecking=no" root@macmini-meg "find /var/lib/asystem/install/nginx/latest/config -name certificates.sh -exec {} pull macmini-eva macmini-meg \;"
 ssh -n -q -o "StrictHostKeyChecking=no" root@macmini-meg "find /var/lib/asystem/install/nginx/latest/config -name certificates.sh -exec {} push macmini-eva macmini-meg \;"
 logger -t pushcerts "Pushed new certificates to [macmini-meg]"
+ssh -n -q -o "StrictHostKeyChecking=no" root@macbook-rae "find /var/lib/asystem/install/appdaemon/latest/config -name certificates.sh -exec {} pull macmini-eva macbook-rae \;"
+ssh -n -q -o "StrictHostKeyChecking=no" root@macbook-rae "find /var/lib/asystem/install/appdaemon/latest/config -name certificates.sh -exec {} push macmini-eva macbook-rae \;"
+logger -t pushcerts "Pushed new certificates to [macbook-rae]"
