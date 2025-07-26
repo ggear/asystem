39fa4dfe5554d53e6a2b69863a2b251da79d9b04
Graham Gear <graham.gear@nowhere.org>
2025-07-26 18:23:02 +0800

    Update asystem-10.100.6146

--- initialise.sh
+++ initialise.sh
@@ -9,15 +9,7 @@
 cd "/Users/graham/Code/asystem/"
 for i in {0..1}; do
   FAB_SKIP_GROUP_ALLBUT=$i FAB_SKIP_DELTA=true fab rel
-  echo "" && echo "" && read -n 1 -s -r -p "Press any key to continue ..." && echo "" && echo ""
+  echo "" && echo "" && read -n 1 -s -r -p "Press any key to continue ... \n\n\n"
 done
-
-FAB_SKIP_GROUP_ALLBUT=10 FAB_SKIP_DELTA=true fab rel
-FAB_SKIP_GROUP_ALLBUT=11 FAB_SKIP_DELTA=true fab rel
-FAB_SKIP_GROUP_ALLBUT=12 FAB_SKIP_DELTA=true fab rel
-
-FAB_SKIP_GROUP_ALLBUT=13 FAB_SKIP_DELTA=true fab rel
-FAB_SKIP_GROUP_ALLBUT=14 FAB_SKIP_DELTA=true fab rel
-FAB_SKIP_GROUP_ALLBUT=15 FAB_SKIP_DELTA=true fab rel
 
 # reboot now
