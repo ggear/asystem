9b6c9b6f21a62a7ecdf92f8d7cb5fa21bdffc429
Graham Gear <graham.gear@nowhere.org>
2025-07-26 14:44:52 +0800

    Update asystem-10.100.6132

--- src/meg_may_max_jil_jen_gui_dar/_links/install.sh
+++ src/meg_may_max_jil_jen_gui_dar/_links/install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 mkdir -p /home/asystem
-[ ! -L /root/home ] && ln -s /home/asystem /root/home || true
+[ -d /root ] && [ ! -L /root/home ] && ln -s /home/asystem /root/home || true
 
 mkdir -p /var/lib/asystem/install
-[ ! -L /root/install ] && ln -s /var/lib/asystem/install /root/install || true
+[ -d /root ] && [ ! -L /root/install ] && ln -s /var/lib/asystem/install /root/install || true
