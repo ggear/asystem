c53d3687dcb838beff566f3f830233a776bb4893
Graham Gear <graham.gear@nowhere.org>
2025-07-26 19:05:11 +0800

    Update asystem-10.100.6153

--- initialise.sh
+++ initialise.sh
@@ -8,23 +8,24 @@
 ################################################################################
 
 initialise() {
-  local start=$1
-  local end=$2
-  local rel_end=0
-  cd "/Users/graham/Code/asystem/"
-  for i in $(seq ${start} ${end}); do
-    SERVICES=$(FAB_SKIP_GROUP_ALLBUT=${i} FAB_SKIP_DELTA=true fab cle | grep "CLEAN TRANSIENTS SUCCESSFUL" | cut -d' ' -f4 | cut -d'-' -f1,2 | paste -sd, - | sed 's/,/, /g' | tr ',' '\n' | sed 's/.*-\([^-, ]*\)$/\1/' | paste -sd', ' -)
-    if [[ -n "${SERVICES}" ]]; then
-      rel_end=${i}
-      echo "Service Group [${i}] found [${SERVICES}]"
+  local start_index=$1
+  local end_index=$2
+  local last_index=0
+  cd "/Users/graham/Code/asystem/" && echo "" && echo ""
+  for i in $(seq ${start_index} ${end_index}); do
+    service_name_csv=$(FAB_SKIP_GROUP_ALLBUT=${i} FAB_SKIP_DELTA=true fab cle | grep "CLEAN TRANSIENTS SUCCESSFUL" | cut -d' ' -f4 | cut -d'-' -f1,2 | paste -sd, - | sed 's/,/, /g' | tr ',' '\n' | sed 's/.*-\([^-, ]*\)$/\1/' | paste -sd', ' -)
+    if [[ -n "${service_name_csv}" ]]; then
+      last_index=${i}
+      echo "Service Group [${i}] found [${service_name_csv}]"
     fi
   done
-  if [ "${rel_end}" -gt 0 ]; then
-    for i in $(seq ${start} ${rel_end}); do
+  echo "" && echo "" && echo -e "Press any key to release all services ... \n\n\n" && read -n 1 -s -r
+  if [ "${last_index}" -gt 0 ]; then
+    for i in $(seq ${start_index} ${last_index}); do
       FAB_SKIP_GROUP_ALLBUT=${i} FAB_SKIP_DELTA=true fab rel
-      echo "" && echo "" && echo -e "Press any key to continue ... \n\n\n" && read -n 1 -s -r
+      echo "" && echo "" && echo -e "Press any key to release next service ... \n\n\n" && read -n 1 -s -r
     done
   fi
 }
 
-initialise 1 9
+initialise 0 9
