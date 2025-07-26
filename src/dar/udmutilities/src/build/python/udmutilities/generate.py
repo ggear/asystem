1aba8bfbfa997de4c83b14d35c019868236a9b50
Graham Gear <graham.gear@nowhere.org>
2025-07-26 17:02:33 +0800

    Update asystem-10.100.6141

--- src/dar/udmutilities/src/build/python/udmutilities/generate.py
+++ src/dar/udmutilities/src/build/python/udmutilities/generate.py
@@ -8,6 +8,11 @@ import pandas as pd
 import requests
 import urllib3
 
+DIR_ROOT = abspath(join(dirname(realpath(__file__)), "../../../.."))
+for dir_module in glob.glob(join(DIR_ROOT, "../../*/*")):
+    if dir_module.endswith("homeassistant"):
+        sys.path.insert(0, join(dir_module, "src/build/python"))
+
 from homeassistant.generate import load_entity_metadata
 from homeassistant.generate import load_env
 from homeassistant.generate import load_modules
