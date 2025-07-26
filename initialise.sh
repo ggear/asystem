defc479dd0158f2a3a3b9d21c59afe7072942e07
Graham Gear <graham.gear@nowhere.org>
2025-07-26 18:58:53 +0800

    Update asystem-10.100.6152

--- initialise.sh
+++ initialise.sh
@@ -6,10 +6,25 @@
 ################################################################################
 # Setup network devices
 ################################################################################
-cd "/Users/graham/Code/asystem/"
-for i in {0..1}; do
-  FAB_SKIP_GROUP_ALLBUT=$i FAB_SKIP_DELTA=true fab rel
-  echo "" && echo "" && echo -e "Press any key to continue ... \n\n\n" && read -n 1 -s -r
-done
 
-# reboot now
+initialise() {
+  local start=$1
+  local end=$2
+  local rel_end=0
+  cd "/Users/graham/Code/asystem/"
+  for i in $(seq ${start} ${end}); do
+    SERVICES=$(FAB_SKIP_GROUP_ALLBUT=${i} FAB_SKIP_DELTA=true fab cle | grep "CLEAN TRANSIENTS SUCCESSFUL" | cut -d' ' -f4 | cut -d'-' -f1,2 | paste -sd, - | sed 's/,/, /g' | tr ',' '\n' | sed 's/.*-\([^-, ]*\)$/\1/' | paste -sd', ' -)
+    if [[ -n "${SERVICES}" ]]; then
+      rel_end=${i}
+      echo "Service Group [${i}] found [${SERVICES}]"
+    fi
+  done
+  if [ "${rel_end}" -gt 0 ]; then
+    for i in $(seq ${start} ${rel_end}); do
+      FAB_SKIP_GROUP_ALLBUT=${i} FAB_SKIP_DELTA=true fab rel
+      echo "" && echo "" && echo -e "Press any key to continue ... \n\n\n" && read -n 1 -s -r
+    done
+  fi
+}
+
+initialise 1 9
