7e49891480b3823d8ff6484184b857631dd76a91
Graham Gear <graham.gear@nowhere.org>
2025-07-27 10:49:08 +0800

    Update asystem-10.100.6159

--- install_all.sh
+++ install_all.sh
@@ -3,31 +3,38 @@
 # Install all modules
 ###############################################################################
 
-install() {
+install_all() {
   local start_index=$1
   local end_index=$2
   local last_index=0
-  cd "/Users/graham/Code/asystem/" && echo "" && echo ""
+  local list_only=${3:-true}
+  cd "/Users/graham/Code/asystem/"
   for i in $(seq ${start_index} ${end_index}); do
-    service_name_csv=$(FAB_SKIP_GROUP_ALLBUT=${i} FAB_SKIP_DELTA=true fab cle | grep "CLEAN TRANSIENTS SUCCESSFUL" | cut -d' ' -f4 | cut -d'-' -f1,2 | paste -sd, - | sed 's/,/, /g' | tr ',' '\n' | sed 's/.*-\([^-, ]*\)$/\1/' | paste -sd', ' -)
+    service_name_csv=$(
+      FAB_SKIP_GROUP_ALLBUT=${i} FAB_SKIP_DELTA=true fab cle |
+        grep "CLEAN TRANSIENTS SUCCESSFUL" | cut -d' ' -f4 | cut -d'-' -f1,2 | paste -sd, - |
+        sed 's/,/, /g' | tr ',' '\n' | sed 's/.*-\([^-, ]*\)$/\1/' | paste -sd', ' -
+    )
     if [[ -n "${service_name_csv}" ]]; then
       last_index=${i}
-      echo "Service Group [${i}] found [${service_name_csv}]"
+      echo "Service Group [$(printf "%02d" ${i})] found services [${service_name_csv}]"
     fi
   done
-  echo "" && echo "" && echo -e "Press any key to install all services ... \n\n\n" && read -n 1 -s -r
-  if [ "${last_index}" -gt 0 ]; then
-    for i in $(seq ${start_index} ${last_index}); do
-      FAB_SKIP_GROUP_ALLBUT=${i} FAB_SKIP_DELTA=true fab rel
-      [[ $i != ${last_index} ]] && echo "" && echo "" && echo -e "Press any key to install next service ... \n\n\n" && read -n 1 -s -r
-    done
+  if [ "${list_only}" = false ]; then
+    echo "" && echo "" && echo -e "Press any key to install all services ... \n\n\n" && read -n 1 -s -r
+    if [ "${last_index}" -gt 0 ]; then
+      for i in $(seq ${start_index} ${last_index}); do
+        FAB_SKIP_GROUP_ALLBUT=${i} FAB_SKIP_DELTA=true fab rel
+        [[ $i != ${last_index} ]] && echo "" && echo "" && echo -e "Press any key to install next service ... \n\n\n" && read -n 1 -s -r
+      done
+    fi
   fi
 }
 
 if [ $# -eq 2 ]; then
-  install "$1" "$2"
+  install_all "$1" "$2" false
 else
-  install 0 9
-  install 10 19
-  install 20 29
+  install_all 0 9
+  install_all 10 19
+  install_all 20 29
 fi
