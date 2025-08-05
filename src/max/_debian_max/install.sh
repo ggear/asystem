ccf8baa42f7a2134ed87eba94ab5b2f733ae6b4c
Graham Gear <graham.gear@nowhere.org>
2025-08-05 10:39:41 +0800

    Update asystem-10.100.6246

--- src/max/_debian_max/install.sh
+++ src/max/_debian_max/install.sh
@@ -33,7 +33,6 @@ cp -rvf ${SERVICE_INSTALL}/fstab /etc/fstab
 systemctl stop remote-fs.target
 systemctl daemon-reload
 for SHARE_DIR in $(grep -v '^#' /etc/fstab | grep '/share' | awk '{print $2}'); do mkdir -p ${SHARE_DIR}; done
-for SHARE_DIR in $(grep -v '^#' /etc/fstab | grep '/share' | grep cifs | awk 'BEGIN{FS=OFS=" "}{print $2}'); do umount -f ${SHARE_DIR} >/dev/null 2>&1; done
 mount -a
 systemctl start remote-fs.target
 echo "" && echo "" && df -h / /var /tmp /home /share/* && echo "" && echo ""
