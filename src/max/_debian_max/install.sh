a44e88a1172a6140249c04c1d5f7bfce8417d2cc
Graham Gear <graham.gear@nowhere.org>
2025-08-05 10:36:38 +0800

    Update asystem-10.100.6244

--- src/max/_debian_max/install.sh
+++ src/max/_debian_max/install.sh
@@ -26,32 +26,7 @@ lvdisplay | grep 'LV Size'
 ################################################################################
 # Mounts
 ################################################################################
-cat <<EOF >/etc/fstab
-# /etc/fstab: static file system information.
-#
-# Use 'blkid' to print the universally unique identifier for a
-# device; this may be used with UUID= as a more robust way to name devices
-# that works even if disks are added and removed. See fstab(5).
-#
-# systemd generates mount units based on this file, see systemd.mount(5).
-# Please run 'systemctl daemon-reload' after making changes here.
-#
-# <file system>                               <mount point>       <type>  <options>                                                                                                                                        <dump>  <pass>
-UUID=85BE-1335                                /boot/efi           vfat    umask=0077                                                                                                                                       0       1
-UUID=360cc974-b634-49d8-9be6-7ef209a0f16b     /boot               ext2    noatime,defaults                                                                                                                                 0       2
-/dev/mapper/macmini--max--vg-swap_1           none                swap    sw                                                                                                                                               0       0
-/dev/mapper/macmini--max--vg-root             /                   ext4    noatime,commit=600,errors=remount-ro                                                                                                             0       1
-/dev/mapper/macmini--max--vg-home             /home               ext4    noatime,commit=600,errors=remount-ro                                                                                                             0       2
-/dev/mapper/macmini--max--vg-tmp              /tmp                ext4    noatime,commit=600,errors=remount-ro                                                                                                             0       2
-/dev/mapper/macmini--max--vg-var              /var                ext4    noatime,commit=600,errors=remount-ro                                                                                                             0       2
-/dev/mapper/macmini--max--vg-share--10        /share/10           ext4    noatime,commit=600,errors=remount-ro                                                                                                             0       2
-#UUID=XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX     /share/7            ext4    noatime,commit=600,errors=remount-ro                                                                                                             0       2
-#//macmini-may/share-1                         /share/1            cifs    guest,uid=graham,gid=users,rw,noperm,iocharset=utf8,file_mode=0640,dir_mode=0750,vers=3.0,nofail,x-systemd.automount,x-systemd.idle-timeout=0s   0       0
-//macmini-may/share-2                         /share/2            cifs    guest,uid=graham,gid=users,rw,noperm,iocharset=utf8,file_mode=0640,dir_mode=0750,vers=3.0,nofail,x-systemd.automount,x-systemd.idle-timeout=0s   0       0
-//macmini-may/share-3                         /share/3            cifs    guest,uid=graham,gid=users,rw,noperm,iocharset=utf8,file_mode=0640,dir_mode=0750,vers=3.0,nofail,x-systemd.automount,x-systemd.idle-timeout=0s   0       0
-//macmini-meg/share-4                         /share/4            cifs    guest,uid=graham,gid=users,rw,noperm,iocharset=utf8,file_mode=0640,dir_mode=0750,vers=3.0,nofail,x-systemd.automount,x-systemd.idle-timeout=0s   0       0
-//macmini-meg/share-5                         /share/5            cifs    guest,uid=graham,gid=users,rw,noperm,iocharset=utf8,file_mode=0640,dir_mode=0750,vers=3.0,nofail,x-systemd.automount,x-systemd.idle-timeout=0s   0       0
-EOF
+cp -rvf ./fstab /etc/fstab
 systemctl stop remote-fs.target
 systemctl daemon-reload
 for SHARE_DIR in $(grep -v '^#' /etc/fstab | grep '/share' | awk '{print $2}'); do mkdir -p ${SHARE_DIR}; done
--- src/meg_may_max/media/src/main/resources/bin/media-mount.sh
+++ src/meg_may_max/media/src/main/resources/bin/media-mount.sh
@@ -1,22 +1,24 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 ROOT_DIR="$(dirname "$(readlink -f "$0")")"
 
 . "${ROOT_DIR}/.env_media"
 
 if [ $(hostname) == "macbook-lyn" ]; then
-
-  find "${LIB_ROOT}/../../../../../.." -name fstab
-
-#  for _SHARE in macmini-max,10 macmini-may,2 macmini-may,3 macmini-meg,4 macmini-meg,5; do
-#    IFS=","
-#    set -- ${_SHARE}
-#    mkdir -p ~/Desktop/share/${2}
-#    diskutil unmount force ~/Desktop/share/${2} &>/dev/null
-#    mount_smbfs -o soft,nodatacache,nodatacache //GUEST:@${1}/share-${2} ~/Desktop/share/${2}
-#  done
-
-
+  hosts_file=$(realpath "${LIB_ROOT}/../../../../../../../.hosts")
+  for fstab_file in $(find "${LIB_ROOT}/../../../../../.." ! -path */target/* -name fstab); do
+    host_label=$(basename $(realpath "${fstab_file}/../../../../.."))
+    host_name="$(grep "^${host_label}=" "${hosts_file}" | cut -d'=' -f2 | cut -d',' -f1)-${host_label}"
+    for share_index in $(grep -v '^#' ${fstab_file} | awk '/\/share/ && /ext4/ {print $2}' | sed 's|/share/||'); do
+      share_dir="${HOME}/Desktop/share/${share_index}"
+      echo -n "Mounting //GUEST:@${host_name}/share-${share_index} at ${share_dir} ... "
+      mkdir -p ${share_dir}
+      chmod 755 ${share_dir}
+      diskutil unmount force ${share_dir} &>/dev/null
+      mount_smbfs -o soft,nodatacache,nodatacache //GUEST:@${host_name}/share-${share_index} ${share_dir}
+      echo "done"
+    done
+  done
 else
   IMPORT_MEDIA_DEV="/dev/"$(lsblk -ro name,label | grep GRAHAM | awk 'BEGIN{FS=OFS=" "}{print $1}')
   umount -fq /media/usbdrive 2>&1 >/dev/null
