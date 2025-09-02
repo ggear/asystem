1194c51ad0f989aa9ded871fe3801c97f0ad9868
Graham Gear <graham.gear@nowhere.org>
2025-09-02 16:46:10 +0800

    Update asystem-10.100.6328-SNAPSHOT

--- src/meg_may_max_mad_jen_jil_gui_dar_jac/_homes/install.sh
+++ src/meg_may_max_mad_jen_jil_gui_dar_jac/_homes/install.sh
@@ -58,7 +58,7 @@ export LC_COLLATE=C
 export CLICOLOR=1
 export LSCOLORS=ExFxCxDxBxegedabagacad
 export LS_OPTIONS='--color=auto'
-PS1='[\[\e[01;35m\]\u@\h\[\e[00m\] \W]\$ '
+PS1='[\[\e[35m\]\u@\h\[\e[0m\] \W]\$ '
 export PROMPT_COMMAND='echo -ne "\033]0;${USER}@${HOSTNAME}: ${PWD}\007"'
 
 alias dmesg='dmesg -T'
