acefada8167762133032c44225d276a36753242d
Graham Gear <graham.gear@nowhere.org>
2025-09-04 15:07:06 +0800

    Update asystem-10.100.6364

--- src/meg_may_max_mad/media/src/main/resources/bin/media-ingress.sh
+++ src/meg_may_max_mad/media/src/main/resources/bin/media-ingress.sh
@@ -4,4 +4,4 @@ ROOT_DIR="$(dirname "$(readlink -f "$0")")"
 
 . "${ROOT_DIR}/.env_media"
 
-[[ $(uname) == "Linux" ]] && "${ROOT_DIR}/lib/ingress.sh" /share/3
+[[ $(uname) == "Linux" ]] && "${ROOT_DIR}/lib/ingress.sh" /share/11
