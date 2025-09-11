d68bd511f3d3a554683ba0221ca71d80eb8e958d
Graham Gear <graham.gear@nowhere.org>
2025-09-11 14:46:12 +0800

    Update asystem-10.100.6426

--- src/may/influxdb3/src/main/resources/image/query.sh
+++ src/may/influxdb3/src/main/resources/image/query.sh
@@ -1,25 +1,48 @@
 #!/bin/bash
 
 influxdb3 show databases
+
 influxdb3 query \
   --database host_private \
   "SHOW TABLES"
+
 influxdb3 query \
   --language influxql \
   --database host_private \
   "SHOW MEASUREMENTS"
+
 influxdb3 query \
   --language influxql \
   --database host_private \
   "SHOW FIELD KEYS FROM cpu"
+
 influxdb3 query \
   --language influxql \
   --database host_private \
   "SHOW TAG KEYS FROM cpu"
+
 influxdb3 query \
   --language influxql \
   --database host_private \
   "SHOW TAG VALUES FROM cpu WITH KEY = host"
+
+influxdb3 query \
+  --language influxql \
+  --format json \
+  --database host_private \
+  "SELECT COUNT(usage_system) FROM cpu" | jq -r '.[0].count'
+
+influxdb3 query \
+  --language influxql \
+  --database host_private "
+    SELECT
+      time, host, usage_system, usage_user
+    FROM cpu
+    GROUP BY host
+    ORDER BY time DESC
+    LIMIT 1
+"
+
 influxdb3 query \
   --language influxql \
   --database host_private "
@@ -27,14 +50,8 @@ influxdb3 query \
       time, host, usage_system, usage_user
     FROM cpu
     WHERE
-      cpu = cpu-total AND
       time >= now() - 2h
---    time >= '2025-02-15T01:45:00Z' AND time <= '2025-02-15T01:45:30Z'
+--    time >= '2025-08-15T01:45:00Z' AND time <= '2025-10-15T01:45:30Z'
     ORDER by time DESC
-    LIMIT 10
+    LIMIT 30
 "
-influxdb3 query \
-  --language influxql \
-  --format json \
-  --database host_private \
-  "SELECT COUNT(usage_system) FROM cpu" | jq -r '.[0].count'
