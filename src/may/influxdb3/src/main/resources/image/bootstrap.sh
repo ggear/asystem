2ed5741366687d114e394d63c07a0a806b73d51c
Graham Gear <graham.gear@nowhere.org>
2025-09-11 10:43:34 +0800

    Update asystem-10.100.6419

--- src/may/influxdb3/src/main/resources/image/bootstrap.sh
+++ src/may/influxdb3/src/main/resources/image/bootstrap.sh
@@ -23,6 +23,9 @@ echo "--------------------------------------------------------------------------
 
 influxdb3 create token --admin --host ${INFLUXDB3_API_URL} | grep -v "token name already exists, _admin" || true
 
+
+sleep 100000
+
 # TODO: Work out if a REST API exists to create databases, else raise feature request for REST API doc or HTTP_BIND_ADDRESS respect, or both
 
 echo "--------------------------------------------------------------------------------"
