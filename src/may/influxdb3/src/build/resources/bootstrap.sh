b073daf0243ff55628288b5046f39daaab0ed4a7
Graham Gear <graham.gear@nowhere.org>
2025-09-11 10:06:20 +0800

    Update asystem-10.100.6417

--- src/may/influxdb3/src/build/resources/bootstrap.sh
+++ src/may/influxdb3/src/build/resources/bootstrap.sh
@@ -1,2 +1,3 @@
+influxdb3 create token --admin | grep -v "token name already exists, _admin" || true
+
 # TODO: Work out if a REST API exists to create databases, else raise feature request for REST API doc or HTTP_BIND_ADDRESS respect, or both
-[ $(curl -sf "http://${INFLUXDB3_SERVICE}:${INFLUXDB3_API_PORT}/api/v3/configure/database?format=csv&show_deleted=false" | grep -c host_private) -eq 0 ] && echo "Need to create database from outside of host!"
--- src/may/influxdb3/src/main/resources/image/bootstrap.sh
+++ src/may/influxdb3/src/main/resources/image/bootstrap.sh
@@ -21,8 +21,9 @@ echo "--------------------------------------------------------------------------
 echo "Bootstrap starting ..."
 echo "--------------------------------------------------------------------------------"
 
+influxdb3 create token --admin | grep -v "token name already exists, _admin" || true
+
 # TODO: Work out if a REST API exists to create databases, else raise feature request for REST API doc or HTTP_BIND_ADDRESS respect, or both
-[ $(curl -sf "http://${INFLUXDB3_SERVICE}:${INFLUXDB3_API_PORT}/api/v3/configure/database?format=csv&show_deleted=false" | grep -c host_private) -eq 0 ] && echo "Need to create database from outside of host!"
 
 echo "--------------------------------------------------------------------------------"
 echo "Bootstrap finished"
