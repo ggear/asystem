e3f9f1518580656b7edc2d307157525ebe4c4e2d
Graham Gear <graham.gear@nowhere.org>
2025-10-06 14:26:47 +0800

    Update asystem-10.100.6457

---
+++ src/roe/kasa/src/build/resources/kasa_init.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+kasa discover
+kasa --host 192.168.0.1
+kasa --host 192.168.0.1 off
+kasa --host 192.168.0.1 on
+kasa --host 192.168.0.1 wifi join BeavisAndButthead # Keytype: 3, Type in password (paste does not work)
+
+
