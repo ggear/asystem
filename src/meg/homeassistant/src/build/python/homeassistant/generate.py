6a624c99604cc57062016bacd4712ee44ae775d7
Graham Gear <graham.gear@nowhere.org>
2025-10-11 13:05:03 +0800

    Update asystem-10.100.6516

--- src/meg/homeassistant/src/build/python/homeassistant/generate.py
+++ src/meg/homeassistant/src/build/python/homeassistant/generate.py
@@ -281,7 +281,7 @@ for dev in "${!devices[@]}"; do
       else
         label=$(basename $(grep $value /etc/fstab | awk '{print $1}' | sed 's/PARTLABEL=//') | sed 's/.*-//')
       fi
-      devices[$dev]="label=${label}${devices[$dev]:+;${devices[$dev]}}"      
+      devices[$dev]="label=${label}${devices[$dev]:+;${devices[$dev]}}"
     fi
   done
 done
