3f38df74a539768652dd458971bce7b0a7c20eab
Graham Gear <graham.gear@nowhere.org>
2025-10-26 11:52:38 +0800

    Update asystem-10.100.6629

--- src/may/digitemp/src/build/resources/checkready.sh
+++ src/may/digitemp/src/build/resources/checkready.sh
@@ -1,3 +1,3 @@
 OUTPUT="$(telegraf --test 2>/dev/null)" &&
-  [ "$(grep -c '^> > digitemp,host=macmini-may,metric=temperature,run_code=0 metrics_failed=0,metrics_succeeded=3' <<<"${OUTPUT}")" -eq 1 ] &&
+  [ "$(grep -c '^> digitemp,host=macmini-may,metric=temperature,run_code=0 metrics_failed=0,metrics_succeeded=3' <<<"${OUTPUT}")" -eq 1 ] &&
   telegraf --once >/dev/null 2>&1
