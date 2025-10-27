5061378e023d9583a454d8d93e86d3cbed06b12d
Graham Gear <graham.gear@nowhere.org>
2025-10-27 15:55:33 +0800

    Update asystem-10.100.6650

--- src/jen/weewx/src/build/resources/checkalive.sh
+++ src/jen/weewx/src/build/resources/checkalive.sh
@@ -1,1 +1,2 @@
-[ $(ps aux | grep weewxd | grep python | grep -v grep | wc -l) -eq 1 ]
+[ $(ps aux | grep weewxd | grep python | grep -v grep | wc -l) -eq 1 ] &&
+  [ "$(curl -sf -o /dev/null -w "%{http_code}" http://${WEEWX_SERVICE}:${WEEWX_HTTP_PORT})" = "200" ]
--- src/jen/weewx/src/main/resources/image/checkalive.sh
+++ src/jen/weewx/src/main/resources/image/checkalive.sh
@@ -34,7 +34,7 @@ set -eo pipefail
 shopt -s expand_aliases
 
 if
-  [ $(ps aux | grep weewxd | grep python | grep -v grep | wc -l) -eq 1 ]
+  [ $(ps aux | grep weewxd | grep python | grep -v grep | wc -l) -eq 1 ] && [ "$(curl -sf -o /dev/null -w "%{http_code}" http://${WEEWX_SERVICE}:${WEEWX_HTTP_PORT})" = "200" ]
 then
   set +x
   [ "${HEALTHCHECK_VERBOSE}" == true ] && echo "✅ The service [weewx] is alive :)" >&2
