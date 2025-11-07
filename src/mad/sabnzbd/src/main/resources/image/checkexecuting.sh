183e904f49f4b75365342e759ba429f19a7b144d
Graham Gear <graham.gear@nowhere.org>
2025-11-07 12:31:58 +0800

    Update asystem-10.100.6689

--- src/mad/sabnzbd/src/main/resources/image/checkexecuting.sh
+++ src/mad/sabnzbd/src/main/resources/image/checkexecuting.sh
@@ -33,7 +33,7 @@ fi
 shopt -s expand_aliases
 
 if
-  READY="$(curl "http://${SABNZBD_SERVICE_PROD}:${SABNZBD_HTTP_PORT}/sabnzbd/api?output=json&apikey=${SABNZBD_API_KEY}&mode=status&skip_dashboard=0")" && [ "$(jq -r '.status.diskspace1 // 0' <<<"${READY}")" -gt 10 ] && [ "$(jq -r '[.status.servers[]?.servertotalconn // 0] | add' <<<"${READY}")" -gt 0 ]
+  curl -s "http://${SABNZBD_SERVICE_PROD}:${SABNZBD_HTTP_PORT}/sabnzbd/api?output=json&apikey=${SABNZBD_API_KEY}&mode=status&skip_dashboard=0" | jq -e '.status.diskspace1 // 0 > 10 and ([.status.servers[]?.servertotalconn // 0] | add) > 0'
 then
   set +x
   [ "${HEALTHCHECK_VERBOSE}" == true ] && echo "✅ The service [sabnzbd] is executing :)" >&2
