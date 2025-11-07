a7f2d26ecd7be7d3267594d05a8b99767644b9e6
Graham Gear <graham.gear@nowhere.org>
2025-11-07 12:26:17 +0800

    Update asystem-10.100.6688

--- src/mad/sabnzbd/src/build/resources/checkexecuting.sh
+++ src/mad/sabnzbd/src/build/resources/checkexecuting.sh
@@ -1,3 +1,3 @@
 READY="$(curl "http://${SABNZBD_SERVICE_PROD}:${SABNZBD_HTTP_PORT}/sabnzbd/api?output=json&apikey=${SABNZBD_API_KEY}&mode=status&skip_dashboard=0")" &&
-  [ "$(jq -er '.status.diskspace1' <<<"${READY}")" -gt 10 ] &&
-  [ "$(jq -er '[.status.servers[].servertotalconn] | add' <<<"${READY}")" -gt 0 ]
+  [ "$(jq -r '.status.diskspace1 // 0' <<<"${READY}")" -gt 10 ] &&
+  [ "$(jq -r '[.status.servers[]?.servertotalconn // 0] | add' <<<"${READY}")" -gt 0 ]
--- src/mad/sabnzbd/src/main/resources/image/checkexecuting.sh
+++ src/mad/sabnzbd/src/main/resources/image/checkexecuting.sh
@@ -33,7 +33,7 @@ fi
 shopt -s expand_aliases
 
 if
-  READY="$(curl "http://${SABNZBD_SERVICE_PROD}:${SABNZBD_HTTP_PORT}/sabnzbd/api?output=json&apikey=${SABNZBD_API_KEY}&mode=status&skip_dashboard=0")" && [ "$(jq -er '.status.diskspace1' <<<"${READY}")" -gt 10 ] && [ "$(jq -er '[.status.servers[].servertotalconn] | add' <<<"${READY}")" -gt 0 ]
+  READY="$(curl "http://${SABNZBD_SERVICE_PROD}:${SABNZBD_HTTP_PORT}/sabnzbd/api?output=json&apikey=${SABNZBD_API_KEY}&mode=status&skip_dashboard=0")" && [ "$(jq -r '.status.diskspace1 // 0' <<<"${READY}")" -gt 10 ] && [ "$(jq -r '[.status.servers[]?.servertotalconn // 0] | add' <<<"${READY}")" -gt 0 ]
 then
   set +x
   [ "${HEALTHCHECK_VERBOSE}" == true ] && echo "✅ The service [sabnzbd] is executing :)" >&2
