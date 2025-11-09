d1e38edd9893ee3b7c98c7159c8acb9ae5b0290b
Graham Gear <graham.gear@nowhere.org>
2025-11-09 19:53:55 +0800

    Update asystem-10.100.6696

--- src/mad/sabnzbd/install_pre.sh
+++ src/mad/sabnzbd/install_pre.sh
@@ -3,6 +3,16 @@
 . ./.env
 
 # TODO: Check share mounted, if not attempt mount. if not failed, write a place holder, check placeholder in health
+SABNZBD_SHARE_ROOT_DIR="/share/$(echo "${SABNZBD_SHARE_DIR}" | awk -F'/' '{print $3}')"
+if grep -qE '^[^#].*[[:space:]]+'"${SABNZBD_SHARE_ROOT_DIR}"'[[:space:]]' /etc/fstab && mountpoint -q "${SABNZBD_SHARE_ROOT_DIR}"; then
+  echo "${SABNZBD_SHARE_ROOT_DIR} is configured and mounted"
+else
+  echo "${SABNZBD_SHARE_ROOT_DIR} missing or not mounted"
+fi
+
+
+
+
 
 for DIR in "enqueued" "finished" "reencoding" "staging"; do
   mkdir -p "${SABNZBD_SHARE_DIR}/$DIR"
