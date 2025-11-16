ccd983f8a14d14f37ef9c8cd1a1b7d7fd4f207a4
Graham Gear <graham.gear@nowhere.org>
2025-11-16 15:19:12 +0800

    Update asystem-10.100.6750

--- src/meg/homeassistant/src/build/resources/checkexecuting.sh
+++ src/meg/homeassistant/src/build/resources/checkexecuting.sh
@@ -1,2 +1,2 @@
 /asystem/etc/checkalive.sh "${POSITIONAL_ARGS[@]}" &&
-  [ "$(curl -H "Authorization: Bearer ${HOMEASSISTANT_API_TOKEN}" -H "Content-Type: application/json" "http://${HOMEASSISTANT_SERVICE}:${HOMEASSISTANT_HTTP_PORT}/api/states/input_boolean.home_started" | jq -er .state)" == "on" ]
+  [ "$(curl -sf -H "Authorization: Bearer ${HOMEASSISTANT_API_TOKEN}" -H "Content-Type: application/json" "http://${HOMEASSISTANT_SERVICE}:${HOMEASSISTANT_HTTP_PORT}/api/states/input_boolean.home_started" | jq -er '.state')" = "on" ]
--- src/meg/homeassistant/src/main/resources/data/checkhealthy.sh
+++ src/meg/homeassistant/src/main/resources/data/checkhealthy.sh
@@ -33,7 +33,7 @@ fi
 shopt -s expand_aliases
 
 if
-  /asystem/etc/checkexecuting.sh "${POSITIONAL_ARGS[@]}" && ! grep -Eq '^10\.0\.[0-9]+\.[0-9]+:' /config/ip_bans.yaml 2>/dev/null
+  /asystem/etc/checkexecuting.sh "${POSITIONAL_ARGS[@]}" && ts=$(date -u +"%Y-%m-%dT%H:%M:%S.%3NZ") && curl -s -X POST -H "Authorization: Bearer ${HOMEASSISTANT_API_TOKEN}" -H "Content-Type: application/json" -d '{"event_type":"postgres_test","event_data":{"ts":"'"$ts"'"}}' "http://${HOMEASSISTANT_SERVICE}:${HOMEASSISTANT_HTTP_PORT}/api/events" >/dev/null && curl -s -H "Authorization: Bearer ${HOMEASSISTANT_API_TOKEN}" "http://${HOMEASSISTANT_SERVICE}:${HOMEASSISTANT_HTTP_PORT}/api/history/period/$ts?filter_entity_id=none" | jq -e 'length > 0' >/dev/null && curl -s -H "Authorization: Bearer ${HOMEASSISTANT_API_TOKEN}" -H "Content-Type: application/json" "http://${HOMEASSISTANT_SERVICE}:${HOMEASSISTANT_HTTP_PORT}/api/hassio/addons" | jq -e '.data.addons[] | select(.slug=="core_influxdb")' >/dev/null && ! grep -Eq '^10\.0\.[0-9]+\.[0-9]+:' /config/ip_bans.yaml 2>/dev/null
 then
   set +x
   [ "${HEALTHCHECK_VERBOSE}" == true ] && echo "✅ The service [homeassistant] is healthy :)" >&2
