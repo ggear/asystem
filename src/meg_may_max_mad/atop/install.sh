c3059b340de01cbb581400f3af97c634ddc640cc
Graham Gear <graham.gear@nowhere.org>
2025-11-18 13:35:26 +0800

    Update asystem-10.100.6787

--- src/meg_may_max_mad/atop/install.sh
+++ src/meg_may_max_mad/atop/install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-chmod +x /var/lib/asystem/install/top/latest/bin/*.sh
-for SCRIPT in /var/lib/asystem/install/top/latest/bin/*.sh; do
+chmod +x /var/lib/asystem/install/atop/latest/bin/*.sh
+for SCRIPT in /var/lib/asystem/install/atop/latest/bin/*.sh; do
   rm -rf /usr/local/bin/$(basename "${SCRIPT}" .sh)
   ln -vs "${SCRIPT}" /usr/local/bin/$(basename "${SCRIPT}" .sh)
 done
