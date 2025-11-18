1ad3dad190a8b2db0fa7c91f6a54d6305ac5ee62
Graham Gear <graham.gear@nowhere.org>
2025-11-18 10:11:33 +0800

    Update asystem-10.100.6764

---
+++ src/meg/mariadb/src/main/resources/image/bootstrap.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+################################################################################
+# WARNING: This file is written by the build process, any manual edits will be lost!
+################################################################################
+
+echo "--------------------------------------------------------------------------------"
+echo "Service is starting ..."
+echo "--------------------------------------------------------------------------------"
+
+ASYSTEM_HOME=${ASYSTEM_HOME:-"/asystem/etc"}
+
+MESSAGE="Waiting for service to come alive ... "
+echo "${MESSAGE}"
+while ! "${ASYSTEM_HOME}/checkalive.sh"; do
+ echo "${MESSAGE}" && sleep 1
+done
+
+echo "--------------------------------------------------------------------------------"
+echo "Bootstrap starting ..."
+echo "--------------------------------------------------------------------------------"
+
+# TODO: Provide implementation
+echo ''
+
+echo "--------------------------------------------------------------------------------"
+echo "Bootstrap finished"
+echo "--------------------------------------------------------------------------------"
+
+set +eo pipefail
+
+MESSAGE="Waiting for service to start executing ... "
+echo "${MESSAGE}"
+while ! "${ASYSTEM_HOME}/checkexecuting.sh"; do
+  echo "${MESSAGE}" && sleep 1
+done
+echo "----------" && echo "✅ Service has started"
