c5cdb096ac3560f1eb2e197771d3428077595c6a
Graham Gear <graham.gear@nowhere.org>
2025-11-18 12:03:52 +0800

    Update asystem-10.100.6773

--- src/meg/vernemq/src/build/resources/checkalive.sh
+++ src/meg/vernemq/src/build/resources/checkalive.sh
@@ -1,1 +1,1 @@
-vmq-admin getpid >/dev/null 2>&1
+vmq-admin node status | awk -F '|' '/version/ {gsub(/ /,"",$3); exit ($3=="")?1:0}'
--- src/meg/vernemq/src/build/resources/checkexecuting.sh
+++ src/meg/vernemq/src/build/resources/checkexecuting.sh
@@ -1,3 +1,2 @@
 /asystem/etc/checkalive.sh "${POSITIONAL_ARGS[@]}" &&
-  vernemq chkconfig >/dev/null 2>&1 &&
-  vernemq ping >/dev/null 2>&1
+  vmq-admin node status | awk -F '|' '/num_offline/ {gsub(/ /,"",$3); offline=$3} /num_online/ {gsub(/ /,"",$3); online=$3} END {exit (offline>0 || online==0)?1:0}'
