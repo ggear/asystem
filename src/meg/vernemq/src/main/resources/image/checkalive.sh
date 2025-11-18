e681d17e3c3e6f7adcc269beed9aa98a04e93a7a
Graham Gear <graham.gear@nowhere.org>
2025-11-18 12:15:13 +0800

    Update asystem-10.100.6780

--- src/meg/vernemq/src/main/resources/image/checkalive.sh
+++ src/meg/vernemq/src/main/resources/image/checkalive.sh
@@ -33,7 +33,7 @@ fi
 shopt -s expand_aliases
 
 if
-  vmq-admin node status | awk -F '|' '/version/ {gsub(/ /,"",$3); exit ($3=="")?1:0}'
+  vmq-admin node status | awk -F '|' '/version/ {gsub(/ /,"",$3); exit ($3=="")?1:0}' >/dev/null 2>&1
 then
   set +x
   [ "${HEALTHCHECK_VERBOSE}" == true ] && echo "✅ The service [vernemq] is alive :)" >&2
