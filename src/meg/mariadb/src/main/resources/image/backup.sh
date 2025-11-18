dbff717d49d1984104f862e206ff6b6300db5f70
Graham Gear <graham.gear@nowhere.org>
2025-11-18 10:00:46 +0800

    Update asystem-10.100.6763-SNAPSHOT

--- src/meg/mariadb/src/main/resources/image/backup.sh
+++ src/meg/mariadb/src/main/resources/image/backup.sh
@@ -6,7 +6,7 @@ TIMESTAMP=$(date +"%Y-%m-%d_%H-%M-%S")
 for SCHEMA in "all"; do
   DIR="${DIR_BACKUP}/${TIMESTAMP}/${SCHEMA}"
   mkdir -p ${DIR}
-  mysqldump -uroot -p"${MARIADB_ROOT_PASSWORD}" --all-databases --single-transaction --quick | gzip > all_${TIMESTAMP}.sql.gz
+  mysqldump -uroot -p"${MARIADB_ROOT_PASSWORD}" --all-databases --single-transaction --quick | gzip >all_${TIMESTAMP}.sql.gz
   echo "Completed backup for timestamp [$TIMESTAMP] to [${DIR}]"
 done
 find ${DIR_BACKUP} -depth -empty -delete -type d
