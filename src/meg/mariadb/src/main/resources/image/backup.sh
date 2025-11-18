fd3ab834d7eabf590957d1ee4688ec8ff087fc92
Graham Gear <graham.gear@nowhere.org>
2025-11-18 10:06:29 +0800

    Update asystem-10.100.6763

--- src/meg/mariadb/src/main/resources/image/backup.sh
+++ src/meg/mariadb/src/main/resources/image/backup.sh
@@ -5,8 +5,9 @@ TIMESTAMP=$(date +"%Y-%m-%d_%H-%M-%S")
 
 for SCHEMA in "all"; do
   DIR="${DIR_BACKUP}/${TIMESTAMP}/${SCHEMA}"
-  mkdir -p ${DIR}
-  mysqldump -uroot -p"${MARIADB_ROOT_PASSWORD}" --all-databases --single-transaction --quick | gzip >all_${TIMESTAMP}.sql.gz
+  mkdir -p "${DIR}"
+  cd "${DIR}"
+  mariadb-dump -uroot -p"${MARIADB_ROOT_PASSWORD}" --all-databases --single-transaction --quick | gzip >"all_${TIMESTAMP}.sql.gz"
   echo "Completed backup for timestamp [$TIMESTAMP] to [${DIR}]"
 done
-find ${DIR_BACKUP} -depth -empty -delete -type d
+find "${DIR_BACKUP}" -depth -empty -delete -type d
--- src/meg/postgres/src/main/resources/image/backup.sh
+++ src/meg/postgres/src/main/resources/image/backup.sh
@@ -5,8 +5,9 @@ TIMESTAMP=$(date +"%Y-%m-%d_%H-%M-%S")
 
 for SCHEMA in "all"; do
   DIR="${DIR_BACKUP}/${TIMESTAMP}/${SCHEMA}"
-  mkdir -p ${DIR}
-  pg_dumpall -U postgres | gzip > all_${TIMESTAMP}.sql.gz
+  mkdir -p "${DIR}"
+  cd "${DIR}"
+  pg_dumpall -U postgres | gzip > "all_${TIMESTAMP}.sql.gz"
   echo "Completed backup for timestamp [$TIMESTAMP] to [${DIR}]"
 done
-find ${DIR_BACKUP} -depth -empty -delete -type d
+find "${DIR_BACKUP}" -depth -empty -delete -type d
