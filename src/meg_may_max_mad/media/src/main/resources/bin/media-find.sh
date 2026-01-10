b7594a4e0dfcc98878f0b7cbc55090c73567791b
Graham Gear <graham.gear@nowhere.org>
2026-01-10 14:25:05 +0800

    Update asystem-10.100.6840

--- src/meg_may_max_mad/media/src/main/resources/bin/media-find.sh
+++ src/meg_may_max_mad/media/src/main/resources/bin/media-find.sh
@@ -15,26 +15,22 @@ if [ "${SHARE_ROOT}" != "/share" ]; then
   share_ssh=(ssh root@macmini-mad)
   echo "Executing remotely ..."
 fi
-if "${share_ssh[@]}" bash -c '
-  . $(asystem-media-home)/.env_media
-  c1=$("$FIND_CMD" /share -mindepth 1 -maxdepth 1 | wc -l)
-  c2=$("$FIND_CMD" /share -mindepth 2 -maxdepth 2 -name media -type d | wc -l)
-  [[ "$c1" -eq "$c2" ]]
-'; then
-  declare -A dirs_found
-  while read -r file_found; do
-    if [[ "$file_found" == *"/series/"* ]]; then
-      dir_found="${file_found%/Season */*}"
-    else
-      dir_found="${file_found%/*}"
-    fi
-    dirs_found["$dir_found"]="$dir_found"
-  done < <("${share_ssh[@]}" ". $(asystem-media-home)/.env_media && $FIND_CMD" /share ! -name '._*' ! -path '*/audio/*' -path '*/media/*' -type f -iname "*${FILE_NAME_TOKEN}*" | sed "s|^/share|${SHARE_ROOT}|")
-  printf '%s\n' "${dirs_found[@]}" |
-    sort |
-    while read -r dir_found; do
-      [ -n "$dir_found" ] && echo "cd '$dir_found'"
-    done
-else
-  echo "Error: Not all shares are properly mounted."
-fi
+declare -A dirs_found
+while IFS= read -r file_found; do
+  if [[ "${file_found}" == *"/series/"* ]]; then
+    dir_found="${file_found%/Season */*}"
+  else
+    dir_found="${file_found%/*}"
+  fi
+  dirs_found["${dir_found}"]="${dir_found}"
+done < <(
+  "${share_ssh[@]}" bash -s <<EOF
+find /share -type f ! -name "._*" ! -path "*/audio/*" -path "*/media/*" -iname "*${FILE_NAME_TOKEN}*"
+EOF
+)
+printf '%s\n' "${dirs_found[@]}" | sort | while read -r dir_found; do
+  dir_found="${dir_found/#\/share/$SHARE_ROOT}"
+  if [ ! -z "${dir_found}" ]; then
+    echo "cd '${dir_found}'"
+  fi
+done
