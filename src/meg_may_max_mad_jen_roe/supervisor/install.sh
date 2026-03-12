e86808ba51bf0ff7086de8bcee6d8c20187d5641
Graham Gear <graham.gear@nowhere.org>
2026-03-12 18:51:58 +0800

    Update asystem-10.100.6864

--- src/meg_may_max_mad_jen_roe/supervisor/install.sh
+++ src/meg_may_max_mad_jen_roe/supervisor/install.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
-
-
+chmod +x /root/install/supervisor/latest/supervisor
+cat >/usr/local/bin/atop <<'EOF'
+#!/bin/bash
+/root/install/supervisor/latest/supervisor watch -m local -F 1 "$@"
+EOF
+chmod +x /usr/local/bin/atop
+cat >/usr/local/bin/atops <<'EOF'
+#!/bin/bash
+/root/install/supervisor/latest/supervisor watch -m remote "$@"
+EOF
+chmod +x /usr/local/bin/atops
