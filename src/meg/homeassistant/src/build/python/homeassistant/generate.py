1f4494ef2aa598f1a1491207d7d105626f5ba3a9
Graham Gear <graham.gear@nowhere.org>
2026-03-19 16:00:45 +0800

    Update asystem-10.100.6970

--- src/meg/homeassistant/src/build/python/homeassistant/generate.py
+++ src/meg/homeassistant/src/build/python/homeassistant/generate.py
@@ -552,35 +552,35 @@ def write_entity_metadata(module_name, working_dir, metadata_df, topics_discover
 # WARNING: This file is written by the build process, any manual edits will be lost!
 ################################################################################
 
-ROOT_DIR="$(dirname $(readlink -f "$0"))/mqtt"
+ROOT_DIR="$(dirname "$(readlink -f "$0")")/mqtt"
 
-. ${{ROOT_DIR}}/../../.env
+. "$ROOT_DIR/../../.env"
 
 printf "\\nEntity Metadata publish script [{}] dropping discovery topics:\\n"
-mosquitto_sub -h $VERNEMQ_SERVICE_PROD -p $VERNEMQ_API_PORT -F '%t' -t "{}" -W 10 2>/dev/null | \
+mosquitto_sub -h $VERNEMQ_SERVICE -p $VERNEMQ_API_PORT -F '%t' -t "{}" -W 5 2>/dev/null | sort -u | \\
   while read topic; do
-    echo "Destroying: $topic"
-    mosquitto_pub -h $VERNEMQ_SERVICE_PROD -p $VERNEMQ_API_PORT -t "$topic" -r -n
+    echo "$topic"
+    mosquitto_pub -h $VERNEMQ_SERVICE -p $VERNEMQ_API_PORT -t "$topic" -r -n
   done
-mosquitto_sub -h $VERNEMQ_SERVICE_PROD -p $VERNEMQ_API_PORT --remove-retained -F '%t' -t "{}" -W 10 2>/dev/null
+mosquitto_sub -h $VERNEMQ_SERVICE -p $VERNEMQ_API_PORT --remove-retained -F '%t' -t "{}" -W 5 2>/dev/null
 
 printf "\\nEntity Metadata publish script [{}] sleeping before dropping data topics ... " && sleep 2 && printf "done\\n\\n"
 
 printf "Entity Metadata publish script [{}] dropping data topics:\\n"
-mosquitto_sub -h $VERNEMQ_SERVICE_PROD -p $VERNEMQ_API_PORT -F '%t' -t "{}" -W 10 2>/dev/null | \
+mosquitto_sub -h $VERNEMQ_SERVICE -p $VERNEMQ_API_PORT -F '%t' -t "{}" -W 5 2>/dev/null | sort -u | \\
   while read topic; do
-    echo "Destroying: $topic"
-    mosquitto_pub -h $VERNEMQ_SERVICE_PROD -p $VERNEMQ_API_PORT -t "$topic" -r -n
+    echo "$topic"
+    mosquitto_pub -h $VERNEMQ_SERVICE -p $VERNEMQ_API_PORT -t "$topic" -r -n
   done
-mosquitto_sub -h $VERNEMQ_SERVICE_PROD -p $VERNEMQ_API_PORT --remove-retained -F '%t' -t "{}" -W 10 2>/dev/null
+mosquitto_sub -h $VERNEMQ_SERVICE -p $VERNEMQ_API_PORT --remove-retained -F '%t' -t "{}" -W 5 2>/dev/null
 
 printf "\\nEntity Metadata publish script [{}] sleeping before publishing discovery topics ... " && sleep 2 && printf "done\\n\\n"
 
 printf "Entity Metadata publish script [{}] publishing discovery topics:\\n"
-find $ROOT_DIR -path "*/{}/*" -name "*.json" -print0 | while read -d $'\\0' METADATA_FILE; do
+find "$ROOT_DIR" -path "*/{}/*" -name "*.json" -print0 | sort -z | while read -d $'\\0' METADATA_FILE; do
   METADATA_TOPIC=$(dirname "${{METADATA_FILE/$ROOT_DIR\\//}}")
-  mosquitto_pub -h $VERNEMQ_SERVICE -p $VERNEMQ_API_PORT -t $METADATA_TOPIC -f $METADATA_FILE -r
-  printf "$METADATA_TOPIC\\n"
+  mosquitto_pub -h $VERNEMQ_SERVICE -p $VERNEMQ_API_PORT -t "$METADATA_TOPIC" -f "$METADATA_FILE" -r
+  printf "%s\\n" "$METADATA_TOPIC"
 done
 printf "\\n"
             """.format(
