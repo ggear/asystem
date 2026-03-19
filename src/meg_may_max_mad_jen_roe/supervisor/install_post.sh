e62a19f957d75ca9de4119389a060ba335ffd843
Graham Gear <graham.gear@nowhere.org>
2026-03-19 11:28:04 +0800

    Update asystem-10.100.6951

--- src/meg_may_max_mad_jen_roe/supervisor/install_post.sh
+++ src/meg_may_max_mad_jen_roe/supervisor/install_post.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
 
-. ./.env
+SERVICE_HOME=/home/asystem/${SERVICE_NAME}/latest
+SERVICE_INSTALL=/var/lib/asystem/install/${SERVICE_NAME}/latest
 
-chmod +x /var/lib/asystem/install/supervisor/latest/data/supervisor
+. "${SERVICE_INSTALL}/.env"
+
+chmod +x ${SERVICE_INSTALL}/data/supervisor
 cat >/usr/local/bin/atop <<'EOF'
 #!/bin/bash
 
-/var/lib/asystem/install/supervisor/latest/data/supervisor watch -m local -F 1 "$@"
+${SERVICE_INSTALL}/data/supervisor watch -m local -F 1 "$@"
 
 EOF
 chmod +x /usr/local/bin/atop
 cat >/usr/local/bin/atops <<'EOF'
 #!/bin/bash
 
-/var/lib/asystem/install/supervisor/latest/data/supervisor watch -m remote "$@"
+${SERVICE_INSTALL}/data/supervisor watch -m remote "$@"
 
 EOF
 chmod +x /usr/local/bin/atops
