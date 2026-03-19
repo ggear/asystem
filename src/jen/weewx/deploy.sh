01a0b7c34162aede4f2b3c3ea444c65e9b0f286c
Graham Gear <graham.gear@nowhere.org>
2026-03-19 12:43:12 +0800

    Update asystem-10.100.6958-SNAPSHOT

--- src/jen/weewx/deploy.sh
+++ src/jen/weewx/deploy.sh
@@ -2,12 +2,7 @@
 
 ROOT_DIR="$(dirname "$(readlink -f "$0")")"
 
-export $(xargs <${ROOT_DIR}/.env) 2>/dev/null
-
-HOME="/home/asystem/$(basename "${ROOT_DIR}")/latest"
-INSTALL="/var/lib/asystem/install/$(basename "${ROOT_DIR}")/latest"
 HOST="$(grep "$(basename "$(dirname "${ROOT_DIR}")")" "${ROOT_DIR}/../../../.hosts" | tr '=' ' ' | tr ',' ' ' | awk '{ print $2 }')"-"$(basename "$(dirname "${ROOT_DIR}")")"
-export VERNEMQ_SERVICE=${VERNEMQ_SERVICE_PROD}
 
-${ROOT_DIR}/src/main/resources/image/mqtt.sh
+ssh root@${HOST} "docker exec weectl report run --date=$(date +%Y-%m-%d) --after=$(date -d '15 minutes ago' +%Y-%m-%dT%H:%M)"
 ssh root@${HOST} "docker exec weectl report run --date=$(date +%Y-%m-%d) --after=$(date -d '15 minutes ago' +%Y-%m-%dT%H:%M)"
