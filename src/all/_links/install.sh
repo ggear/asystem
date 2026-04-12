818a759afdffbe3b556a6ae5d329c317e403c122
Graham Gear <graham@nowhere.com>
2026-04-12 12:11:55 +0800

    Update asystem-10.100.7089

--- src/all/_links/install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-mkdir -p /home/asystem
-[ -d /root ] && [ ! -L /root/home ] && ln -s /home/asystem /root/home || true
-
-mkdir -p /var/lib/asystem/install
-[ -d /root ] && [ ! -L /root/install ] && ln -s /var/lib/asystem/install /root/install || true
