5b0f0b4b0f53ca1981443fbc53d1eea12f64b358
Graham Gear <graham@nowhere.com>
2026-06-29 14:51:11 +0800

    Update asystem-10.200.1087

--- src/all/media/deploy.sh
+++ src/all/media/deploy.sh
@@ -1,29 +1,26 @@
 #!/bin/bash
 
 ROOT_DIR="$(dirname "$(readlink -f "$0")")"
+SHARES_FILE="${ROOT_DIR}/src/main/resources/shares.csv"
 
-media-truncate
-media-refresh
-media-normalise
-media-clean
-media-analyse
-media-space
+[ -f "${SHARES_FILE}" ] || {
+  echo "Missing shares file [${SHARES_FILE}]" >&2
+  exit 1
+}
 
-HOSTS=$(echo $(basename $(dirname $(pwd))) | tr "_" "\n")
-HOST="$(grep $(echo "${HOSTS}" | head -1) ${ROOT_DIR}/../../../.hosts | tr '=' ' ' | tr ',' ' ' | awk '{ print $2 }')-$(echo "${HOSTS}" | head -1)"
-HOSTS=$(echo ${HOSTS} | xargs -n1 | sort | xargs)
+HOSTS="$(cut -d "," -f 1 "${SHARES_FILE}" | sort -u)"
+HOST="$(echo "${HOSTS}" | head -1)"
 
-#echo "------------------------------------------------------------" && echo "Server [${HOST}] media truncate starting"
-#ssh -o StrictHostKeyChecking=no root@${HOST} "/root/install/media/latest/bin/media-truncate.sh"
-#ssh -o StrictHostKeyChecking=no root@${HOST} "/root/install/media/latest/bin/media-refresh.sh"
-#echo "Server [${HOST}] media truncate completed" && echo "------------------------------------------------------------"
-#
-#for HOST in ${HOSTS}; do
-#  HOST="$(grep ${HOST} ${ROOT_DIR}/../../../.hosts | tr '=' ' ' | tr ',' ' ' | awk '{ print $2 }')-${HOST}"
-#  echo "------------------------------------------------------------" && echo "Server [${HOST}] media operations starting"
-#  ssh -o StrictHostKeyChecking=no -t -t -q root@${HOST} "/root/install/media/latest/bin/media-normalise.sh"
-#  ssh -o StrictHostKeyChecking=no -t -t -q root@${HOST} "/root/install/media/latest/bin/media-clean.sh"
-#  ssh -o StrictHostKeyChecking=no -t -t -q root@${HOST} "/root/install/media/latest/bin/media-analyse.sh"
-#  ssh -o StrictHostKeyChecking=no -t -t -q root@${HOST} "/root/install/media/latest/bin/media-space.sh"
-#  echo "Server [${HOST}] media operations completed" && echo "------------------------------------------------------------"
-#done
+echo "------------------------------------------------------------" && echo "Executing remotely ..."
+ssh -o StrictHostKeyChecking=no "root@${HOST}" "/root/install/media/latest/bin/media-truncate.sh"
+ssh -o StrictHostKeyChecking=no "root@${HOST}" "/root/install/media/latest/bin/media-refresh.sh"
+echo "------------------------------------------------------------"
+
+for HOST in ${HOSTS}; do
+  echo "------------------------------------------------------------" && echo "Executing remotely ..."
+  ssh -o StrictHostKeyChecking=no -t -t -q "root@${HOST}" "/root/install/media/latest/bin/media-normalise.sh"
+  ssh -o StrictHostKeyChecking=no -t -t -q "root@${HOST}" "/root/install/media/latest/bin/media-clean.sh"
+  ssh -o StrictHostKeyChecking=no -t -t -q "root@${HOST}" "/root/install/media/latest/bin/media-analyse.sh"
+  ssh -o StrictHostKeyChecking=no -t -t -q "root@${HOST}" "/root/install/media/latest/bin/media-space.sh"
+  echo "------------------------------------------------------------"
+done
