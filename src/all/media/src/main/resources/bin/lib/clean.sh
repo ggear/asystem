9380d753b418524ba7d810ca51d6bde833ec2ddb
Graham Gear <graham@nowhere.com>
2026-06-29 15:21:11 +0800

    Update asystem-10.200.1088

--- src/all/media/src/main/resources/bin/lib/clean.sh
+++ src/all/media/src/main/resources/bin/lib/clean.sh
@@ -13,19 +13,19 @@ fi
 LOG="/tmp/media-clean.log"
 
 echo -n "Cleaning '${WORKING_DIR}' ... "
-rm -rf ${LOG}
-${FIND_CMD} "${WORKING_DIR}" -name ".DS_Store" -type f -delete &>>${LOG}
-${FIND_CMD} "${WORKING_DIR}" -name "._metadata_*.yaml" -type f -delete &>>${LOG}
-${FIND_CMD} "${WORKING_DIR}" -name "._defaults_analysed_*.yaml" -type f -delete &>>${LOG}
+rm -f "${LOG}"
+${FIND_CMD} "${WORKING_DIR}" -name ".DS_Store" -type f -delete >>"${LOG}" 2>&1
+${FIND_CMD} "${WORKING_DIR}" -name "._metadata_*.yaml" -type f -delete >>"${LOG}" 2>&1
+${FIND_CMD} "${WORKING_DIR}" -name "._defaults_analysed_*.yaml" -type f -delete >>"${LOG}" 2>&1
 for SCRIPT in "rename" "check" "merge" "upscale" "reformat" "transcode" "downscale"; do
-  ${FIND_CMD} "${WORKING_DIR}" -name "${SCRIPT}.sh" -type f -delete &>>${LOG}
-  ${FIND_CMD} "${WORKING_DIR}" -name "._${SCRIPT}_*" -type d -exec rm -rf '{}' + &>>${LOG}
+  ${FIND_CMD} "${WORKING_DIR}" -name "${SCRIPT}.sh" -type f -delete >>"${LOG}" 2>&1
+  ${FIND_CMD} "${WORKING_DIR}" -name "._${SCRIPT}_*" -type d -exec rm -rf '{}' + >>"${LOG}" 2>&1
 done
-${FIND_CMD} "${WORKING_DIR}" -path "*/share/*/media/*" -type d -empty -delete &>>${LOG}
-${FIND_CMD} "${WORKING_DIR}" -path "*/share/*/media/*" -type d -empty -delete &>>${LOG}
+[ -d "${WORKING_DIR}" ] && ${FIND_CMD} "${WORKING_DIR}" -path "*/share/*/media/*" -type d -empty -delete >>"${LOG}" 2>&1
+[ -d "${WORKING_DIR}" ] && ${FIND_CMD} "${WORKING_DIR}" -path "*/share/*/media/*" -type d -empty -delete >>"${LOG}" 2>&1
 if [ -s "${LOG}" ]; then
   echo "failed"
-  cat ${LOG}
+  cat "${LOG}"
   exit 1
 fi
 echo "done"
