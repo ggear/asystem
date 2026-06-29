3cfd6430c909814dcd1d4bc550afa63970d1ba48
Graham Gear <graham@nowhere.com>
2026-06-29 14:21:20 +0800

    Update asystem-10.200.1087-SNAPSHOT

--- src/all/media/deploy.sh
+++ src/all/media/deploy.sh
@@ -8,3 +8,22 @@ media-normalise
 media-clean
 media-analyse
 media-space
+
+HOSTS=$(echo $(basename $(dirname $(pwd))) | tr "_" "\n")
+HOST="$(grep $(echo "${HOSTS}" | head -1) ${ROOT_DIR}/../../../.hosts | tr '=' ' ' | tr ',' ' ' | awk '{ print $2 }')-$(echo "${HOSTS}" | head -1)"
+HOSTS=$(echo ${HOSTS} | xargs -n1 | sort | xargs)
+
+#echo "------------------------------------------------------------" && echo "Server [${HOST}] media truncate starting"
+#ssh -o StrictHostKeyChecking=no root@${HOST} "/root/install/media/latest/bin/media-truncate.sh"
+#ssh -o StrictHostKeyChecking=no root@${HOST} "/root/install/media/latest/bin/media-refresh.sh"
+#echo "Server [${HOST}] media truncate completed" && echo "------------------------------------------------------------"
+#
+#for HOST in ${HOSTS}; do
+#  HOST="$(grep ${HOST} ${ROOT_DIR}/../../../.hosts | tr '=' ' ' | tr ',' ' ' | awk '{ print $2 }')-${HOST}"
+#  echo "------------------------------------------------------------" && echo "Server [${HOST}] media operations starting"
+#  ssh -o StrictHostKeyChecking=no -t -t -q root@${HOST} "/root/install/media/latest/bin/media-normalise.sh"
+#  ssh -o StrictHostKeyChecking=no -t -t -q root@${HOST} "/root/install/media/latest/bin/media-clean.sh"
+#  ssh -o StrictHostKeyChecking=no -t -t -q root@${HOST} "/root/install/media/latest/bin/media-analyse.sh"
+#  ssh -o StrictHostKeyChecking=no -t -t -q root@${HOST} "/root/install/media/latest/bin/media-space.sh"
+#  echo "Server [${HOST}] media operations completed" && echo "------------------------------------------------------------"
+#done
