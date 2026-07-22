6f8336e6b939910bd056bd2918cae8f240534378
Graham Gear <graham@nowhere.com>
2026-07-22 12:58:36 +0800

    Update asystem-10.200.1175

--- src/all/supervisor/src/build/python/supervisor/generate.py
+++ src/all/supervisor/src/build/python/supervisor/generate.py
@@ -8,7 +8,6 @@ DIR_ROOT = abspath(join(dirname(realpath(__file__)), "../../../.."))
 if __name__ == "__main__":
     metadata_df = load_entity_metadata()
 
-    write_bootstrap()
     write_healthcheck()
 
     # Build MQTT schema
--- src/all/supervisor/src/build/resources/bootstrap.sh
+++
@@ -1,2 +0,0 @@
-# TODO: Provide implementation
-echo ''
