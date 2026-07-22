3277744915ee1e8c66cc4311ba641600b16eedcc
Graham Gear <graham@nowhere.com>
2026-07-22 12:46:43 +0800

    Update asystem-10.200.1173

--- src/all/supervisor/src/main/resources/image/checkhealthy.sh
+++ src/all/supervisor/src/main/resources/image/checkhealthy.sh
@@ -33,7 +33,7 @@ fi
 shopt -s expand_aliases
 
 if
-  /asystem/etc/checkexecuting.sh "${POSITIONAL_ARGS[@]}" && curl -sf --unix-socket /var/run/docker.sock --max-time 2 "http://localhost/_ping" | grep -q "^OK$" && PAYLOAD=$(mosquitto_sub -h "$BROKER_HOST" -p "$BROKER_PORT" ${BROKER_TOKEN:+-u supervisor -P $BROKER_TOKEN} -t "supervisor/${SUPERVISOR_HOST}/data/host" -C 1 -W 2 2>/dev/null) && jq -e '.pulse.OK == true' <<<"$PAYLOAD" >/dev/null && TIMESTAMP=$(jq -r '.timestamp' <<<"$PAYLOAD") && (($(date +%s) - TIMESTAMP < 1200)) && PAYLOAD=$(mosquitto_sub -h "$BROKER_HOST" -p "$BROKER_PORT" ${BROKER_TOKEN:+-u supervisor -P $BROKER_TOKEN} -t "supervisor/${SUPERVISOR_HOST}/data/service/supervisor" -C 1 -W 2 2>/dev/null) && jq -e '.pulse.OK == true' <<<"$PAYLOAD" >/dev/null
+  /asystem/etc/checkexecuting.sh "${POSITIONAL_ARGS[@]}" && curl -sf --unix-socket /var/run/docker.sock --max-time 2 "http://localhost/_ping" | grep -q "^OK$" && PAYLOAD=$(mosquitto_sub -h "$BROKER_HOST" -p "$BROKER_PORT" ${BROKER_TOKEN:+-u supervisor -P $BROKER_TOKEN} -t "supervisor/${SUPERVISOR_HOST}/data/host" -C 1 -W 2 2>/dev/null) && jq -e '.pulse.ok == true' <<<"$PAYLOAD" >/dev/null && TIMESTAMP=$(jq -r '.timestamp' <<<"$PAYLOAD") && (($(date +%s) - TIMESTAMP < 1200)) && PAYLOAD=$(mosquitto_sub -h "$BROKER_HOST" -p "$BROKER_PORT" ${BROKER_TOKEN:+-u supervisor -P $BROKER_TOKEN} -t "supervisor/${SUPERVISOR_HOST}/data/service/supervisor" -C 1 -W 2 2>/dev/null) && jq -e '.pulse.ok == true' <<<"$PAYLOAD" >/dev/null
 then
   set +x
   [ "${HEALTHCHECK_VERBOSE}" == true ] && echo "✅ The service [supervisor] is healthy :)" >&2
