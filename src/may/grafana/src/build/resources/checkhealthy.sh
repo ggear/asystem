f589241755abddd566c152db85e3ce6429cccc74
Graham Gear <graham@nowhere.com>
2026-08-12 15:38:53 +0800

    Update asystem-10.200.1260

--- src/may/grafana/src/build/resources/checkhealthy.sh
+++ src/may/grafana/src/build/resources/checkhealthy.sh
@@ -1,2 +1,2 @@
 /asystem/etc/checkexecuting.sh "${POSITIONAL_ARGS[@]}" &&
-  [ "$(curl -o /dev/null -s -w "%{http_code}" "https://grafana.local.janeandgraham.com/d/public-home-default/home")" = "200" ]
+  [ "$(curl -L -o /dev/null -s -w "%{http_code}" "https://grafana.janeandgraham.com")" = "200" ]
