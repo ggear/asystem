f4a0643d19d6bf1268cad1b21de7e41303b273d8
Graham Gear <graham@nowhere.com>
2026-08-24 21:26:35 +0800

    Update asystem-10.200.1392

--- src/all/supervisor/src/build/python/supervisor/generate.py
+++ src/all/supervisor/src/build/python/supervisor/generate.py
@@ -8,7 +8,6 @@ DIR_ROOT = abspath(join(dirname(realpath(__file__)), "../../../.."))
 if __name__ == "__main__":
     metadata_df = load_bootstrap_entities()
 
-    write_container_backup()
     write_container_healthchecks()
 
     # Build broker schema
